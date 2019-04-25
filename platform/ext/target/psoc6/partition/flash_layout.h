0be69fdd2514c8084e436e19afa8e8ee90cc7ba9
Chris Brand <chris.brand@cypress.com>
2019-04-25 12:09:58 -0700

    plat: Copy files from ref_twincpu target to psoc6 target
    These files are copied verbatim as a starting point for the new target,
    with the exception that a few are renamed to suit the new target:
    ref_twincpu_host.cmake -> psoc6_host.cmake
    ref_twincpu_sc.cmake -> psoc6_sc.cmake
    ref_twincpu_bl2.sct -> psoc6_bl2.sct
    ref_twincpu_ns.sct -> psoc6_ns.sct
    ref_twincpu_s.sct -> psoc6_s.sct
    startup_cmsdk_ref_twincpu_s.s -> startup_ARMCM0P.s
    startup_cmsdk_ref_twincpu_ns.s -> startup_ARMCM4.s
    startup_cmsdk_ref_twincpu_bl2.s -> startup_psoc6_bl2.s
    
    Change-Id: I331666935cfe14422935c91d8a19f3cc4a204ff5
    Signed-off-by: Chris Brand <chris.brand@cypress.com>

---
+++ platform/ext/target/psoc6/partition/flash_layout.h
@@ -0,0 +1,140 @@
+/*
+ * Copyright (c) 2017-2019 Arm Limited. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __FLASH_LAYOUT_H__
+#define __FLASH_LAYOUT_H__
+
+/* Flash layout on MPS2 AN519 with BL2:
+ *
+ * 0x0000_0000 BL2 - MCUBoot(0.5 MB)
+ * 0x0008_0000 Flash_area_image_0(1 MB):
+ *    0x0008_0000 Secure     image primary
+ *    0x0010_0000 Non-secure image primary
+ * 0x0018_0000 Flash_area_image_1(1 MB):
+ *    0x0018_0000 Secure     image secondary
+ *    0x0020_0000 Non-secure image secondary
+ * 0x0028_0000 Scratch area(1 MB)
+ * 0x0038_0000 Secure Storage Area(0.02 MB)
+ * 0x0038_5000 NV counters area(16 Bytes)
+ * 0x0038_5010 Unused(0.491 MB)
+ *
+ * Flash layout on MPS2 AN519, if BL2 not defined:
+ * 0x0000_0000 Secure     image
+ * 0x0010_0000 Non-secure image
+ */
+
+/* This header file is included from linker scatter file as well, where only a
+ * limited C constructs are allowed. Therefore it is not possible to include
+ * here the platform_retarget.h to access flash related defines. To resolve this
+ * some of the values are redefined here with different names, these are marked
+ * with comment.
+ */
+
+/* The size of a partition. This should be large enough to contain a S or NS
+ * sw binary. Each FLASH_AREA_IMAGE contains two partitions. See Flash layout
+ * above.
+ */
+#define FLASH_PARTITION_SIZE            (0x80000)    /* 512 kB */
+
+/* Sector size of the flash hardware; same as FLASH0_SECTOR_SIZE */
+#define FLASH_AREA_IMAGE_SECTOR_SIZE    (0x1000)     /* 4 kB */
+/* Same as FLASH0_SIZE */
+#define FLASH_TOTAL_SIZE                (0x00400000) /* 4 MB */
+
+/* Flash layout info for BL2 bootloader */
+#define FLASH_BASE_ADDRESS              (0x10000000) /* same as FLASH0_BASE */
+
+/* Offset and size definitions of the flash partitions that are handled by the
+ * bootloader. The image swapping is done between IMAGE_0 and IMAGE_1, SCRATCH
+ * is used as a temporary storage during image swapping.
+ */
+#define FLASH_AREA_BL2_OFFSET           (0x0)
+#define FLASH_AREA_BL2_SIZE             (FLASH_PARTITION_SIZE)
+
+#define FLASH_AREA_IMAGE_0_OFFSET       (0x040000)
+#define FLASH_AREA_IMAGE_0_SIZE         (2 * FLASH_PARTITION_SIZE)
+
+#define FLASH_AREA_IMAGE_1_OFFSET       (0x180000)
+#define FLASH_AREA_IMAGE_1_SIZE         (2 * FLASH_PARTITION_SIZE)
+
+#define FLASH_AREA_IMAGE_SCRATCH_OFFSET (0x280000)
+#define FLASH_AREA_IMAGE_SCRATCH_SIZE   (2 * FLASH_PARTITION_SIZE)
+
+/* Maximum number of status entries supported by the bootloader. */
+#define BOOT_STATUS_MAX_ENTRIES         ((2 * FLASH_PARTITION_SIZE) / \
+                                         FLASH_AREA_IMAGE_SCRATCH_SIZE)
+
+/** Maximum number of image sectors supported by the bootloader. */
+#define BOOT_MAX_IMG_SECTORS            ((2 * FLASH_PARTITION_SIZE) / \
+                                         FLASH_AREA_IMAGE_SECTOR_SIZE)
+
+#define FLASH_SST_AREA_OFFSET           (0x380000)
+#define FLASH_SST_AREA_SIZE             (0x5000)   /* 20 KB */
+
+#define FLASH_NV_COUNTERS_AREA_OFFSET   (0x385000)
+#define FLASH_NV_COUNTERS_AREA_SIZE     (0x10)     /* 16 Bytes */
+
+/* Offset and size definition in flash area, used by assemble.py */
+#define SECURE_IMAGE_OFFSET             0x0
+#define SECURE_IMAGE_MAX_SIZE           0x80000
+
+#define NON_SECURE_IMAGE_OFFSET         0x80000
+#define NON_SECURE_IMAGE_MAX_SIZE       0x80000
+
+/* Flash device name used by BL2 and SST
+ * Name is defined in flash driver file: Driver_Flash.c
+ */
+#define FLASH_DEV_NAME Driver_FLASH0
+
+/* Secure Storage (SST) Service definitions
+ * Note: Further documentation of these definitions can be found in the
+ * TF-M SST Integration Guide.
+ */
+#define SST_FLASH_DEV_NAME Driver_FLASH0
+
+/* Secure Storage (SST) Service definitions */
+/* In this target the CMSIS driver requires only the offset from the base
+ * address instead of the full memory address.
+ */
+#define SST_FLASH_AREA_ADDR  FLASH_SST_AREA_OFFSET
+#define SST_SECTOR_SIZE      FLASH_AREA_IMAGE_SECTOR_SIZE
+/* The sectors must be in consecutive memory location */
+#define SST_NBR_OF_SECTORS  (FLASH_SST_AREA_SIZE / SST_SECTOR_SIZE)
+/* Specifies the smallest flash programmable unit in bytes */
+#define SST_FLASH_PROGRAM_UNIT  0x1
+/* The maximum asset size to be stored in the SST area */
+#define SST_MAX_ASSET_SIZE      2048
+/* The maximum number of assets to be stored in the SST area */
+#define SST_NUM_ASSETS          10
+
+/* NV Counters definitions */
+#define TFM_NV_COUNTERS_AREA_ADDR    FLASH_NV_COUNTERS_AREA_OFFSET
+#define TFM_NV_COUNTERS_AREA_SIZE    FLASH_NV_COUNTERS_AREA_SIZE
+#define TFM_NV_COUNTERS_SECTOR_ADDR  FLASH_NV_COUNTERS_AREA_OFFSET
+#define TFM_NV_COUNTERS_SECTOR_SIZE  FLASH_AREA_IMAGE_SECTOR_SIZE
+
+/* FIXME: Use SRAM2 memory to store RW data */
+#define S_RAM_ALIAS_BASE  (0x08000000)
+#define NS_RAM_ALIAS_BASE (0x08000000)
+
+/* Shared data area between bootloader and runtime firmware.
+ * Shared data area is allocated at the beginning of the RAM, it is overlapping
+ * with TF-M Secure code's MSP stack
+ */
+#define BOOT_TFM_SHARED_DATA_BASE S_RAM_ALIAS_BASE
+#define BOOT_TFM_SHARED_DATA_SIZE 0x400
+
+#endif /* __FLASH_LAYOUT_H__ */
