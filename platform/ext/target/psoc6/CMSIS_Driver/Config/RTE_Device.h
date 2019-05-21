4374f070aa5b42b8787fc0278cb7bc07fe3bc8cd
Chris Brand <chris.brand@cypress.com>
2019-05-21 11:41:25 -0700

    plat: Add PSoC6 USART driver
    Change-Id: I83512ba1200c8631390c9b006c68d5c54b299ca2
    Signed-off-by: Chris Brand <chris.brand@cypress.com>

--- platform/ext/target/psoc6/CMSIS_Driver/Config/RTE_Device.h
+++ platform/ext/target/psoc6/CMSIS_Driver/Config/RTE_Device.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2018-2019 Arm Limited. All rights reserved
+ * Copyright (c) 2019, Cypress Semiconductor Corporation. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,22 +22,22 @@
 
 // <e> MPC (Memory Protection Controller) [Driver_ISRAM0_MPC]
 // <i> Configuration settings for Driver_ISRAM0_MPC in component ::Drivers:MPC
-#define   RTE_ISRAM0_MPC                  1
+#define   RTE_ISRAM0_MPC                  0
 // </e> MPC (Memory Protection Controller) [Driver_ISRAM0_MPC]
 
 // <e> MPC (Memory Protection Controller) [Driver_ISRAM1_MPC]
 // <i> Configuration settings for Driver_SRAM1_MPC in component ::Drivers:MPC
-#define   RTE_ISRAM1_MPC                  1
+#define   RTE_ISRAM1_MPC                  0
 // </e> MPC (Memory Protection Controller) [Driver_ISRAM1_MPC]
 
 // <e> MPC (Memory Protection Controller) [Driver_ISRAM2_MPC]
 // <i> Configuration settings for Driver_ISRAM2_MPC in component ::Drivers:MPC
-#define   RTE_ISRAM2_MPC                  1
+#define   RTE_ISRAM2_MPC                  0
 // </e> MPC (Memory Protection Controller) [Driver_ISRAM2_MPC]
 
 // <e> MPC (Memory Protection Controller) [Driver_ISRAM3_MPC]
 // <i> Configuration settings for Driver_SRAM2_MPC in component ::Drivers:MPC
-#define   RTE_ISRAM3_MPC                  1
+#define   RTE_ISRAM3_MPC                  0
 // </e> MPC (Memory Protection Controller) [Driver_SRAM3_MPC]
 
 // <e> MPC (Memory Protection Controller) [Driver_CODE_SRAM_MPC]
@@ -46,28 +47,58 @@
 
 // <e> MPC (Memory Protection Controller) [Driver_QSPI_MPC]
 // <i> Configuration settings for Driver_QSPI_MPC in component ::Drivers:MPC
-#define   RTE_QSPI_MPC                    1
+#define   RTE_QSPI_MPC                    0
 // </e> MPC (Memory Protection Controller) [Driver_QSPI_MPC]
 
 // <e> MPC (Memory Protection Controller) [Driver_EFLASH0_MPC]
 // <i> Configuration settings for Driver_EFLASH0_MPC in component ::Drivers:MPC
-#define   RTE_EFLASH0_MPC                 1
+#define   RTE_EFLASH0_MPC                 0
 // </e> MPC (Memory Protection Controller) [Driver_EFLASH0_MPC]
 
 // <e> MPC (Memory Protection Controller) [Driver_EFLASH1_MPC]
 // <i> Configuration settings for Driver_EFLASH1_MPC in component ::Drivers:MPC
-#define   RTE_EFLASH1_MPC                 1
+#define   RTE_EFLASH1_MPC                 0
 // </e> MPC (Memory Protection Controller) [Driver_EFLASH1_MPC]
 
 // <e> USART (Universal synchronous - asynchronous receiver transmitter) [Driver_USART0]
 // <i> Configuration settings for Driver_USART0 in component ::Drivers:USART
-#define   RTE_USART0                      1
+#define   RTE_USART0                      0
 // </e> USART (Universal synchronous - asynchronous receiver transmitter) [Driver_USART0]
 
 // <e> USART (Universal synchronous - asynchronous receiver transmitter) [Driver_USART1]
 // <i> Configuration settings for Driver_USART1 in component ::Drivers:USART
-#define   RTE_USART1                      1
+#define   RTE_USART1                      0
 // </e> USART (Universal synchronous - asynchronous receiver transmitter) [Driver_USART1]
+
+// <e> USART (Universal synchronous - asynchronous receiver transmitter) [Driver_USART2]
+// <i> Configuration settings for Driver_USART2 in component ::Drivers:USART
+#define   RTE_USART2                      0
+// </e> USART (Universal synchronous - asynchronous receiver transmitter) [Driver_USART2]
+
+// <e> USART (Universal synchronous - asynchronous receiver transmitter) [Driver_USART3]
+// <i> Configuration settings for Driver_USART3 in component ::Drivers:USART
+#define   RTE_USART3                      0
+// </e> USART (Universal synchronous - asynchronous receiver transmitter) [Driver_USART3]
+
+// <e> USART (Universal synchronous - asynchronous receiver transmitter) [Driver_USART4]
+// <i> Configuration settings for Driver_USART4 in component ::Drivers:USART
+#define   RTE_USART4                      0
+// </e> USART (Universal synchronous - asynchronous receiver transmitter) [Driver_USART4]
+
+// <e> USART (Universal synchronous - asynchronous receiver transmitter) [Driver_USART5]
+// <i> Configuration settings for Driver_USART5 in component ::Drivers:USART
+#define   RTE_USART5                      1
+// </e> USART (Universal synchronous - asynchronous receiver transmitter) [Driver_USART5]
+
+// <e> USART (Universal synchronous - asynchronous receiver transmitter) [Driver_USART6]
+// <i> Configuration settings for Driver_USART6 in component ::Drivers:USART
+#define   RTE_USART6                      0
+// </e> USART (Universal synchronous - asynchronous receiver transmitter) [Driver_USART6]
+
+// <e> USART (Universal synchronous - asynchronous receiver transmitter) [Driver_USART7]
+// <i> Configuration settings for Driver_USART7 in component ::Drivers:USART
+#define   RTE_USART7                      0
+// </e> USART (Universal synchronous - asynchronous receiver transmitter) [Driver_USART7]
 
 // <e> PPC (Peripheral Protection Controller) [Driver_AHB_PPC0]
 // <i> Configuration settings for Driver_AHB_PPC0 in component ::Drivers:PPC
@@ -76,7 +107,7 @@
 
 // <e> PPC (Peripheral Protection Controller) [Driver_AHB_PPCEXP0]
 // <i> Configuration settings for Driver_AHB_PPCEXP0 in component ::Drivers:PPC
-#define   RTE_AHB_PPCEXP0                 1
+#define   RTE_AHB_PPCEXP0                 0
 // </e> PPC (Peripheral Protection Controller) [Driver_AHB_PPCEXP0]
 
 // <e> PPC (Peripheral Protection Controller) [Driver_AHB_PPCEXP1]
@@ -96,22 +127,22 @@
 
 // <e> PPC (Peripheral Protection Controller) [Driver_APB_PPC0]
 // <i> Configuration settings for Driver_APB_PPC0 in component ::Drivers:PPC
-#define   RTE_APB_PPC0                    1
+#define   RTE_APB_PPC0                    0
 // </e> PPC (Peripheral Protection Controller) [Driver_APB_PPC0]
 
 // <e> PPC (Peripheral Protection Controller) [Driver_APB_PPC1]
 // <i> Configuration settings for Driver_APB_PPC1 in component ::Drivers:PPC
-#define   RTE_APB_PPC1                    1
+#define   RTE_APB_PPC1                    0
 // </e> PPC (Peripheral Protection Controller) [Driver_APB_PPC1]
 
 // <e> PPC (Peripheral Protection Controller) [Driver_APB_PPCEXP0]
 // <i> Configuration settings for Driver_APB_PPCEXP0 in component ::Drivers:PPC
-#define   RTE_APB_PPCEXP0                 1
+#define   RTE_APB_PPCEXP0                 0
 // </e> PPC (Peripheral Protection Controller) [Driver_APB_PPCEXP0]
 
 // <e> PPC (Peripheral Protection Controller) [Driver_APB_PPCEXP1]
 // <i> Configuration settings for Driver_APB_PPCEXP1 in component ::Drivers:PPC
-#define   RTE_APB_PPCEXP1                 1
+#define   RTE_APB_PPCEXP1                 0
 // </e> PPC (Peripheral Protection Controller) [Driver_APB_PPCEXP1]
 
 // <e> PPC (Peripheral Protection Controller) [Driver_APB_PPCEXP2]
