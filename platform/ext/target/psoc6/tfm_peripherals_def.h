0f6c1825188d3a2b6b9214f6c2fd3c6da5ef77ef
David Hu <david.hu@arm.com>
2019-08-23 17:46:01 +0800

    Platform: Add a dummy TFM_PERIPHERAL_FPGA_IO definition on PSoC 6
    A test secure partition declares TFM_PERIPHERAL_FPGA_IO HW resource
    in its manifest. Define a dummy one on PSoC 6
    
    Change-Id: I57b92c16b87568c9e513b34ba44bd4026dbade6d
    Signed-off-by: David Hu <david.hu@arm.com>

--- platform/ext/target/psoc6/tfm_peripherals_def.h
+++ platform/ext/target/psoc6/tfm_peripherals_def.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, Arm Limited. All rights reserved.
+ * Copyright (c) 2018-2019, Arm Limited. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  *
@@ -15,5 +15,6 @@ extern struct tfm_spm_partition_platform_data_t tfm_peripheral_uart1;
 
 #define TFM_PERIPHERAL_STD_UART     (&tfm_peripheral_std_uart)
 #define TFM_PERIPHERAL_UART1        (&tfm_peripheral_uart1)
+#define TFM_PERIPHERAL_FPGA_IO      (0)
 
 #endif /* __TFM_PERIPHERALS_DEF_H__ */
