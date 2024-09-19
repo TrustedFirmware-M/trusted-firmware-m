5ecea445aec92ff006017cdbc1c48ab96bce11f0
Raef Coles <raef.coles@arm.com>
2024-09-19 16:03:20 +0100

    RSE: TC: Enable relocatable ROM by default on TC4
    So that it is tested in CI on at least one platform
    
    Change-Id: I133e21f8c86a09fa25126ab4f2c91ae8797280f5
    Signed-off-by: Raef Coles <raef.coles@arm.com>
    Signed-off-by: Jackson Cooper-Driver <jackson.cooper-driver@arm.com>

--- platform/ext/target/arm/rse/tc/tc4/config.cmake
+++ platform/ext/target/arm/rse/tc/tc4/config.cmake
@@ -5,14 +5,16 @@
 #
 #-------------------------------------------------------------------------------
 
-set(RSE_XIP                     ON      CACHE BOOL    "Whether to run runtime firmware XIP via the SIC")
+set(RSE_XIP                        ON    CACHE BOOL   "Whether to run runtime firmware XIP via the SIC")
 
-set(PLAT_MHU_VERSION            3       CACHE STRING  "Supported MHU version by platform")
+set(PLAT_MHU_VERSION               3     CACHE STRING "Supported MHU version by platform")
 
-set(RSE_USE_HOST_UART           OFF     CACHE BOOL    "Whether RSE should setup to use the UART from the host system")
-set(RSE_USE_LOCAL_UART          ON      CACHE BOOL    "Whether RSE should setup to use the UART local to the RSE subsystem")
+set(RSE_USE_HOST_UART              OFF   CACHE BOOL   "Whether RSE should setup to use the UART from the host system")
+set(RSE_USE_LOCAL_UART             ON    CACHE BOOL   "Whether RSE should setup to use the UART local to the RSE subsystem")
 
-set(TFM_ATTESTATION_SCHEME      "DPE"   CACHE STRING  "Attestation scheme to use [OFF, PSA, CCA, DPE]")
+set(TFM_ATTESTATION_SCHEME         "DPE" CACHE STRING "Attestation scheme to use [OFF, PSA, CCA, DPE]")
+
+set(RSE_SUPPORT_ROM_LIB_RELOCATION ON    CACHE BOOL   "Whether shared ROM code supports being copied to SRAM and then executed")
 
 # Once all options are set, set common options as fallback
 
