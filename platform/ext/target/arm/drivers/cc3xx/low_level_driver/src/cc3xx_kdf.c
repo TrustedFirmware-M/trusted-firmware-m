7b665e59dd2288c9c6ee465e243c0434f3a0e984
Raef Coles <raef.coles@arm.com>
2025-03-26 09:19:13 +0000

    CC3XX: Improve DPA resistance of KDF
    Pre-fill temporary buffer with random values to mask write power usage. Avoid
    second copy of key material.
    
    Change-Id: I66021cd2e59febbee7476e9ff62d2ecebe5b416b
    Signed-off-by: Raef Coles <raef.coles@arm.com>

--- platform/ext/target/arm/drivers/cc3xx/low_level_driver/src/cc3xx_kdf.c
+++ platform/ext/target/arm/drivers/cc3xx/low_level_driver/src/cc3xx_kdf.c
@@ -28,15 +28,15 @@ cc3xx_err_t cc3xx_lowlevel_kdf_cmac(
         (8 * out_length) & 0xFF};
     const uint8_t null_byte = 0;
     cc3xx_err_t err;
-    uint32_t cmac_buf[AES_TAG_MAX_LEN / sizeof(uint32_t)];
-    uint32_t k0[AES_TAG_MAX_LEN / sizeof(uint32_t)];
+    const size_t block_output_size = AES_TAG_MAX_LEN;
+    uint32_t k0[block_output_size / sizeof(uint32_t)];
     size_t output_idx;
 
     /* Check alignment */
 #ifdef CC3XX_CONFIG_STRICT_UINT32_T_ALIGNMENT
     assert(((uintptr_t)output_key & 0b11) == 0);
 #endif
-    assert((out_length / sizeof(cmac_buf)) != 0);
+    assert((out_length / block_output_size) != 0);
 
     err = cc3xx_lowlevel_aes_init(CC3XX_AES_DIRECTION_ENCRYPT, CC3XX_AES_MODE_CMAC,
                                   key_id, key, key_size, NULL, 0);
@@ -44,7 +44,12 @@ cc3xx_err_t cc3xx_lowlevel_kdf_cmac(
         goto out;
     }
 
-    cc3xx_lowlevel_aes_set_tag_len(AES_TAG_MAX_LEN);
+#ifdef CC3XX_CONFIG_DPA_MITIGATIONS_ENABLE
+    /* Pre-erase buffer with random values as a DPA countermeasure */
+    cc3xx_secure_erase_buffer(k0, sizeof(k0) / sizeof(uint32_t));
+#endif
+
+    cc3xx_lowlevel_aes_set_tag_len(block_output_size);
 
     /* K(0) = PRF(K_in, Label || 0x00 || Context || [L]2) */
     cc3xx_lowlevel_aes_update_authed_data(label, label_length);
@@ -54,7 +59,7 @@ cc3xx_err_t cc3xx_lowlevel_kdf_cmac(
 
     cc3xx_lowlevel_aes_finish(k0, NULL);
 
-    for(output_idx = 0; output_idx < out_length; output_idx += sizeof(cmac_buf)) {
+    for(output_idx = 0; output_idx < out_length; output_idx += block_output_size) {
         /* [i]2 is encoded on 4 bytes, i.e. r = 32, in binary. Assumes processor is LE */
         const uint8_t i_encoded[] = {
             (i_idx >> 24) & 0xFF,
@@ -78,10 +83,7 @@ cc3xx_err_t cc3xx_lowlevel_kdf_cmac(
         cc3xx_lowlevel_aes_update_authed_data(l_total_length, sizeof(l_total_length));
         cc3xx_lowlevel_aes_update_authed_data((uint8_t *)k0, sizeof(k0));
 
-        cc3xx_lowlevel_aes_finish(cmac_buf, NULL);
-
-        cc3xx_dpa_hardened_word_copy((void *)output_key + output_idx, cmac_buf,
-                                     sizeof(cmac_buf) / sizeof(uint32_t));
+        cc3xx_lowlevel_aes_finish((void *)output_key + output_idx, NULL);
         i_idx += 1;
     }
 
@@ -89,7 +91,6 @@ cc3xx_err_t cc3xx_lowlevel_kdf_cmac(
 
 out:
     cc3xx_secure_erase_buffer(k0, sizeof(k0) / sizeof(uint32_t));
-    cc3xx_secure_erase_buffer(cmac_buf, sizeof(cmac_buf) / sizeof(uint32_t));
 
     if (err != CC3XX_ERR_SUCCESS) {
         cc3xx_secure_erase_buffer(output_key, out_length / sizeof(uint32_t));
