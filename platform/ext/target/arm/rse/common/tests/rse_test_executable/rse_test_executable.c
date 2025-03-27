84de4e10a7d0caa479460653748088981bb1af1c
Jackson Cooper-Driver <jackson.cooper-driver@arm.com>
2025-03-27 14:47:35 +0000

    RSE: Tests: Configure r9 to point to GOT
    This is required when using relocatable ROM code. We need to configure
    the GOT to allow the test code to call into it successfully.
    
    Change-Id: I542d2fc66302c7d78e56033f8fffd89d57fd6284
    Signed-off-by: Jackson Cooper-Driver <jackson.cooper-driver@arm.com>

--- platform/ext/target/arm/rse/common/tests/rse_test_executable/rse_test_executable.c
+++ platform/ext/target/arm/rse/common/tests/rse_test_executable/rse_test_executable.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024, Arm Limited. All rights reserved.
+ * SPDX-FileCopyrightText: Copyright The TrustedFirmware-M Contributors
  *
  * SPDX-License-Identifier: BSD-3-Clause
  *
@@ -8,6 +8,7 @@
 #include <stdbool.h>
 
 #include "uart_stdout.h"
+#include "region_defs.h"
 
 #ifdef TEST_BL1_1
 #include "bl1_1_suites.h"
@@ -17,8 +18,24 @@
 #include "bl1_2_suites.h"
 #endif /* TEST_BL1_2 */
 
+#ifdef RSE_SUPPORT_ROM_LIB_RELOCATION
+static void setup_got_register(void)
+{
+    __asm volatile(
+        "mov r9, %0 \n"
+        "mov r2, %1 \n"
+        "lsl r9, #16 \n"
+        "orr r9, r9, r2 \n"
+        : : "I" (BL1_1_DATA_START >> 16), "I" (BL1_1_DATA_START & 0xFFFF) : "r2"
+    );
+}
+#endif /* RSE_SUPPORT_ROM_LIB_RELOCATION */
+
 int main(void)
 {
+#ifdef RSE_SUPPORT_ROM_LIB_RELOCATION
+    setup_got_register();
+#endif /* RSE_SUPPORT_ROM_LIB_RELOCATION */
     stdio_init();
 
 #ifdef TEST_BL1_2
