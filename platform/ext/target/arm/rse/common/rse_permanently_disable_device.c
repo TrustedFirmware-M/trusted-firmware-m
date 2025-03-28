65f3cf06e13481717b5d6d4ea7754a0bdef5f1a1
Raef Coles <raef.coles@arm.com>
2025-03-28 13:11:31 +0000

    RSE: Reserve bit 16 of device status for debug bit
    Change masks so that disable codes start at bit 17
    
    Change-Id: I0e8ad12aa5b2f106fea7504618f134bb73f459f0
    Signed-off-by: Raef Coles <raef.coles@arm.com>

--- platform/ext/target/arm/rse/common/rse_permanently_disable_device.c
+++ platform/ext/target/arm/rse/common/rse_permanently_disable_device.c
@@ -29,7 +29,7 @@ enum tfm_plat_err_t rse_permanently_disable_device(
         return err;
     }
 
-    device_status |= (fatal_error & 0xFFFF) << 16;
+    device_status |= (fatal_error & 0x7FFF) << 17;
 
     return tfm_plat_otp_write(PLAT_OTP_ID_DEVICE_STATUS, sizeof(device_status),
                               (uint8_t *)&device_status);
