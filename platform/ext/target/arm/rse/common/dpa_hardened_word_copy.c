f321f558d03e239ff9e4529718838fd71d69d5db
Amjad Ouled-Ameur <amjad.ouled-ameur@arm.com>
2025-03-31 16:35:49 +0100

    RSE: Remove dpa_hardened_word_copy size limit
    dpa_hardened_word_copy() is currently limited to 256
    words which is not enough for some particular cases.
    This improves the limit by performing a permutation
    based chunk copying.
    
    Signed-off-by: Raef Coles <raef.coles@arm.com>
    Signed-off-by: Amjad Ouled-Ameur <amjad.ouled-ameur@arm.com>
    Change-Id: I55c9d7b3dd2f696a9bb8803f475d0df18962cbe9

--- platform/ext/target/arm/rse/common/dpa_hardened_word_copy.c
+++ platform/ext/target/arm/rse/common/dpa_hardened_word_copy.c
@@ -11,31 +11,64 @@
 #include "device_definition.h"
 #include "cc3xx_rng.h"
 
-/**
- * \brief This is a requirement for the maximum number of words that can
- *        be copied through a single call to \ref cc3xx_dpa_hardened_word_copy
+#define ROUND_UP(x, bound) ((((x) + bound - 1) / bound) * bound)
+
+/*
+ * These can be tweaked depending on the memory and security requirements.
+ * CHUNK_WORD_SIZE = 32 and CHUNK_MAX_COUNT = 16 is enough to cover 2 KiB copy.
  */
-#define CC3XX_STDLIB_SECURE_COPY_MAX_WORDS (256)
+#define CHUNK_WORD_SIZE 32
+#define CHUNK_MAX_COUNT 16
 
 void dpa_hardened_word_copy(volatile uint32_t *dst,
-                            volatile const uint32_t *src, size_t word_count)
+                             volatile const uint32_t *src, size_t word_count)
 {
-    uint8_t permutation_buf[word_count]; /* This is a VLA */
+    static uint8_t permutation_buf[CHUNK_WORD_SIZE];
+    static uint8_t chunks_permutation_buf[CHUNK_MAX_COUNT];
     size_t idx;
 
-    /* We don't support more than 256 word permutations per copy, i.e. 2048 bit copy */
-    assert(word_count <= CC3XX_STDLIB_SECURE_COPY_MAX_WORDS);
+    assert(word_count <= CHUNK_WORD_SIZE * CHUNK_MAX_COUNT);
 
-    /* Initializes the permutation buffer */
-    for (idx = 0; idx < word_count; idx++) {
-        permutation_buf[idx] = idx;
+    if (!word_count) {
+        return;
     }
 
-    cc3xx_lowlevel_rng_get_random_permutation(permutation_buf, word_count,
-                                              CC3XX_RNG_FAST);
+    if (word_count > CHUNK_WORD_SIZE) {
+        /* Split src into chunks */
+        size_t chunks = ROUND_UP(word_count, CHUNK_WORD_SIZE) / CHUNK_WORD_SIZE;
+        size_t unaligned_words = word_count % CHUNK_WORD_SIZE;
 
-    for(idx = 0; idx < word_count; idx++) {
-        kmu_random_delay(&KMU_DEV_S, KMU_DELAY_LIMIT_32_CYCLES);
-        dst[permutation_buf[idx]] = src[permutation_buf[idx]];
+        /* Initializes the permutation buffer */
+        for (idx = 0; idx < chunks; idx++) {
+            chunks_permutation_buf[idx] = idx;
+        }
+
+        cc3xx_lowlevel_rng_get_random_permutation(chunks_permutation_buf,
+                                            chunks, CC3XX_RNG_FAST);
+
+        for (idx = 0; idx < chunks; idx++) {
+            size_t chunk_idx = chunks_permutation_buf[idx];
+            /* Handle a potential last unaligned chunk */
+            size_t copy_size = ((chunk_idx == chunks - 1) && !!unaligned_words) ?
+                                    unaligned_words : CHUNK_WORD_SIZE;
+            size_t copy_shift = chunk_idx * CHUNK_WORD_SIZE;
+
+            kmu_random_delay(&KMU_DEV_S, KMU_DELAY_LIMIT_32_CYCLES);
+            dpa_hardened_word_copy(dst + copy_shift,
+                                    src + copy_shift, copy_size);
+            }
+    } else {
+        /* Initializes the permutation buffer */
+        for (idx = 0; idx < word_count; idx++) {
+            permutation_buf[idx] = idx;
+        }
+
+        cc3xx_lowlevel_rng_get_random_permutation(permutation_buf, word_count,
+                                            CC3XX_RNG_FAST);
+
+        for(idx = 0; idx < word_count; idx++) {
+            kmu_random_delay(&KMU_DEV_S, KMU_DELAY_LIMIT_32_CYCLES);
+            dst[permutation_buf[idx]] = src[permutation_buf[idx]];
+        }
     }
 }
