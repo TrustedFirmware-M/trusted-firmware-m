1902c63f64ba89375fc014c9f936f794a3e65c67
Amjad Ouled-Ameur <amjad.ouled-ameur@arm.com>
2025-04-17 17:08:03 +0100

    cc3xx: support CMAC KDF key sizes not aligned to AES block size
    The CMAC KDF is currently limited to generating keys which size is a
    multiple of AES block size (16 bytes).
    
    This eliminates such limitation by allowing unaligned key sizes as long
    as they are 32-bit aligned, and comply with NIST SP800-38B which in this
    case means the last unaligned block should be at least 8 bytes long.
    
    This is tested for:
    - 128-bit key.
    - 192-bit key.
    - 224-bit key.
    - 256-bit key.
    
    Change-Id: I2d70e45208796ea0eb919d66765f7d8390c2807d
    Signed-off-by: Amjad Ouled-Ameur <amjad.ouled-ameur@arm.com>

--- platform/ext/target/arm/drivers/cc3xx/low_level_driver/src/cc3xx_kdf.c
+++ platform/ext/target/arm/drivers/cc3xx/low_level_driver/src/cc3xx_kdf.c
@@ -12,6 +12,8 @@
 
 #include <assert.h>
 
+#define ROUND_UP(x, bound) ((((x) + bound - 1) / bound) * bound)
+
 cc3xx_err_t cc3xx_lowlevel_kdf_cmac(
     cc3xx_aes_key_id_t key_id, const uint32_t *key,
     cc3xx_aes_keysize_t key_size,
@@ -19,24 +21,25 @@ cc3xx_err_t cc3xx_lowlevel_kdf_cmac(
     const uint8_t *context, size_t context_length,
     uint32_t *output_key, size_t out_length)
 {
-    uint32_t i_idx = 1;
     /* [L]2 is encoded as the length in bits, in binary. Safely assumes processor is LE */
+    const size_t out_length_bits = 8 * out_length;
     const uint8_t l_total_length[] = {
-        ((8 * out_length) >> 24) & 0xFF,
-        ((8 * out_length) >> 16) & 0xFF,
-        ((8 * out_length) >> 8) & 0xFF,
-        (8 * out_length) & 0xFF};
+        (out_length_bits >> 24) & 0xFF,
+        (out_length_bits >> 16) & 0xFF,
+        (out_length_bits >> 8) & 0xFF,
+         out_length_bits & 0xFF};
     const uint8_t null_byte = 0;
+    const size_t num_blocks = ROUND_UP(out_length, AES_TAG_MAX_LEN) / AES_TAG_MAX_LEN;
+    const size_t unaligned_block_size = out_length % AES_TAG_MAX_LEN;
+    uint32_t k0[AES_TAG_MAX_LEN / sizeof(uint32_t)];
     cc3xx_err_t err;
-    const size_t block_output_size = AES_TAG_MAX_LEN;
-    uint32_t k0[block_output_size / sizeof(uint32_t)];
-    size_t output_idx;
 
     /* Check alignment */
 #ifdef CC3XX_CONFIG_STRICT_UINT32_T_ALIGNMENT
     assert(((uintptr_t)output_key & 0b11) == 0);
 #endif
-    assert((out_length / block_output_size) != 0);
+    /* AES CMAC output tag is copied in words */
+    assert((out_length % sizeof(uint32_t)) == 0);
 
     err = cc3xx_lowlevel_aes_init(CC3XX_AES_DIRECTION_ENCRYPT, CC3XX_AES_MODE_CMAC,
                                   key_id, key, key_size, NULL, 0);
@@ -49,7 +52,7 @@ cc3xx_err_t cc3xx_lowlevel_kdf_cmac(
     cc3xx_secure_erase_buffer(k0, sizeof(k0) / sizeof(uint32_t));
 #endif
 
-    cc3xx_lowlevel_aes_set_tag_len(block_output_size);
+    cc3xx_lowlevel_aes_set_tag_len(AES_TAG_MAX_LEN);
 
     /* K(0) = PRF(K_in, Label || 0x00 || Context || [L]2) */
     cc3xx_lowlevel_aes_update_authed_data(label, label_length);
@@ -59,8 +62,12 @@ cc3xx_err_t cc3xx_lowlevel_kdf_cmac(
 
     cc3xx_lowlevel_aes_finish(k0, NULL);
 
-    for(output_idx = 0; output_idx < out_length; output_idx += block_output_size) {
+    for (size_t idx = 0; idx < num_blocks; idx++) {
+        const uint8_t *output_key_offset = ((uint8_t *) output_key) + idx * AES_TAG_MAX_LEN;
+        const size_t aes_tag_size = ((idx == num_blocks - 1) && (unaligned_block_size > 0)) ?
+                                    unaligned_block_size : AES_TAG_MAX_LEN;
         /* [i]2 is encoded on 4 bytes, i.e. r = 32, in binary. Assumes processor is LE */
+        const size_t i_idx = idx + 1;
         const uint8_t i_encoded[] = {
             (i_idx >> 24) & 0xFF,
             (i_idx >> 16) & 0xFF,
@@ -73,7 +80,7 @@ cc3xx_err_t cc3xx_lowlevel_kdf_cmac(
             goto out;
         }
 
-        cc3xx_lowlevel_aes_set_tag_len(AES_TAG_MAX_LEN);
+        cc3xx_lowlevel_aes_set_tag_len(aes_tag_size);
 
         /* K(i) = PRF(K_in, [i]2 || Label || 0x00 || Context || [L]2 || K(0)) */
         cc3xx_lowlevel_aes_update_authed_data(i_encoded, sizeof(i_encoded));
@@ -83,8 +90,7 @@ cc3xx_err_t cc3xx_lowlevel_kdf_cmac(
         cc3xx_lowlevel_aes_update_authed_data(l_total_length, sizeof(l_total_length));
         cc3xx_lowlevel_aes_update_authed_data((uint8_t *)k0, sizeof(k0));
 
-        cc3xx_lowlevel_aes_finish((void *)output_key + output_idx, NULL);
-        i_idx += 1;
+        cc3xx_lowlevel_aes_finish((void *) output_key_offset, NULL);
     }
 
     err = CC3XX_ERR_SUCCESS;
