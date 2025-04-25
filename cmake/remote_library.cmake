1e77550712f35730e8807fd1091fdac243d1b15a
Anton Komlev <anton.komlev@arm.com>
2025-04-25 16:37:55 +0100

    Build: Update method for remote content fetching
    FetchContent_Populate() is deprecated as of CMake 3.30. This change
    updates the build system to use FetchContent_MakeAvailable() instead,
    following the latest CMake guidelines.
    
    Signed-off-by: Anton Komlev <anton.komlev@arm.com>
    Change-Id: I70bfa9cd8c793bba6dfb19c2aaa7efe26abf8592

--- cmake/remote_library.cmake
+++ cmake/remote_library.cmake
@@ -1,7 +1,5 @@
 #-------------------------------------------------------------------------------
-# Copyright (c) 2022 Cypress Semiconductor Corporation (an Infineon company)
-# or an affiliate of Cypress Semiconductor Corporation. All rights reserved.
-# Copyright (c) 2023 Arm Limited. All rights reserved.
+# SPDX-FileCopyrightText: Copyright The TrustedFirmware-M Contributors#
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
@@ -11,126 +9,6 @@ include(FetchContent)
 set(FETCHCONTENT_QUIET FALSE)
 
 find_package(Git)
-
-# This function applies patches if they are not applied yet.
-# It assumes that patches have not been applied if it's not possible to revert them.
-#
-# WORKING_DIRECTORY - working directory where patches should be applied.
-# PATCH_FILES - list of patches. Patches will be applied in alphabetical order.
-function(apply_patches WORKING_DIRECTORY PATCH_FILES)
-    # Validate if patches are already applied by reverting patches in reverse order
-    # Step 1 - keep changes in stash with random message/name to detect
-    # that stash has been created by git
-    string(RANDOM LENGTH 16 STASH_NAME)
-    set(STASH_NAME "tfm-remote_library-apply_patches-${STASH_NAME}")
-    execute_process(COMMAND "${GIT_EXECUTABLE}" stash push -u -m "${STASH_NAME}"
-        WORKING_DIRECTORY ${WORKING_DIRECTORY}
-        RESULT_VARIABLE VALIDATION_STATUS
-        ERROR_QUIET OUTPUT_QUIET
-    )
-    # Step 2 - get list of stashes to validate that stash has been created
-    if (VALIDATION_STATUS EQUAL 0)
-        execute_process(COMMAND "${GIT_EXECUTABLE}" stash list
-            WORKING_DIRECTORY ${WORKING_DIRECTORY}
-            OUTPUT_VARIABLE STASH_LIST
-            RESULT_VARIABLE VALIDATION_STATUS
-            ERROR_QUIET
-        )
-        # Look for stash message to detect stash creation
-        string(FIND "${STASH_LIST}" "${STASH_NAME}" STASH_INDEX)
-        if (STASH_INDEX LESS 0)
-            # Stash is not created, most probably because there is no changes
-            set(VALIDATION_STATUS 0)
-        else()
-            # Step 3 - restore changes with git stash apply
-            if (VALIDATION_STATUS EQUAL 0)
-                execute_process(COMMAND "${GIT_EXECUTABLE}" stash apply
-                    WORKING_DIRECTORY ${WORKING_DIRECTORY}
-                    RESULT_VARIABLE VALIDATION_STATUS
-                    ERROR_QUIET OUTPUT_QUIET
-                )
-            endif()
-        endif()
-    endif()
-    # Step 4 - revert patches in reverse order
-    if (VALIDATION_STATUS EQUAL 0)
-        # Sort list of patches in descending order for validation
-        list(SORT PATCH_FILES ORDER DESCENDING)
-        foreach(PATCH ${PATCH_FILES})
-            execute_process(COMMAND "${GIT_EXECUTABLE}" apply --reverse --verbose "${PATCH}"
-                WORKING_DIRECTORY ${WORKING_DIRECTORY}
-                RESULT_VARIABLE VALIDATION_STATUS
-                ERROR_QUIET OUTPUT_QUIET
-            )
-            if (NOT VALIDATION_STATUS EQUAL 0)
-                # patch failed to be applied, assume that we need to restore and
-                # apply all patch set
-                break()
-            endif()
-        endforeach()
-    endif()
-    # Step 5 - pop stash to restore original state
-    if (STASH_INDEX GREATER_EQUAL 0)
-        # Clear index before restore
-        execute_process(COMMAND "${GIT_EXECUTABLE}" clean -df
-            WORKING_DIRECTORY ${WORKING_DIRECTORY}
-            ERROR_QUIET OUTPUT_QUIET
-        )
-        execute_process(COMMAND "${GIT_EXECUTABLE}" reset --hard
-            WORKING_DIRECTORY ${WORKING_DIRECTORY}
-            ERROR_QUIET OUTPUT_QUIET
-        )
-        execute_process(COMMAND "${GIT_EXECUTABLE}" stash pop --index
-            WORKING_DIRECTORY ${WORKING_DIRECTORY}
-            ERROR_QUIET OUTPUT_QUIET
-        )
-    else()
-        # There is no stash, restore commit by clearing index
-        execute_process(COMMAND "${GIT_EXECUTABLE}" clean -df
-            WORKING_DIRECTORY ${WORKING_DIRECTORY}
-            ERROR_QUIET OUTPUT_QUIET
-        )
-        execute_process(COMMAND "${GIT_EXECUTABLE}" reset --hard
-            WORKING_DIRECTORY ${WORKING_DIRECTORY}
-            ERROR_QUIET OUTPUT_QUIET
-        )
-    endif()
-
-    if (NOT VALIDATION_STATUS EQUAL 0)
-        # Validation has been failed, so we assume that patches should be applied
-        # Sort list of patches in ascending order
-        list(SORT PATCH_FILES ORDER ASCENDING)
-
-        set(EXECUTE_COMMAND "${GIT_EXECUTABLE}" apply --verbose ${PATCH_FILES})
-        execute_process(COMMAND ${EXECUTE_COMMAND}
-            WORKING_DIRECTORY ${WORKING_DIRECTORY}
-            RESULT_VARIABLE PATCH_STATUS
-            COMMAND_ECHO STDOUT
-        )
-        if (NOT PATCH_STATUS EQUAL 0)
-            message( FATAL_ERROR "Failed to apply patches at ${WORKING_DIRECTORY}" )
-        endif()
-    endif()
-endfunction()
-
-
-# Returns a repository URL and a reference to the commit used to checkout the repository.
-#
-# REPO_URL_VAR - name of variable which receives repository URL.
-# TAG_VAR - name of variable which receives reference to commit.
-function(_get_fetch_remote_properties REPO_URL_VAR TAG_VAR)
-    # Parse arguments
-    set(options "")
-    set(oneValueArgs GIT_REPOSITORY GIT_TAG)
-    set(multiValueArgs "")
-    cmake_parse_arguments(PARSE_ARGV 2 ARG "${options}" "${oneValueArgs}" "${multiValueArgs}")
-
-    if (ARG_GIT_REPOSITORY)
-        set(${REPO_URL_VAR} ${ARG_GIT_REPOSITORY} PARENT_SCOPE)
-        set(${TAG_VAR} ${ARG_GIT_TAG} PARENT_SCOPE)
-    endif()
-endfunction()
-
 
 # This function helps to handle options with an empty string values.
 # There is a feature/bug in CMake that result in problem with the empty string arguments.
@@ -238,7 +116,6 @@ function(fetch_remote_library)
     endif()
 
     if ("${${ARG_LIB_SOURCE_PATH_VAR}}" STREQUAL "DOWNLOAD")
-        set(SOURCE_PATH_IS_DOWNLOAD TRUE)
         # Process arguments which can be an empty string
         # There is a feature/bug in CMake that result in problem with empty string arguments
         # See https://gitlab.kitware.com/cmake/cmake/-/issues/16341 for details
@@ -254,37 +131,24 @@ function(fetch_remote_library)
             message(FATAL_ERROR "fetch_remote_library: Unexpected empty string value for ${EMPTY_KEY}. "
                                 "Please, validate arguments or update fetch_remote_library to support empty value for ${EMPTY_KEY}!!!")
         endif()
-
-        # Content fetching
-        FetchContent_Declare(${ARG_LIB_NAME}
-            ${ARG_FETCH_CONTENT_ARGS}
-            "${GIT_SUBMODULES}"      "${GIT_SUBMODULES_VALUE}"
-        )
-
-        FetchContent_GetProperties(${ARG_LIB_NAME})
-        if(NOT ${ARG_LIB_NAME}_POPULATED)
-            FetchContent_Populate(${ARG_LIB_NAME})
-
-            # Get remote properties
-            _get_fetch_remote_properties(REPO_URL_VAR TAG_VAR ${ARG_FETCH_CONTENT_ARGS})
-            set(${ARG_LIB_SOURCE_PATH_VAR} ${${ARG_LIB_NAME}_SOURCE_DIR} CACHE PATH "Library has been downloaded from ${REPO_URL_VAR}, tag ${TAG_VAR}" FORCE)
-            if (DEFINED ARG_LIB_BINARY_PATH_VAR)
-                set(${ARG_LIB_BINARY_PATH_VAR} ${${ARG_LIB_NAME}_BINARY_DIR} CACHE PATH "Path to build directory of ${ARG_LIB_NAME}")
-            endif()
-        endif()
+    else()
+        string(TOUPPER ${ARG_LIB_NAME} ARG_LIB_NAME_UPCASE)
+        set(FETCHCONTENT_SOURCE_DIR_${ARG_LIB_NAME_UPCASE} ${${ARG_LIB_SOURCE_PATH_VAR}})
     endif()
 
-    if (ARG_LIB_FORCE_PATCH)
-        set(FORCE_PATCH ${ARG_LIB_FORCE_PATCH})
+    # list all available patches in absolute names
+    file(GLOB PATCH_FILES ${ARG_LIB_PATCH_DIR}/*.patch LIST_DIRECTORIES true)
+
+    if(PATCH_FILES)
+        set(PATCH_COMMAND PATCH_COMMAND ${GIT_EXECUTABLE} am --ignore-date ${PATCH_FILES})
     endif()
 
-    if (ARG_LIB_PATCH_DIR AND (SOURCE_PATH_IS_DOWNLOAD OR FORCE_PATCH))
-        # look for patch files
-        file(GLOB PATCH_FILES "${ARG_LIB_PATCH_DIR}/*.patch")
-
-        if(PATCH_FILES)
-            # Apply patches for existing sources
-            apply_patches("${${ARG_LIB_SOURCE_PATH_VAR}}" "${PATCH_FILES}")
-        endif()
-    endif()
+    # Content fetching
+    FetchContent_Declare(${ARG_LIB_NAME}
+        ${ARG_FETCH_CONTENT_ARGS}
+        "${GIT_SUBMODULES}"      "${GIT_SUBMODULES_VALUE}"
+        ${PATCH_COMMAND}
+    )
+    FetchContent_MakeAvailable(${ARG_LIB_NAME})
+    set(${ARG_LIB_SOURCE_PATH_VAR} ${${ARG_LIB_NAME}_SOURCE_DIR} CACHE PATH "Library has been downloaded from ${REPO_URL_VAR}, tag ${TAG_VAR}" FORCE)
 endfunction()
