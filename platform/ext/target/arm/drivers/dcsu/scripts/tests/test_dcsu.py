6d82bc23f4e1778ad138f4412d53787f5716d07c
Jackson Cooper-Driver <jackson.cooper-driver@arm.com>
2025-05-13 11:12:30 +0100

    DCSU: Add test cases for SoC Area
    Add additional test cases to the existing DCSU test suite which verify
    the writing to and reading from the SoC Area. These tests verify that
    the generate SoC ID command works correctly and that we can write and
    read valid values to the SoC family ID and IEEE ECID. There are also
    tests cases for the SoC configuration area handling.
    
    Change-Id: I161ffe9b9bf14cb698992a453af6f21c63f329fa
    Signed-off-by: Jackson Cooper-Driver <jackson.cooper-driver@arm.com>

--- platform/ext/target/arm/drivers/dcsu/scripts/tests/test_dcsu.py
+++ platform/ext/target/arm/drivers/dcsu/scripts/tests/test_dcsu.py
@@ -15,11 +15,16 @@ sys.path.append(parent_dir)
 
 from dcsu import *
 
-WORD_SIZE = 4
-TEST_DATA_BYTES = b"\xAB\xCD\xEF\x10\x01\x23\x45\x67" * 128
-
 def get_random_bytes(num_bytes):
     return os.urandom(num_bytes)
+
+WORD_SIZE = 4
+
+TEST_DATA_BYTES = b"\xAB\xCD\xEF\x10\x01\x23\x45\x67" * 128
+
+SOC_FAMILY_ID = b"\xDE\xAD\xBE\xEF"
+IEEE_ECID = b"\xFE\xED\xC0\xDE\xFE\xED\xC0\xDE\xFE\xED\xC0\xDE\xFE\xED\xC0\xDE"
+SOC_CONFIG_DATA = get_random_bytes(0x100)
 
 def setup_args(args, data=None, data_file=None, offset=0x0, byte_order='little'):
     assert (data is not None) ^ (data_file is not None)
@@ -34,6 +39,82 @@ def setup_args(args, data=None, data_file=None, offset=0x0, byte_order='little')
 def test_initial_import_ready(backend, ctx, args):
     status = dcsu_command(backend, ctx, dcsu_rx_command.DCSU_RX_COMMAND_IMPORT_READY, args)
     return status == 0
+
+def test_generate_soc_uid(backend, ctx, args):
+    status = dcsu_command(backend, ctx, dcsu_tx_command.DCSU_TX_COMMAND_GENERATE_SOC_UNIQUE_ID, args)
+    if status != dcsu_tx_message_error.DCSU_TX_MSG_RESP_SUCCESS:
+        return False
+    status = dcsu_command(backend, ctx, dcsu_tx_command.DCSU_TX_COMMAND_GENERATE_SOC_UNIQUE_ID, args)
+    return status == dcsu_tx_message_error.DCSU_TX_MSG_RESP_OTP_ALREADY_WRITTEN
+
+def test_write_soc_family_id(backend, ctx, args):
+    setup_args(args, SOC_FAMILY_ID, None, 0x0)
+    status = dcsu_command(backend, ctx, dcsu_tx_command.DCSU_TX_COMMAND_WRITE_SOC_FAMILY_ID, args)
+    if status != dcsu_tx_message_error.DCSU_TX_MSG_RESP_SUCCESS:
+        return False
+    status, data = dcsu_command(backend, ctx, dcsu_tx_command.DCSU_TX_COMMAND_READ_SOC_FAMILY_ID, args)
+    if status != dcsu_tx_message_error.DCSU_TX_MSG_RESP_SUCCESS:
+        return False
+    if data != SOC_FAMILY_ID:
+        return False
+    return True
+
+def test_write_ieee_ecid(backend, ctx, args):
+    setup_args(args, IEEE_ECID, None, 0x0)
+    status = dcsu_command(backend, ctx, dcsu_tx_command.DCSU_TX_COMMAND_WRITE_SOC_IEEE_ECID, args)
+    if status != dcsu_tx_message_error.DCSU_TX_MSG_RESP_SUCCESS:
+        return False
+    status, data = dcsu_command(backend, ctx, dcsu_tx_command.DCSU_TX_COMMAND_READ_SOC_IEEE_ECID, args)
+    if status != dcsu_tx_message_error.DCSU_TX_MSG_RESP_SUCCESS:
+        return False
+    if data != IEEE_ECID:
+        return False
+    return True
+
+def test_write_soc_id_fields_zc(backend, ctx, args):
+    status = dcsu_command(backend, ctx, dcsu_tx_command.DCSU_TX_COMMAND_COMPUTE_ZC_SOC_IDS, args)
+    return status == dcsu_tx_message_error.DCSU_TX_MSG_RESP_SUCCESS
+
+def test_soc_config_area_with_too_large_offset(backend, ctx, args):
+    # Current RSE_OTP_SOC_RESERVED_SIZE is 0x12D4
+    setup_args(args, get_random_bytes(4), None, 0x12D4 + 4)
+    status = dcsu_command(backend, ctx, dcsu_tx_command.DCSU_TX_COMMAND_WRITE_SOC_CONFIG_DATA, args)
+    return status == dcsu_tx_message_error.DCSU_TX_MSG_RESP_TOO_LARGE_OFFSET_PARAM
+
+def write_soc_config_data(backend, ctx, args, data):
+    for i in range(0, len(data), 16):
+        setup_args(args, data[i:i+16], None, i)
+        status = dcsu_command(backend, ctx, dcsu_tx_command.DCSU_TX_COMMAND_WRITE_SOC_CONFIG_DATA, args)
+        if status != dcsu_tx_message_error.DCSU_TX_MSG_RESP_SUCCESS:
+            return status
+    return status
+
+def test_write_valid_soc_config_area(backend, ctx, args):
+    status = write_soc_config_data(backend, ctx, args, SOC_CONFIG_DATA)
+    return status == dcsu_tx_message_error.DCSU_TX_MSG_RESP_SUCCESS
+
+def test_second_write_soc_config_area(backend, ctx, args):
+    status = write_soc_config_data(backend, ctx, args, get_random_bytes(0x100))
+    return status == dcsu_tx_message_error.DCSU_TX_MSG_RESP_OTP_ALREADY_WRITTEN
+
+def test_write_soc_config_area_zc(backend, ctx, args):
+    status = dcsu_command(backend, ctx, dcsu_tx_command.DCSU_TX_COMMAND_COMPUTE_ZC_SOC_CFG, args)
+    return status == dcsu_tx_message_error.DCSU_TX_MSG_RESP_SUCCESS
+
+def test_read_soc_config_area_values(backend, ctx, args):
+    args.size = str(4)
+    data = bytes(0)
+    for i in range(0, len(SOC_CONFIG_DATA), 16):
+        args.offset = str(i)
+        status, tmp_data = dcsu_command(backend, ctx, dcsu_tx_command.DCSU_TX_COMMAND_READ_SOC_CONFIG_DATA, args)
+        if status != dcsu_tx_message_error.DCSU_TX_MSG_RESP_SUCCESS:
+            return False
+        data += tmp_data
+
+    if data != SOC_CONFIG_DATA:
+        return False
+
+    return True
 
 def test_data_with_checksum(backend, ctx, args):
     setup_args(args, get_random_bytes(28), None, 0x0)
@@ -137,7 +218,31 @@ tests_0001 = [
     ("Initial IMPORT_READY command", test_initial_import_ready)
 ]
 
-tests_0002 = [
+tests_0100 = [
+    ("Generate SoC unique ID", test_generate_soc_uid)
+]
+
+tests_0101 = [
+    ("Write SoC family ID", test_write_soc_family_id)
+]
+
+tests_0102 = [
+    ("Write IEEE ECID", test_write_ieee_ecid)
+]
+
+tests_0103 = [
+    ("Trigger SoC ID fields zero count command", test_write_soc_id_fields_zc)
+]
+
+tests_0104 = [
+    ("SoC config area with too large offset", test_soc_config_area_with_too_large_offset),
+    ("Write valid SoC config area", test_write_valid_soc_config_area),
+    ("Attempt second SoC config area write", test_second_write_soc_config_area),
+    ("Trigger SoC config area zero count write", test_write_soc_config_area_zc),
+    ("Read SoC config area and check values", test_read_soc_config_area_values),
+]
+
+tests_0200 = [
     ("Data with checksum", test_data_with_checksum),
     ("Data without checksum", test_data_without_checksum),
     ("Data without checksum overwrite buffer", test_data_without_checksum_outside_buffer),
@@ -148,11 +253,11 @@ tests_0002 = [
     ("Invalid checksum", test_invalid_checksum),
 ]
 
-tests_0003 = [
+tests_0201 = [
     ("Specific data with checksum", test_specific_data_with_checksum),
 ]
 
-tests_0004 = [
+tests_0202 = [
     ("Specific data in file with checksum", test_specific_file_with_checksum),
 ]
 
@@ -162,9 +267,10 @@ tests_send_completion = [
 ]
 
 tests_defs = tests_0001 + \
-            tests_0002 + tests_send_completion + \
-            tests_0003 + tests_send_completion + \
-            tests_0004 + tests_send_completion
+            tests_0100 + tests_0101 + tests_0102 + tests_0103 + tests_0104 + \
+            tests_0200 + tests_send_completion + \
+            tests_0201 + tests_send_completion + \
+            tests_0202 + tests_send_completion
 
 def test_fail(name):
     print("\033[31m")
--- platform/ext/target/arm/drivers/dcsu/tests/test_dcsu_drv.c
+++ platform/ext/target/arm/drivers/dcsu/tests/test_dcsu_drv.c
@@ -13,6 +13,8 @@
 #include "dcsu_drv.h"
 #include "test_dcsu_drv.h"
 #include "device_definition.h"
+#include "rse_otp_dev.h"
+#include "rse_zero_count.h"
 
 #define TEST_ASSERT(cond, msg) \
     if (!(cond)) {             \
@@ -24,17 +26,19 @@
 #define EXPECTED_DATA_BYTES "\xAB\xCD\xEF\x10\x01\x23\x45\x67"
 #define EXPECTED_DATA_BYTES_SIZE (sizeof(EXPECTED_DATA_BYTES) - 1)
 
+#define SOC_FAMILY_ID_BYTES "\xDE\xAD\xBE\xEF"
+#define IEEE_ECID_BYTES "\xFE\xED\xC0\xDE\xFE\xED\xC0\xDE\xFE\xED\xC0\xDE\xFE\xED\xC0\xDE"
+
 static uint8_t rx_recv_buffer[1024];
 
-static enum dcsu_error_t process_commands_until_complete(void)
+static enum dcsu_error_t process_multiple_commands(enum dcsu_rx_command command)
 {
     enum dcsu_error_t err;
     bool got_complete = false;
 
     while (!got_complete) {
         TEST_LOG("Waiting for command...\n");
-        if (dcsu_wait_for_rx_command(&DCSU_DEV_S, DCSU_RX_COMMAND_COMPLETE_IMPORT_DATA) ==
-            DCSU_ERROR_NONE) {
+        if (dcsu_wait_for_rx_command(&DCSU_DEV_S, command) == DCSU_ERROR_NONE) {
             got_complete = true;
         }
 
@@ -45,6 +49,11 @@ static enum dcsu_error_t process_commands_until_complete(void)
     }
 
     return DCSU_ERROR_NONE;
+}
+
+static enum dcsu_error_t process_commands_until_complete(void)
+{
+    return process_multiple_commands(DCSU_RX_COMMAND_COMPLETE_IMPORT_DATA);
 }
 
 static void test_teardown(void)
@@ -64,7 +73,132 @@ static void dcsu_drv_test_0001(struct test_result_t *ret)
     test_teardown();
 }
 
-static void dcsu_drv_test_0002(struct test_result_t *ret)
+static enum dcsu_error_t wait_run_command(enum dcsu_rx_command command)
+{
+    enum dcsu_error_t dcsu_err;
+
+    dcsu_err = dcsu_wait_for_rx_command(&DCSU_DEV_S, command);
+    if (dcsu_err != DCSU_ERROR_NONE) {
+        return dcsu_err;
+    }
+
+    return dcsu_handle_rx_command(&DCSU_DEV_S);
+}
+
+static enum lcm_error_t check_otp_area_correct(uint32_t offset, size_t len, uint8_t *buf,
+                                               bool *correct)
+{
+    uint8_t read_buf[len];
+    enum lcm_error_t lcm_err;
+
+    lcm_err = lcm_otp_read(&LCM_DEV_S, offset, len, read_buf);
+    if (lcm_err != LCM_ERROR_NONE) {
+        return lcm_err;
+    }
+
+    for (unsigned i = 0; i < len; i++) {
+        if (buf != NULL) {
+            if (read_buf[i] != buf[i]) {
+                *correct = false;
+                return LCM_ERROR_NONE;
+            }
+        } else {
+            if (read_buf[i] == 0) {
+                *correct = false;
+                return LCM_ERROR_NONE;
+            }
+        }
+    }
+
+    *correct = true;
+    return LCM_ERROR_NONE;
+}
+
+static void dcsu_drv_test_0100(struct test_result_t *ret)
+{
+    bool correct_otp_val;
+
+    /* Generate SoC Unique ID */
+    TEST_ASSERT(wait_run_command(DCSU_RX_COMMAND_GENERATE_SOC_UNIQUE_ID) == DCSU_ERROR_NONE,
+                "Failed to handle SoC UID command");
+
+    TEST_ASSERT(check_otp_area_correct(OTP_OFFSET(P_RSE_OTP_SOC->soc_id_area.unique_id),
+                                       sizeof(P_RSE_OTP_SOC->soc_id_area.unique_id), NULL,
+                                       &correct_otp_val) == LCM_ERROR_NONE,
+                "Failed to read SoC UID");
+    TEST_ASSERT(correct_otp_val, "SoC UID value incorrect");
+
+    /* Second attempted write should succeed here but return failure to the host */
+    TEST_ASSERT(wait_run_command(DCSU_RX_COMMAND_GENERATE_SOC_UNIQUE_ID) == DCSU_ERROR_NONE,
+                "Failed to handle SoC UID command");
+}
+
+static void test_write_read_otp_field(struct test_result_t *ret, enum dcsu_rx_command write_command,
+                                      enum dcsu_rx_command read_command, uint32_t offset,
+                                      size_t len, uint8_t *buf)
+{
+    bool correct_otp_val;
+
+    TEST_ASSERT(wait_run_command(write_command) == DCSU_ERROR_NONE,
+                "Failed to handle write command");
+
+    TEST_ASSERT(check_otp_area_correct(offset, len, buf, &correct_otp_val) == LCM_ERROR_NONE,
+                "Failed to read OTP field");
+    TEST_ASSERT(correct_otp_val, "OTP field value incorrect");
+
+    /* Allow host to read back value to confirm */
+    TEST_ASSERT(wait_run_command(read_command) == DCSU_ERROR_NONE, "Failed to handle read command");
+}
+
+static void dcsu_drv_test_0101(struct test_result_t *ret)
+{
+    test_write_read_otp_field(ret, DCSU_RX_COMMAND_WRITE_SOC_FAMILY_ID,
+                              DCSU_RX_COMMAND_READ_SOC_FAMILY_ID,
+                              OTP_OFFSET(P_RSE_OTP_SOC->soc_id_area.family_id),
+                              sizeof(P_RSE_OTP_SOC->soc_id_area.family_id),
+                              (uint8_t *)SOC_FAMILY_ID_BYTES);
+}
+
+static void dcsu_drv_test_0102(struct test_result_t *ret)
+{
+    test_write_read_otp_field(ret, DCSU_RX_COMMAND_WRITE_SOC_IEEE_ECID,
+                              DCSU_RX_COMMAND_READ_SOC_IEEE_ECID,
+                              OTP_OFFSET(P_RSE_OTP_SOC->soc_id_area.ieee_ecid),
+                              sizeof(P_RSE_OTP_SOC->soc_id_area.ieee_ecid),
+                              (uint8_t *)IEEE_ECID_BYTES);
+}
+
+static void dcsu_drv_test_0103(struct test_result_t *ret)
+{
+    TEST_ASSERT(wait_run_command(DCSU_RX_COMMAND_COMPUTE_ZC_SOC_IDS) == DCSU_ERROR_NONE,
+                "Failed to handle compute SoC ID zero count command");
+
+    TEST_ASSERT(rse_check_zero_bit_count((uint8_t *)&P_RSE_OTP_SOC->soc_id_area.family_id,
+                                         sizeof(P_RSE_OTP_SOC->soc_id_area.family_id) +
+                                             sizeof(P_RSE_OTP_SOC->soc_id_area.ieee_ecid),
+                                         P_RSE_OTP_SOC->soc_id_area.zero_count_id) ==
+                    TFM_PLAT_ERR_SUCCESS,
+                "SoC Config area zero count failed");
+}
+
+static void dcsu_drv_test_0104(struct test_result_t *ret)
+{
+    /* Allow host to test multiple commands */
+    TEST_ASSERT(process_multiple_commands(DCSU_RX_COMMAND_READ_SOC_CONFIG_DATA) == DCSU_ERROR_NONE,
+                "Command processing should have succeeded!");
+
+    /* Host should have called the write zero count command, ensure that the
+     * value written is correct here */
+    TEST_ASSERT(rse_check_zero_bit_count(
+                    (uint8_t *)((uintptr_t)&P_RSE_OTP_SOC->soc_cfg_area.soc_cfg_data_zc +
+                                sizeof(P_RSE_OTP_SOC->soc_cfg_area.soc_cfg_data_zc)),
+                    sizeof(P_RSE_OTP_SOC->soc_cfg_area) -
+                        sizeof(P_RSE_OTP_SOC->soc_cfg_area.soc_cfg_data_zc),
+                    P_RSE_OTP_SOC->soc_cfg_area.soc_cfg_data_zc) == TFM_PLAT_ERR_SUCCESS,
+                "SoC Config area zero count failed");
+}
+
+static void dcsu_drv_test_0202(struct test_result_t *ret)
 {
     /* Spin in command loop and process until the script indicates that
      * we should move to the next test (using COMPLETE)*/
@@ -93,23 +227,30 @@ static void dcsu_drv_test_receieve_check_data(struct test_result_t *ret)
     test_teardown();
 }
 
-static void dcsu_drv_test_0003(struct test_result_t *ret)
+static void dcsu_drv_test_0203(struct test_result_t *ret)
 {
     /* Receive direct data */
     dcsu_drv_test_receieve_check_data(ret);
 }
 
-static void dcsu_drv_test_0004(struct test_result_t *ret)
+static void dcsu_drv_test_0204(struct test_result_t *ret)
 {
     /* Receive data passed to script from file */
     dcsu_drv_test_receieve_check_data(ret);
 }
 
 static struct test_t dcsu_drv_tests[] = {
-    { &dcsu_drv_test_0001, "DCSU_DRV_TEST_001", "Initial DCSU driver setup and wait for signal" },
-    { &dcsu_drv_test_0002, "DCSU_DRV_TEST_002", "Process arbitrary commands until COMPLETE" },
-    { &dcsu_drv_test_0003, "DCSU_DRV_TEST_003", "Receieve data until COMPLETE and verify" },
-    { &dcsu_drv_test_0004, "DCSU_DRV_TEST_004",
+    { &dcsu_drv_test_0001, "DCSU_DRV_TEST_0001", "Initial DCSU driver setup and wait for signal" },
+    { &dcsu_drv_test_0100, "DCSU_DRV_TEST_0100", "Process Generate SoC UID command" },
+    { &dcsu_drv_test_0101, "DCSU_DRV_TEST_0101", "Process write SoC family ID command" },
+    { &dcsu_drv_test_0102, "DCSU_DRV_TEST_0102", "Process write IEEE ECID command" },
+    { &dcsu_drv_test_0103, "DCSU_DRV_TEST_0103",
+      "Process SoC ID zero count write command and check zero count" },
+    { &dcsu_drv_test_0104, "DCSU_DRV_TEST_0104",
+      "Process multiple SoC Config Area commands and check zero count" },
+    { &dcsu_drv_test_0202, "DCSU_DRV_TEST_0202", "Process arbitrary commands until COMPLETE" },
+    { &dcsu_drv_test_0203, "DCSU_DRV_TEST_0203", "Receieve data until COMPLETE and verify" },
+    { &dcsu_drv_test_0204, "DCSU_DRV_TEST_0204",
       "Receieve data from file until COMPLETE and verify" },
 
 };
