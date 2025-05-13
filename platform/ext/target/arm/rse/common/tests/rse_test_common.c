acf02dd55456e044786524f7b832333d6e866559
Jackson Cooper-Driver <jackson.cooper-driver@arm.com>
2025-05-13 13:32:30 +0100

    Revert "RSE: Tests: Refine FVP limitation workaround"
    This commit follows on from 405fbacaa8c61a0acdec72efacf096ef9f5081a2
    and therefore we must revert this change first before reverting the
    other change.
    
    This reverts commit f9c4472d422ca50b07e0400c156a92434eb27f15.
    
    Change-Id: If7896d21e308c40f15ab7309ae3526633dd40bd9
    Signed-off-by: Jackson Cooper-Driver <jackson.cooper-driver@arm.com>

--- platform/ext/target/arm/rse/common/tests/rse_test_common.c
+++ platform/ext/target/arm/rse/common/tests/rse_test_common.c
@@ -27,7 +27,6 @@ void add_conditional_tests_to_testsuite(struct conditional_test_t *tests, uint32
     enum lcm_lcs_t lcs;
     enum lcm_error_t lcm_err;
     struct conditional_test_t *test;
-    bool invalid_sp_state = false;
 
     lcm_err = lcm_get_tp_mode(&LCM_DEV_S, &tp_mode);
     if (lcm_err != LCM_ERROR_NONE) {
@@ -47,15 +46,15 @@ void add_conditional_tests_to_testsuite(struct conditional_test_t *tests, uint32
     /* FixMe: SP should only be enabled when in CM/DM mode, but due
      * to an FVP limitation, it can be enabled when in SP mode
      * as well. Prevent tests running in this state */
-    if ((sp_enabled == LCM_TRUE) && (lcs != LCM_LCS_CM) && (lcs != LCM_LCS_DM)) {
-        invalid_sp_state = true;
+    if ((lcs != LCM_LCS_CM) && (lcs != LCM_LCS_DM)) {
+        sp_enabled = LCM_FALSE;
     }
 
     for (uint32_t idx = 0; idx < test_am; idx++) {
         test = &tests[idx];
 
         if ((test->any_tp_mode || (tp_mode == test->tp_mode))
-         && (test->any_sp_state || ((sp_enabled == test->sp_enabled) && !invalid_sp_state))
+         && (test->any_sp_state || (sp_enabled == test->sp_enabled))
          && (test->any_lcs || (lcs == test->lcs))) {
             add_tests_to_testsuite(&(test->test), 1, p_ts, ts_size);
         }
