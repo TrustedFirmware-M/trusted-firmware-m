813ff921b1f091b74933f709544caccaf5fa8f51
Roman Mazurak <roman.mazurak@infineon.com>
2025-05-19 14:12:16 +0300

    add_subdirectory_ext - fix for Windows long path issue
    add_subdirectory_ext is wrapper function for built in add_subdirectory
    that create a hashed path in CMAKE_BINARY_DIR folder for Windows to
    avoid long path during the build
    
    Change-Id: If2bd3e6cb8b6abb48e083baba4b58056f5e225fe
    Signed-off-by: Roman Mazurak <roman.mazurak@infineon.com>
    Signed-off-by: Chris Brand <chris.brand@cypress.com>

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -1,9 +1,80 @@
 #-------------------------------------------------------------------------------
-# Copyright (c) 2022, Arm Limited. All rights reserved.
+# SPDX-FileCopyrightText: Copyright The TrustedFirmware-M Contributors
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
 #-------------------------------------------------------------------------------
+
+# This function is a wrapper around the standard add_subdirectory command.
+# On Windows, the binary directory path is truncated and hashed to avoid path length issues.
+#
+# Usage:
+#   add_subdirectory_ext(source_dir [binary_dir] [EXCLUDE_FROM_ALL] [SYSTEM])
+# - source_dir: Source directory to add.
+# - binary_dir (optional): Custom binary directory. If not specified, uses source_dir.
+# - EXCLUDE_FROM_ALL (optional): Exclude from all build targets.
+# - SYSTEM (optional): Treat as a system directory.
+function(add_subdirectory_ext source_dir)
+    set(options EXCLUDE_FROM_ALL SYSTEM)
+    set(oneValueArgs "")
+    set(multiValueArgs "")
+
+    unset(binary_dir)
+    if(ARGC GREATER 1)
+        list(FIND options "${ARGV1}" option_index)
+        if(option_index EQUAL -1)
+            set(binary_dir "${ARGV1}")
+        endif()
+    endif()
+
+    cmake_parse_arguments(PARSE_ARGV 2 ARG "${options}" "${oneValueArgs}" "${multiValueArgs}")
+
+    if (ARG_EXCLUDE_FROM_ALL)
+        set(ARG_EXCLUDE_FROM_ALL "EXCLUDE_FROM_ALL")
+    else()
+        unset(ARG_EXCLUDE_FROM_ALL)
+    endif()
+
+    if (ARG_SYSTEM)
+        set(ARG_SYSTEM "SYSTEM")
+    else()
+        unset(ARG_SYSTEM)
+    endif()
+
+    if(CMAKE_HOST_SYSTEM_NAME STREQUAL "Windows")
+        # Limit path length to avoid problems during build on Windows
+        # Set FULL_BINARY_DIR to the requested absolute path
+        if (DEFINED binary_dir)
+            if(IS_ABSOLUTE "${binary_dir}")
+                set(full_binary_dir "${binary_dir}")
+            else()
+                set(full_binary_dir "${CMAKE_CURRENT_BINARY_DIR}/${binary_dir}")
+            endif()
+        else()
+            set(full_binary_dir "${CMAKE_CURRENT_BINARY_DIR}/${source_dir}")
+        endif()
+
+        # Get relative path to the build directory
+        file(RELATIVE_PATH ref_binary_dir "${CMAKE_BINARY_DIR}" "${full_binary_dir}")
+
+        # Calculate MD5 of the relative path and truncate it to 24 characters
+        string(MD5 hashed_path "${ref_binary_dir}")
+        string(SUBSTRING "${hashed_path}" 0 24 hashed_path)
+
+        # If the original binary_dir is shorter than the hashed path, use it instead
+        set(hashed_binary_dir "${CMAKE_BINARY_DIR}/${hashed_path}")
+        string(LENGTH "${full_binary_dir}" full_binary_dir_len)
+        string(LENGTH "${hashed_binary_dir}" hashed_binary_dir_len)
+        if(full_binary_dir_len LESS_EQUAL hashed_binary_dir_len)
+            set(binary_dir "${full_binary_dir}")
+        else()
+            set(binary_dir "${hashed_binary_dir}")
+        endif()
+    endif()
+
+    # Add subdirectory with optionally truncated path
+    add_subdirectory("${source_dir}" "${binary_dir}" ${ARG_EXCLUDE_FROM_ALL} ${ARG_SYSTEM})
+endfunction()
 
 # A string calibrating routine to the specified length by
 # appending to an input string a specified character.
