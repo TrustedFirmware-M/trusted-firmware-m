f7ae0eceb872686c6a40d1078adaff6301881f96
Antonio de Angelis <Antonio.deAngelis@arm.com>
2025-06-11 13:54:26 +0100

    RSE: Drivers: Add doxy comments to tram driver
    Keep a uniform style and improves in-code documentation using
    Doxygen style comments for the TRAM driver APIs
    
    Signed-off-by: Antonio de Angelis <antonio.deangelis@arm.com>
    Change-Id: I4392d6708f4d874f03b8860e23195f6174b6ada2

--- platform/ext/target/arm/rse/common/native_drivers/tram_drv.h
+++ platform/ext/target/arm/rse/common/native_drivers/tram_drv.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023-2024, Arm Limited. All rights reserved.
+ * Copyright (c) 2023-2025, Arm Limited. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,8 +15,8 @@
  */
 
 /**
- * \file tram_drv.h
- * \brief Driver for Arm TRAM.
+ * @file tram_drv.h
+ * @brief Driver for Arm TRAM.
  */
 
 #ifndef __TRAM_DRV_H__
@@ -29,26 +29,53 @@
 extern "C" {
 #endif
 
-#define TRAM_KEY_SIZE       32
+/**
+ * @brief Size in bytes of the key required by the TRAM. Used for masking
+ *        and address scrambling
+ */
+#define TRAM_KEY_SIZE       (32)
 
 /**
- * \brief ARM TRAM device configuration structure
+ * @brief ARM TRAM device configuration structure
  */
 struct tram_dev_cfg_t {
     const uint32_t base;                         /*!< TRAM base address */
 };
 
 /**
- * \brief ARM TRAM device structure
+ * @brief ARM TRAM device structure
  */
 struct tram_dev_t {
     const struct tram_dev_cfg_t *const cfg;       /*!< TRAM configuration */
 };
 
+/**
+ * @brief Sets the key on the TRAM interface, must be of \a TRAM_KEY_SIZE bytes
+ *
+ * @param[in] dev Pointer to the TRAM device
+ * @param[in] key Bytes of the key, word aligned
+ */
 void tram_set_key(struct tram_dev_t *dev, const uint32_t *key);
+
+/**
+ * @brief Wipes the configured key on the TRAM interface
+ *
+ * @param[in] dev Pointer to the TRAM device
+ */
 void tram_wipe_key(struct tram_dev_t *dev);
 
+/**
+ * @brief Enables masking and address scrambling on the TRAM interface provided
+ *
+ * @param[in] dev Pointer to the TRAM device
+ */
 void tram_enable_encryption(struct tram_dev_t *dev);
+
+/**
+ * @brief Disables masking and address scrambling on the TRAM interface provided
+ *
+ * @param[in] dev Pointer to the TRAM device
+ */
 void tram_disable_encryption(struct tram_dev_t *dev);
 
 #ifdef __cplusplus
