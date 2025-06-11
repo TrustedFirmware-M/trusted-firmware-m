ef7e8f34b48100c9ab0d169980e84d37d6b3c8b9
Antonio de Angelis <Antonio.deAngelis@arm.com>
2025-06-11 09:39:26 +0100

    BL2: MCUboot: Use v2.2.0
    Move from v.2.2.0-rc2 to v2.2.0 now that the final release has
    been tagged.
    
    Signed-off-by: Antonio de Angelis <antonio.deangelis@arm.com>
    Change-Id: I89b8e924ec9a5fbbf2071d22877f8f060b60f857

--- config/config_base.cmake
+++ config/config_base.cmake
@@ -42,7 +42,7 @@ set(MBEDCRYPTO_VERSION                  "mbedtls-3.6.3" CACHE STRING "The versio
 set(MBEDCRYPTO_GIT_REMOTE               "https://github.com/Mbed-TLS/mbedtls.git" CACHE STRING "The URL (or path) to retrieve MbedTLS from.")
 
 set(MCUBOOT_PATH                        "DOWNLOAD"  CACHE PATH      "Path to MCUboot (or DOWNLOAD to fetch automatically")
-set(MCUBOOT_VERSION                     "v2.2.0-rc2" CACHE STRING   "The version of MCUboot to use")
+set(MCUBOOT_VERSION                     "v2.2.0"    CACHE STRING    "The version of MCUboot to use")
 
 set(TFM_EXTRAS_REPO_PATH                "DOWNLOAD"  CACHE PATH      "Path to tf-m-extras repo (or DOWNLOAD to fetch automatically")
 set(TFM_EXTRAS_REPO_VERSION             "8add4af"   CACHE STRING    "The version of tf-m-extras to use")
