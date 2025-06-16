7cfce928f5f2f7750909bf2fcf49996fff620a6e
Jackson Cooper-Driver <jackson.cooper-driver@arm.com>
2025-06-16 16:18:19 +0100

    RSE: Add rse_comms_link_hal.c
    Add the header for the RSE comms HAL and the implementation for the RSE.
    The API defined in the header should be able to be integrated into
    different components in the RSE comms system, without having to modify
    the higher level stack.
    
    Change-Id: I221440636b2076fd47434981442bb0389a560e9d
    Signed-off-by: Jackson Cooper-Driver <jackson.cooper-driver@arm.com>

---
+++ platform/ext/target/arm/rse/common/rse_comms/rse_comms_link_hal.c
@@ -0,0 +1,217 @@
+/*
+ * SPDX-FileCopyrightText: Copyright The TrustedFirmware-M Contributors
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#include <assert.h>
+
+#include "platform_error_codes.h"
+#include "rse_comms_link_hal.h"
+#include "rse_get_routing_tables.h"
+#include "rse_get_rse_id.h"
+#include "rse_comms_platform.h"
+#include "mhu.h"
+
+static enum rse_comms_hal_error_t get_routing_tables_for_rse_id(const uint8_t **routing_tables,
+                                                                size_t *routing_tables_size,
+                                                                uint32_t *rse_id)
+{
+    enum tfm_plat_err_t plat_err;
+
+    plat_err = rse_get_rse_id(rse_id);
+    if (plat_err != TFM_PLAT_ERR_SUCCESS) {
+        return RSE_COMMS_HAL_ERROR_CANNOT_GET_ID;
+    }
+
+    plat_err = rse_get_routing_tables(routing_tables, routing_tables_size, *rse_id);
+    if (plat_err != TFM_PLAT_ERR_SUCCESS) {
+        return RSE_COMMS_HAL_ERROR_CANNOT_GET_ROUTING_TABLES;
+    }
+
+    return RSE_COMMS_HAL_ERROR_SUCCESS;
+}
+
+rse_comms_link_id_t rse_comms_hal_get_route(rse_comms_node_id_t node_id)
+{
+    enum rse_comms_hal_error_t comms_err;
+    uint32_t rse_id;
+    const uint8_t *routing_tables;
+    size_t routing_tables_size;
+
+    comms_err = get_routing_tables_for_rse_id(&routing_tables, &routing_tables_size, &rse_id);
+    if (comms_err != RSE_COMMS_HAL_ERROR_SUCCESS) {
+        return 0;
+    }
+
+    assert(node_id != rse_id);
+
+    if (node_id >= routing_tables_size) {
+        return 0;
+    }
+
+    return routing_tables[node_id];
+}
+
+enum rse_comms_hal_error_t rse_comms_hal_get_my_node_id(rse_comms_node_id_t *node_id)
+{
+    uint32_t rse_id;
+    enum tfm_plat_err_t plat_err;
+
+    plat_err = rse_get_rse_id(&rse_id);
+    if (plat_err != TFM_PLAT_ERR_SUCCESS) {
+        return RSE_COMMS_HAL_ERROR_CANNOT_GET_ID;
+    }
+
+    *node_id = rse_id;
+
+    return RSE_COMMS_HAL_ERROR_SUCCESS;
+}
+
+enum rse_comms_hal_error_t rse_comms_hal_send_message(rse_comms_link_id_t link_id,
+                                                      const uint8_t *message, size_t message_size)
+{
+    struct rse_comms_platform_device_t device;
+
+    device = rse_comms_platform_get_send_device(link_id);
+
+    switch (device.type) {
+    case RSE_COMMS_PLATFORM_DEVICE_TYPE_MHUV2:
+    case RSE_COMMS_PLATFORM_DEVICE_TYPE_MHUV3: {
+        enum mhu_error_t mhu_err = mhu_send_data((void *)device.device, message, message_size);
+        if (mhu_err != MHU_ERR_NONE) {
+            return RSE_COMMS_HAL_ERROR_DEVICE_SEND_FAIL;
+        }
+
+        break;
+    }
+    default:
+        return RSE_COMMS_HAL_ERROR_UNSUPPORTED_DEVICE;
+    }
+
+    return RSE_COMMS_HAL_ERROR_SUCCESS;
+}
+
+enum rse_comms_hal_error_t rse_comms_hal_is_message_available(rse_comms_link_id_t link_id,
+                                                              bool *is_available)
+{
+    struct rse_comms_platform_device_t device;
+
+    device = rse_comms_platform_get_receive_device(link_id);
+
+    switch (device.type) {
+    case RSE_COMMS_PLATFORM_DEVICE_TYPE_MHUV2:
+    case RSE_COMMS_PLATFORM_DEVICE_TYPE_MHUV3: {
+        enum mhu_error_t mhu_err = mhu_data_is_available((void *)device.device, is_available);
+        if (mhu_err != MHU_ERR_NONE) {
+            return RSE_COMMS_HAL_ERROR_DEVICE_IS_AVAILABLE_FAIL;
+        }
+
+        break;
+    }
+    default:
+        return RSE_COMMS_HAL_ERROR_UNSUPPORTED_DEVICE;
+    }
+
+    return RSE_COMMS_HAL_ERROR_SUCCESS;
+}
+
+enum rse_comms_hal_error_t rse_comms_hal_receive_message(rse_comms_link_id_t link_id,
+                                                         uint8_t *message, size_t buffer_size,
+                                                         size_t *message_size)
+{
+    struct rse_comms_platform_device_t device;
+
+    device = rse_comms_platform_get_receive_device(link_id);
+
+    switch (device.type) {
+    case RSE_COMMS_PLATFORM_DEVICE_TYPE_MHUV2:
+    case RSE_COMMS_PLATFORM_DEVICE_TYPE_MHUV3: {
+        /* This MHU driver API is confusing, as the size argument is both an input parameter with the
+         * size of the buffer and an output parameter specifying the size of data received */
+        size_t size = buffer_size;
+
+        enum mhu_error_t mhu_err = mhu_receive_data((void *)device.device, message, &size);
+        if (mhu_err != MHU_ERR_NONE) {
+            return RSE_COMMS_HAL_ERROR_DEVICE_RECEIVE_FAIL;
+        }
+
+        *message_size = size;
+
+        break;
+    }
+    default:
+        return RSE_COMMS_HAL_ERROR_UNSUPPORTED_DEVICE;
+    }
+
+    return RSE_COMMS_HAL_ERROR_SUCCESS;
+}
+
+static enum rse_comms_hal_error_t init_sender_receiver(struct rse_comms_platform_device_t device,
+                                                       bool is_sender)
+{
+    switch (device.type) {
+    case RSE_COMMS_PLATFORM_DEVICE_TYPE_MHUV2:
+    case RSE_COMMS_PLATFORM_DEVICE_TYPE_MHUV3: {
+        enum mhu_error_t mhu_err;
+
+        if (is_sender) {
+            mhu_err = mhu_init_sender((void *)device.device);
+        } else {
+            mhu_err = mhu_init_receiver((void *)device.device);
+        }
+
+        /* In some platforms, there is a mixture of MHU versions, but the current MHU API
+         * only allows support for single version to be compiled in. Different components
+         * use different MHU devices (and only one type) but the routing tables are shared
+         * and therefore we could try and initialise an MHU device with a different version
+         * to what the compiled in driver supports. Allow that to happen here without error,
+         * if we try and actually use the device we will fail with an incorrect version
+         * then */
+        if ((mhu_err != MHU_ERR_NONE) && (mhu_err != MHU_ERR_INVALID_VERSION)) {
+            return RSE_COMMS_HAL_ERROR_DEVICE_INIT_FAIL;
+        }
+
+        break;
+    }
+    default:
+        return RSE_COMMS_HAL_ERROR_UNSUPPORTED_DEVICE;
+    }
+
+    return RSE_COMMS_HAL_ERROR_SUCCESS;
+}
+
+enum rse_comms_hal_error_t rse_comms_hal_init(void)
+{
+    enum rse_comms_hal_error_t comms_err;
+    uint32_t rse_id;
+    const uint8_t *routing_tables;
+    size_t routing_tables_size;
+    rse_comms_link_id_t link_id;
+
+    comms_err = get_routing_tables_for_rse_id(&routing_tables, &routing_tables_size, &rse_id);
+    if (comms_err != RSE_COMMS_HAL_ERROR_SUCCESS) {
+        return comms_err;
+    }
+
+    for (rse_comms_node_id_t node = 0; node < routing_tables_size; node++) {
+        if (node == rse_id) {
+            continue;
+        }
+
+        link_id = routing_tables[node];
+
+        comms_err = init_sender_receiver(rse_comms_platform_get_send_device(link_id), true);
+        if (comms_err != RSE_COMMS_HAL_ERROR_SUCCESS) {
+            return comms_err;
+        }
+
+        comms_err = init_sender_receiver(rse_comms_platform_get_receive_device(link_id), false);
+        if (comms_err != RSE_COMMS_HAL_ERROR_SUCCESS) {
+            return comms_err;
+        }
+    }
+
+    return RSE_COMMS_HAL_ERROR_SUCCESS;
+}
---
+++ platform/ext/target/arm/rse/common/rse_comms/rse_comms_link_hal.h
@@ -0,0 +1,109 @@
+/*
+ * SPDX-FileCopyrightText: Copyright The TrustedFirmware-M Contributors
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#ifndef __RSE_COMMS_LINK_HAL_H__
+#define __RSE_COMMS_LINK_HAL_H__
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "platform_error_codes.h"
+#include "rse_comms_link_defs.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum rse_comms_hal_error_t {
+    RSE_COMMS_HAL_ERROR_SUCCESS = 0,
+    RSE_COMMS_HAL_ERROR_UNSUPPORTED_DEVICE,
+    RSE_COMMS_HAL_ERROR_CANNOT_GET_ID,
+    RSE_COMMS_HAL_ERROR_CANNOT_GET_ROUTING_TABLES,
+    RSE_COMMS_HAL_ERROR_DEVICE_SEND_FAIL,
+    RSE_COMMS_HAL_ERROR_DEVICE_IS_AVAILABLE_FAIL,
+    RSE_COMMS_HAL_ERROR_DEVICE_RECEIVE_FAIL,
+    RSE_COMMS_HAL_ERROR_DEVICE_INIT_FAIL,
+    RSE_COMMS_HAL_ERROR_FORCE_UINT32 = UINT32_MAX,
+};
+
+/**
+ * \brief Retrieves the route (link ID) to the specified node.
+ *
+ * This function determines the appropriate communication link (route)
+ * to send a message to the node.
+ *
+ * \param node_id The identifier of the target node.
+ * \return The link ID corresponding to the link to send the message on.
+ */
+rse_comms_link_id_t rse_comms_hal_get_route(rse_comms_node_id_t node_id);
+
+/**
+ * \brief Retrieves the current node's identifier.
+ *
+ * This function obtains the node ID for the current device.
+ *
+ * \param[out] node_id Pointer to a variable where the node ID will be stored.
+ * \return RSE_COMMS_HAL_ERROR_SUCCESS on success, or an appropriate error code.
+ */
+enum rse_comms_hal_error_t rse_comms_hal_get_my_node_id(rse_comms_node_id_t *node_id);
+
+/**
+ * \brief Sends a message over the specified communication link.
+ *
+ * This function sends a message to a remote node using the provided link ID.
+ *
+ * \param link_id      The link identifier to use for transmission.
+ * \param message      Pointer to the message data to send.
+ * \param message_size The size of the message in bytes.
+ * \return RSE_COMMS_HAL_ERROR_SUCCESS on success, or an appropriate error code.
+ */
+enum rse_comms_hal_error_t rse_comms_hal_send_message(rse_comms_link_id_t link_id,
+                                                      const uint8_t *message, size_t message_size);
+
+/**
+ * \brief Waits for a message on the specified communication link.
+ *
+ * This function blocks until a message is received on the specified link.
+ *
+ * \note This is a blocking call. Consider updating to a non-blocking variant if needed.
+ *
+ * \param link_id The link identifier to wait on.
+ * \param is_available Whether or not the message is available
+ * \return RSE_COMMS_HAL_ERROR_SUCCESS on success, or an appropriate error code.
+ */
+enum rse_comms_hal_error_t rse_comms_hal_is_message_available(rse_comms_link_id_t link_id,
+                                                              bool *is_available);
+
+/**
+ * \brief Receives a message from the specified communication link.
+ *
+ * This function receives a message from the given link and stores it in the provided buffer.
+ *
+ * \param link_id      The link identifier to receive the message from.
+ * \param message      Pointer to the buffer where the received message will be stored.
+ * \param buffer_size  The maximum size of the buffer.
+ * \param[out] message_size Pointer to store the actual size of the received message.
+ * \return RSE_COMMS_HAL_ERROR_SUCCESS on success, or an appropriate error code.
+ */
+enum rse_comms_hal_error_t rse_comms_hal_receive_message(rse_comms_link_id_t link_id,
+                                                         uint8_t *message, size_t buffer_size,
+                                                         size_t *message_size);
+
+/**
+ * \brief Initializes the communication HAL layer.
+ *
+ * This function initializes internal data structures and hardware resources required
+ * for communication.
+ *
+ * \return RSE_COMMS_HAL_ERROR_SUCCESS on successful initialization, or an appropriate error code.
+ */
+enum rse_comms_hal_error_t rse_comms_hal_init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __RSE_COMMS_LINK_HAL_H__ */
