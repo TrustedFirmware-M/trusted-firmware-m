29e81f6fba92ec7a9292cdd581d5cea8f66abd8b
Antonio de Angelis <Antonio.deAngelis@arm.com>
2025-06-26 23:53:42 +0100

    RSE: Fix gpt.h header/entry structs
    Make sure that the reserved area in a gpt_entry_t is correctly
    represented as a Flexible Array Member (FMA).
    
    Signed-off-by: Antonio de Angelis <antonio.deangelis@arm.com>
    Change-Id: Ia0c730495ea78e35da5cbb39471a3fa9eac5b427

--- platform/ext/target/arm/rse/common/fip_parser/gpt.h
+++ platform/ext/target/arm/rse/common/fip_parser/gpt.h
@@ -31,6 +31,7 @@ typedef struct gpt_entry {
     uint64_t        last_lba;
     uint64_t        attr;
     uint16_t        name[EFI_NAMELEN_MAX];
+    uint8_t         reserved[]; /*!< If list_entry_size in gpt_header_t > 128, this is reserved as 0 */
 } gpt_entry_t;
 
 typedef struct gpt_header {
@@ -44,12 +45,9 @@ typedef struct gpt_header {
     uint64_t        first_lba;
     uint64_t        last_lba;
     struct efi_guid disk_uuid;
-    /* starting LBA of array of partition entries */
-    uint64_t        list_lba;
-    /* number of partition entries in array */
-    uint32_t        list_num;
-    /* size of a single partition entry (usually 128) */
-    uint32_t        list_entry_size;
+    uint64_t        list_lba;        /*!< starting LBA of array of partition entries */
+    uint32_t        list_num;        /*!< number of partition entries in array */
+    uint32_t        list_entry_size; /*!< size of a single partition entry (usually 128) */
     uint32_t        list_crc;
 } gpt_header_t;
 
