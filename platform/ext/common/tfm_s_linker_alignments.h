3c83b13a41ef247f85d191f588bedc9073f8d26f
Meggyesi, Janos <janos.meggyesi@arm.com>
2025-07-04 11:17:21 +0200

    RSE: move SPM Code and RO data to ITCM
    GCC, LLVM and Armclang related linker scripts are modified to move the
    code and read-only data sections of the Secure Partition Manager
    to ITCM.
    
    Signed-off-by: Meggyesi, Janos <janos.meggyesi@arm.com>
    Change-Id: Ib4e6f51005f0a989a1a9b9d6bdca074c8e3b031a

--- platform/ext/common/tfm_s_linker_alignments.h
+++ platform/ext/common/tfm_s_linker_alignments.h
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2022 Cypress Semiconductor Corporation (an Infineon company)
  * or an affiliate of Cypress Semiconductor Corporation. All rights reserved.
- * Copyright (c) 2024, Arm Limited. All rights reserved.
+ * SPDX-FileCopyrightText: Copyright The TrustedFirmware-M Contributors
  *
  * SPDX-License-Identifier: BSD-3-Clause
  *
@@ -101,6 +101,10 @@
 
 #ifndef TFM_LINKER_PT_PROT_PART_PRIVATE_DATA_ALIGNMENT
 #define TFM_LINKER_PT_PROT_PART_PRIVATE_DATA_ALIGNMENT  TFM_LINKER_DEFAULT_ALIGNMENT
+#endif
+
+#ifndef TFM_LINKER_SPM_CODE_ALIGNMENT
+#define TFM_LINKER_SPM_CODE_ALIGNMENT  TFM_LINKER_DEFAULT_ALIGNMENT
 #endif
 
 /* Alignment of stack of NS agent TZ partition. This should not be needed once
