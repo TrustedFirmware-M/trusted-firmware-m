88c6f2a81ee03bd095b4525e85fdd22a1a0423df
Mudit Sharma <mudit.sharma@arm.com>
2025-08-01 11:02:28 +0100

    Platform: Add dTPM PCR to Measured boot slot mapping
    Introduce getter and init function prototypes for dTPM PCR to mesured
    boot slot mapping. These functions have to be implemented by each
    platform wishing to implement dTPM client partition.
    
    Change-Id: Iad8a1fb1ab4c8c4f77d715826cc6e3dd559545d4
    Signed-off-by: Mudit Sharma <mudit.sharma@arm.com>

--- platform/include/tfm_boot_measurement.h
+++ platform/include/tfm_boot_measurement.h
@@ -8,6 +8,12 @@
 #ifndef __TFM_BOOT_MEASUREMENT_H__
 #define __TFM_BOOT_MEASUREMENT_H__
 
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+#include "stdint.h"
+
 #if defined(PLATFORM_DEFAULT_MEASUREMENT_SLOTS)
 enum boot_measurement_slot_t {
     BOOT_MEASUREMENT_SLOT_BL1_2 = 0,
@@ -16,13 +22,46 @@ enum boot_measurement_slot_t {
     BOOT_MEASUREMENT_SLOT_RT_1,
     BOOT_MEASUREMENT_SLOT_RT_2,
     BOOT_MEASUREMENT_SLOT_MAX = 32,
-    BOOT_MEASUREMENT_SLOT_MAX_THEORETICAL = 63  /* Slot index is stored in
-                                                 * 6 bits in the shared
-                                                 * memory area.
-                                                 */
+    BOOT_MEASUREMENT_SLOT_MAX_THEORETICAL = 63,  /* Slot index is stored in
+                                                  * 6 bits in the shared
+                                                  * memory area.
+                                                  */
+    __BOOT_MEASUREMENT_MAX_VAL = UINT32_MAX
 };
 #else
 #include "tfm_plat_boot_measurement.h"
 #endif /* PLATFORM_DEFAULT_MEASUREMENT_SLOTS */
+
+/**
+ * \brief                               Initializes the PCR index mapping for
+ *                                      boot measurement slots.
+ *
+ * \note                                This function sets all internal PCR index
+ *                                      mappings to -1, then assigns specific PCR
+ *                                      indices to predefined boot measurement
+ *                                      slots using the SET_MBOOT_SLOT_PCR macro.
+ *
+ * \retval 0                            The PCR index mapping was initialized
+ *                                      successfully.
+ * \retval 1                            A slot or PCR index was out of range
+ *                                      during initialization.
+ */
+int init_pcr_index_for_boot_measurement(void);
+
+/**
+ * \brief                               Retrieves the PCR index associated with
+ *                                      a given boot measurement slot.
+ *
+ * \param[in]  slot                     Boot measurement slot identifier. Must be
+ *                                      less than BOOT_MEASUREMENT_SLOT_MAX.
+ *
+ * \return                              The PCR index assigned to the specified
+ *                                      slot, or -1 if uninitialized.
+ */
+int8_t get_pcr_index_for_boot_measurement(uint8_t slot);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
 
 #endif /* __TFM_BOOT_MEASUREMENT_H__ */
