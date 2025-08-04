3e7df7fb69a71df984f224c22e83b0390b422efe
Mudit Sharma <mudit.sharma@arm.com>
2025-08-04 15:40:32 +0100

    rp2350: Clear shared boot data in BL2 post init
    For this platform, BL2 is the first stage of the TF-M boot process. The
    shared buffer used between the bootloader and runtime must be cleared
    after each reset or reboot. This prevents residual state from
    previous boots from affecting subsequent boot measurement related checks
    performed in function `boot_add_data_to_shared_area`.
    
    Signed-off-by: Mudit Sharma <mudit.sharma@arm.com>
    Change-Id: Ic5856ccabb84538643da190d3d33e1d38cb0c0a7

--- platform/ext/target/rpi/rp2350/bl2/boot_hal_bl2.c
+++ platform/ext/target/rpi/rp2350/bl2/boot_hal_bl2.c
@@ -3,10 +3,11 @@
  * SPDX-FileCopyrightText: Copyright The TrustedFirmware-M Contributors
  */
 
-#include "boot_hal.h"
-
 #include <string.h>
+
+#include "boot_hal.h"
 #include "bl2_image_id.h"
+#include "tfm_plat_shared_measurement_data.h"
 
 bool boot_platform_should_load_image(uint32_t image_id)
 {
@@ -17,4 +18,13 @@ bool boot_platform_should_load_image(uint32_t image_id)
 #endif /* TFM_LOAD_NS_IMAGE */
 
     return true;
+}
+
+int32_t boot_platform_post_init(void)
+{
+    /* Clear boot data area */
+    memset((void *)tfm_plat_get_shared_measurement_data_base(), 0,
+           tfm_plat_get_shared_measurement_data_size());
+
+    return 0;
 }
