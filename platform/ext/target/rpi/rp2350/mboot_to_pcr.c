e55bea530048c9621d359d1851b15a2fcf4768e9
Mudit Sharma <mudit.sharma@arm.com>
2025-08-07 10:25:59 +0100

    Rp2350: Implement measured boot slot to PCR index helper functions
    Implement getter and init functions for measured boot slot to PCR index
    mapping.
    
    Change-Id: I1f87d73d9113f08dd1e077328956ba7e8510eeb2
    Signed-off-by: Mudit Sharma <mudit.sharma@arm.com>

---
+++ platform/ext/target/rpi/rp2350/mboot_to_pcr.c
@@ -0,0 +1,38 @@
+/*
+ * SPDX-FileCopyrightText: Copyright The TrustedFirmware-M Contributors
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#include "tfm_boot_measurement.h"
+
+static int8_t pcr_index_for_boot_measurement[BOOT_MEASUREMENT_SLOT_MAX] = {0};
+
+#define SET_MBOOT_SLOT_PCR(slot, pcr)                  \
+    do {                                               \
+        if (slot >= BOOT_MEASUREMENT_SLOT_MAX &&       \
+                pcr < 24) {                            \
+            return 1; /* Out of range */               \
+        }                                              \
+        pcr_index_for_boot_measurement[slot] = (pcr);  \
+    } while (0)
+
+int init_pcr_index_for_boot_measurement(void)
+{
+    for (int i = 0; i < BOOT_MEASUREMENT_SLOT_MAX; i++) {
+        pcr_index_for_boot_measurement[i] = -1;
+    }
+
+    SET_MBOOT_SLOT_PCR(BOOT_MEASUREMENT_SLOT_BL2, 0);
+    SET_MBOOT_SLOT_PCR(BOOT_MEASUREMENT_SLOT_RT_0, 0);
+    SET_MBOOT_SLOT_PCR(BOOT_MEASUREMENT_SLOT_RT_1, 0);
+    SET_MBOOT_SLOT_PCR(BOOT_MEASUREMENT_SLOT_RT_2, 0);
+
+    return 0;
+}
+
+int8_t get_pcr_index_for_boot_measurement(uint8_t slot)
+{
+    return pcr_index_for_boot_measurement[slot];
+}
