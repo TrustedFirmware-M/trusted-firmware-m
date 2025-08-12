384cbbfe088e419e638adc38bc60781847c9b2b4
Mudit Sharma <mudit.sharma@arm.com>
2025-08-12 16:59:51 +0100

    RP2350: Implement get_event_log_metadata_for_measurement_slot() API
    Define a platform-specific `event_log_metadata_t` array that specifies
    the PCR_EVENT2 event data and PCR index for each boot measurement slot.
    The corresponding value from this array is copied into
    `event_log_metadata_t` structure provided by the caller.
    
    Change-Id: Id1483eea33bcfbcb8f5dd052210cb40f7458c022
    Signed-off-by: Mudit Sharma <mudit.sharma@arm.com>

--- platform/ext/target/rpi/rp2350/mboot_to_pcr.c
+++ platform/ext/target/rpi/rp2350/mboot_to_pcr.c
@@ -6,8 +6,16 @@
  */
 
 #include "tfm_boot_measurement.h"
+#include "string.h"
 
 static int8_t pcr_index_for_boot_measurement[BOOT_MEASUREMENT_SLOT_MAX] = {0};
+
+static const event_log_metadata_t tfm_event_log_metadata[] = {
+    { BOOT_MEASUREMENT_SLOT_BL2, "TFM_BL2", 0 },
+    { BOOT_MEASUREMENT_SLOT_RT_0, "TFM_RT_0", 0 },
+    { BOOT_MEASUREMENT_SLOT_RT_1, "TFM_RT_1", 0 },
+    { BOOT_MEASUREMENT_SLOT_RT_2, "TFM_RT_2", 0 },
+};
 
 #define SET_MBOOT_SLOT_PCR(slot, pcr)                  \
     do {                                               \
@@ -39,4 +47,17 @@ int8_t get_pcr_index_for_boot_measurement(uint8_t slot)
     }
 
     return pcr_index_for_boot_measurement[slot];
+}
+
+int get_event_log_metadata_for_measurement_slot(uint8_t slot, event_log_metadata_t *event_log_metadata)
+{
+    size_t metadata_arr_size = sizeof(tfm_event_log_metadata)/sizeof(tfm_event_log_metadata[0]);
+
+    if (slot > (metadata_arr_size - 1)) {
+        return -1;
+    }
+
+    memcpy(event_log_metadata, &tfm_event_log_metadata[slot], sizeof(event_log_metadata_t));
+
+    return 0;
 }
