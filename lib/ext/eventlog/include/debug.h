443677d1c8ae899f9e7544872dc0a11ee196a518
Mudit Sharma <mudit.sharma@arm.com>
2025-08-12 09:57:02 +0100

    Lib: eventLog: Update libeventlog
    Logging backend in libeventlog has been updated and its APIs have been
    split across multiple headers. This commit enables logging in
    libeventlog for TF-M.
    
    `debug.h` in libeventlog is replaced with TF-M's custom implementation,
    since the logging macros used by libeventlog are already defined within
    TF-M.
    
    Additionally, Existing code has been reorganized and split across
    multiple source files which have been added to sources list in Cmake.
    
    Change-Id: Ia0be783107a58426cdb772c80f1d73398a86201a
    Signed-off-by: Mudit Sharma <mudit.sharma@arm.com>

---
+++ lib/ext/eventlog/include/debug.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2025, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef DEBUG_H
+#define DEBUG_H
+
+#include "tfm_log.h"
+
+#endif /* DEBUG_H */
