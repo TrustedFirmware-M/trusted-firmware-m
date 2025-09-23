ff028e958a26b20613dd3fe3d9da6fb6595a5bbf
Mudit Sharma <mudit.sharma@arm.com>
2025-09-23 14:55:15 +0100

    bl1: Use standard python script template
    Add `main` function to `create_bl1_2_img.py` and
    `create_provisioning_bundle`. Add `bl1` package and script entry point
    to `pyproject.toml`.
    
    Signed-off-by: Mudit Sharma <mudit.sharma@arm.com>
    Change-Id: Ib0ca8d4f278ca5f4d1b997535c0172894f5709a6

--- bl1/bl1_1/scripts/create_bl1_2_img.py
+++ bl1/bl1_1/scripts/create_bl1_2_img.py
@@ -1,9 +1,9 @@
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 # SPDX-FileCopyrightText: Copyright The TrustedFirmware-M Contributors
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 
 import argparse
 import hashlib
@@ -13,31 +13,42 @@ from bl2.macro_parser import evaluate_macro
 import struct
 from tfm_tools.struct_pack import struct_pack
 
+
 def hash_binary_blob(blob, hash_alg):
-   hash = getattr(hashlib, hash_alg.lower())()
-   hash.update(blob)
-   return hash.digest()
+    hash = getattr(hashlib, hash_alg.lower())()
+    hash.update(blob)
+    return hash.digest()
 
-parser = argparse.ArgumentParser(allow_abbrev=False)
-parser.add_argument("--input_file", help="the image to process", required=True)
-parser.add_argument("--img_output_file", help="image output file", required=True)
-parser.add_argument("--hash_output_file", help="hash output file", required=True)
-parser.add_argument("--hash_alg", help="Hash algorithm to use for measurement", required=True)
-parser.add_argument("--signing_layout_file", help="signing layout file", required=True)
-args = parser.parse_args()
 
-with open(args.input_file, "rb") as in_file:
-    bl1_2_code = in_file.read()
+def main():
+    parser = argparse.ArgumentParser(allow_abbrev=False)
+    parser.add_argument(
+        "--input_file", help="the image to process", required=True)
+    parser.add_argument("--img_output_file",
+                        help="image output file", required=True)
+    parser.add_argument("--hash_output_file",
+                        help="hash output file", required=True)
+    parser.add_argument(
+        "--hash_alg", help="Hash algorithm to use for measurement", required=True)
+    parser.add_argument("--signing_layout_file",
+                        help="signing layout file", required=True)
+    args = parser.parse_args()
 
-bl1_2_partition_size = evaluate_macro(args.signing_layout_file,
-                                      ".*(RE_BL1_2_BIN_SIZE) = *(.*)",
-                                      1, 2, True)['RE_BL1_2_BIN_SIZE']
+    with open(args.input_file, "rb") as in_file:
+        bl1_2_code = in_file.read()
 
-image = struct_pack([bl1_2_code], pad_to=bl1_2_partition_size)
-hash = hash_binary_blob(image, args.hash_alg)
+    bl1_2_partition_size = evaluate_macro(args.signing_layout_file,
+                                          ".*(RE_BL1_2_BIN_SIZE) = *(.*)",
+                                          1, 2, True)['RE_BL1_2_BIN_SIZE']
 
-with open(args.img_output_file, "wb") as img_out_file:
-    img_out_file.write(image)
+    image = struct_pack([bl1_2_code], pad_to=bl1_2_partition_size)
+    hash = hash_binary_blob(image, args.hash_alg)
 
-with open(args.hash_output_file, "wb") as hash_out_file:
-    hash_out_file.write(hash)
+    with open(args.img_output_file, "wb") as img_out_file:
+        img_out_file.write(image)
+
+    with open(args.hash_output_file, "wb") as hash_out_file:
+        hash_out_file.write(hash)
+
+if __name__ == '__main__':
+    main()
--- bl1/bl1_1/scripts/create_provisioning_bundle.py
+++ bl1/bl1_1/scripts/create_provisioning_bundle.py
@@ -1,54 +1,64 @@
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 # SPDX-FileCopyrightText: Copyright The TrustedFirmware-M Contributors
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 
 import argparse
 import struct
 import secrets
 from tfm_tools.struct_pack import struct_pack
 
-parser = argparse.ArgumentParser(allow_abbrev=False)
-parser.add_argument("--bl2_encryption_key_input_file", help="the key that BL2 was encrypted with", required=True)
-parser.add_argument("--bl2_signing_key_input_file", help="the key that BL2 was signed with", required=False)
-parser.add_argument("--guk_input_file", help="the GUK", required=True)
-parser.add_argument("--bl1_2_padded_hash_input_file", help="the hash of the final bl1_2 image", required=True)
-parser.add_argument("--bl1_2_input_file", help="the final bl1_2 image", required=True)
-parser.add_argument("--bundle_output_file", help="bundle output file", required=True)
-args = parser.parse_args()
 
-with open(args.bl2_encryption_key_input_file, "rb") as in_file:
-    bl1_2_encryption_key = in_file.read()
+def main():
+    parser = argparse.ArgumentParser(allow_abbrev=False)
+    parser.add_argument("--bl2_encryption_key_input_file",
+                        help="the key that BL2 was encrypted with", required=True)
+    parser.add_argument("--bl2_signing_key_input_file",
+                        help="the key that BL2 was signed with", required=False)
+    parser.add_argument("--guk_input_file", help="the GUK", required=True)
+    parser.add_argument("--bl1_2_padded_hash_input_file",
+                        help="the hash of the final bl1_2 image", required=True)
+    parser.add_argument("--bl1_2_input_file",
+                        help="the final bl1_2 image", required=True)
+    parser.add_argument("--bundle_output_file",
+                        help="bundle output file", required=True)
+    args = parser.parse_args()
 
-with open(args.guk_input_file, "rb") as in_file:
-    guk = in_file.read()
+    with open(args.bl2_encryption_key_input_file, "rb") as in_file:
+        bl1_2_encryption_key = in_file.read()
 
-with open(args.bl1_2_padded_hash_input_file, "rb") as in_file:
-    bl1_2_padded_hash = in_file.read()
+    with open(args.guk_input_file, "rb") as in_file:
+        guk = in_file.read()
 
-with open(args.bl1_2_input_file, "rb") as in_file:
-    bl1_2 = in_file.read()
+    with open(args.bl1_2_padded_hash_input_file, "rb") as in_file:
+        bl1_2_padded_hash = in_file.read()
 
-if args.bl2_signing_key_input_file:
-    with open(args.bl2_signing_key_input_file, "rb") as in_file:
-        # Remove the first 4 bytes since it's HSS info
-        bl1_rotpk_0 = in_file.read()[4:]
-else:
-    bl1_rotpk_0 = bytes(56)
+    with open(args.bl1_2_input_file, "rb") as in_file:
+        bl1_2 = in_file.read()
 
-bl1_2_image_len = len(bl1_2).to_bytes(4, byteorder='little')
+    if args.bl2_signing_key_input_file:
+        with open(args.bl2_signing_key_input_file, "rb") as in_file:
+            # Remove the first 4 bytes since it's HSS info
+            bl1_rotpk_0 = in_file.read()[4:]
+    else:
+        bl1_rotpk_0 = bytes(56)
 
-bundle = struct_pack([
-    int("0xC0DEFEED", 16).to_bytes(4, 'little'),
-    bl1_2_encryption_key,
-    guk,
-    bl1_2_padded_hash,
-    bl1_2,
-    bl1_2_image_len,
-    bl1_rotpk_0
-])
+    bl1_2_image_len = len(bl1_2).to_bytes(4, byteorder='little')
 
-with open(args.bundle_output_file, "wb") as out_file:
-    out_file.write(bundle)
+    bundle = struct_pack([
+        int("0xC0DEFEED", 16).to_bytes(4, 'little'),
+        bl1_2_encryption_key,
+        guk,
+        bl1_2_padded_hash,
+        bl1_2,
+        bl1_2_image_len,
+        bl1_rotpk_0
+    ])
+
+    with open(args.bundle_output_file, "wb") as out_file:
+        out_file.write(bundle)
+
+if __name__ == "__main__":
+    main()
