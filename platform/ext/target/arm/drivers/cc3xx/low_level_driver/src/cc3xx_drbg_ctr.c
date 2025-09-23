76b7f858b31d58bd65f28d13f1b2cefe307efd2a
Amjad Ouled-Ameur <amjad.ouled-ameur@arm.com>
2025-09-23 12:56:10 +0100

    cc3xx: avoid re-initializing PKA state in DRBG CTR
    DRBG CTR which leverages the PKA engine, in case DRBG CTR is called by a
    driver concurrently using the PKA engine. If the calling driver has
    already initialized the PKA engine, DRBG CTR can simply use a scratch
    register without clearing the current PKA state.
    
    Change-Id: I7688b5ec0059f376939d7776b0b9605bed975143
    Signed-off-by: Amjad Ouled-Ameur <amjad.ouled-ameur@arm.com>

--- platform/ext/target/arm/drivers/cc3xx/low_level_driver/src/cc3xx_drbg_ctr.c
+++ platform/ext/target/arm/drivers/cc3xx/low_level_driver/src/cc3xx_drbg_ctr.c
@@ -16,11 +16,15 @@
 
 static void long_inc_int(uint32_t *acc, size_t acc_size, bool is_increment)
 {
+    const bool pka_initialized = cc3xx_lowlevel_pka_is_initialized();
     cc3xx_pka_reg_id_t r0;
+
     assert(acc_size == CC3XX_DRBG_CTR_BLOCKLEN);
 
-    /* Accumulation happen only on 128 bit accumulators */
-    cc3xx_lowlevel_pka_init(CC3XX_DRBG_CTR_BLOCKLEN);
+    if (!pka_initialized) {
+        /* Accumulation happen only on 128 bit accumulators */
+        cc3xx_lowlevel_pka_init(CC3XX_DRBG_CTR_BLOCKLEN);
+    }
 
     /* Allocate a register among those not in use, given configured size */
     r0 = cc3xx_lowlevel_pka_allocate_reg();
@@ -38,8 +42,12 @@ static void long_inc_int(uint32_t *acc, size_t acc_size, bool is_increment)
     /* Read back the accumulator register */
     cc3xx_lowlevel_pka_read_reg_swap_endian(r0, acc, CC3XX_DRBG_CTR_BLOCKLEN);
 
-    /* Uninit the engine */
-    cc3xx_lowlevel_pka_uninit();
+    cc3xx_lowlevel_pka_free_reg(r0);
+
+    if (!pka_initialized) {
+        /* Uninit the engine */
+        cc3xx_lowlevel_pka_uninit();
+    }
 }
 
 static inline void long_inc(uint32_t *acc, size_t acc_size)
