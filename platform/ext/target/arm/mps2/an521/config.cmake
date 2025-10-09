e7782d431a56c6f037c09578f1b564c993bf3f48
Dávid Házi <david.hazi@arm.com>
2025-10-09 11:34:46 +0000

    an521: Enable combined hex generation
    Enabled combined hex generation feature for an521 as a reference
    
    Signed-off-by: Dávid Házi <david.hazi@arm.com>
    Change-Id: Ia473a7a4b6332995a92e772307a4b40382b88d66

--- platform/ext/target/arm/mps2/an521/config.cmake
+++ platform/ext/target/arm/mps2/an521/config.cmake
@@ -1,11 +1,13 @@
 #-------------------------------------------------------------------------------
-# Copyright (c) 2020-2024, Arm Limited. All rights reserved.
+# Copyright (c) 2020-2025, Arm Limited. All rights reserved.
 # Copyright (c) 2022 Cypress Semiconductor Corporation (an Infineon company)
 # or an affiliate of Cypress Semiconductor Corporation. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
 #-------------------------------------------------------------------------------
+
+include(hex_generator)
 
 if(BL2)
     set(BL2_TRAILER_SIZE 0x10000 CACHE STRING "Trailer size")
@@ -25,3 +27,12 @@ set(MCUBOOT_USE_PSA_CRYPTO            ON               CACHE BOOL      "Enable t
 set(MCUBOOT_SIGNATURE_TYPE            "EC-P256"        CACHE STRING    "Algorithm to use for signature validation [RSA-2048, RSA-3072, EC-P256, EC-P384]")
 set(MCUBOOT_HW_KEY                    OFF              CACHE BOOL      "Whether to embed the entire public key in the image metadata instead of the hash only")
 set(MCUBOOT_BUILTIN_KEY               ON               CACHE BOOL      "Use builtin key(s) for validation, no public key data is embedded into the image metadata")
+
+set(TFM_MERGE_HEX_FILES               ON                                              CACHE BOOL   "Create merged hex file in the end of the build")
+set(TFM_S_HEX_FILE_PATH               "${CMAKE_BINARY_DIR}/bin/secure_fw.hex"         CACHE STRING "Merged secure hex file's path")
+
+create_tfm_s_hex_merge_list(
+    BL2_TARGET          bl2_hex
+    TFM_S_TARGET        tfm_s_hex
+    TFM_S_SIGNED_TARGET tfm_s_signed_hex
+)
