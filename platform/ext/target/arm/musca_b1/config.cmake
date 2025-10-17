d7f20f76f734caf9cddbdca0c2257522d54af78e
Dávid Házi <david.hazi@arm.com>
2025-10-17 21:28:30 +0000

    musca_b1: Enable hex generation feature
    Signed-off-by: Dávid Házi <david.hazi@arm.com>
    Change-Id: I1b23c082a0fd80e1e58e0c98b77065abc0cba867

--- platform/ext/target/arm/musca_b1/config.cmake
+++ platform/ext/target/arm/musca_b1/config.cmake
@@ -1,11 +1,13 @@
 #-------------------------------------------------------------------------------
-# Copyright (c) 2020-2024, Arm Limited. All rights reserved.
 # Copyright (c) 2022 Cypress Semiconductor Corporation (an Infineon company)
 # or an affiliate of Cypress Semiconductor Corporation. All rights reserved.
+# SPDX-FileCopyrightText: Copyright The TrustedFirmware-M Contributors
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
 #-------------------------------------------------------------------------------
+
+include(hex_generator)
 
 set(CRYPTO_HW_ACCELERATOR               ON          CACHE BOOL      "Whether to enable the crypto hardware accelerator on supported platforms")
 
@@ -37,3 +39,6 @@ set(MCUBOOT_USE_PSA_CRYPTO            ON               CACHE BOOL      "Enable t
 set(MCUBOOT_SIGNATURE_TYPE            "EC-P256"        CACHE STRING    "Algorithm to use for signature validation [RSA-2048, RSA-3072, EC-P256, EC-P384]")
 set(MCUBOOT_HW_KEY                    OFF              CACHE BOOL      "Whether to embed the entire public key in the image metadata instead of the hash only")
 set(MCUBOOT_BUILTIN_KEY               ON               CACHE BOOL      "Use builtin key(s) for validation, no public key data is embedded into the image metadata")
+
+set(TFM_MERGE_HEX_FILES               ON                                              CACHE BOOL   "Create merged hex file in the end of the build")
+set(TFM_S_HEX_FILE_PATH               "${CMAKE_BINARY_DIR}/bin/secure_fw.hex"         CACHE STRING "Merged secure hex file's path")
