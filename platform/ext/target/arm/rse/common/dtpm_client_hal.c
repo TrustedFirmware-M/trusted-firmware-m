9d0ced68e591abeac57529007fb2cd12348364a3
Mudit Sharma <mudit.sharma@arm.com>
2025-10-21 15:48:42 +0100

    RSE: dTPM: Add TCG_DEBUG claim
    TCG debug security_config claim is determined by reading 128 bit DCU
    state. If any of the bit is enabled, `tcg_debug` is set to `true`.
    
    `tfm_plat_get_security_config_data` implementation has been updated with
    the following:
        - Introduce `struct platform_security_config` to pair function
          computing security config with respective event log metadata.
        - Refactor `tfm_plat_get_security_config_data` to loop over an array
          of `struct platform_security_config` and populate security_config
          array passed in by the caller.
    
    Signed-off-by: Mudit Sharma <mudit.sharma@arm.com>
    Change-Id: I77355f4a546af413fbe4b666dad7baf353c51384

--- platform/ext/target/arm/rse/common/dtpm_client_hal.c
+++ platform/ext/target/arm/rse/common/dtpm_client_hal.c
@@ -21,6 +21,11 @@
 #define DEVICE_STATUS_FUNCTIONAL_STATUS_OFFSET 15
 #define DEVICE_STATUS_FUNCTIONAL_STATUS_DEVICE_FUNCTIONAL 0x0
 
+struct platform_security_config {
+    enum tfm_plat_err_t (*security_config_function)(struct security_config_data *security_config);
+    event_log_metadata_t evlog_security_config_metadata;
+};
+
 /* This implementation is platform-specific. The determination of TCG_INSECURE_LIFECYCLE
  * may depend on additional system attributes not reflected in the implementation below.
  */
@@ -73,31 +78,74 @@ static enum tfm_plat_err_t get_tcg_insecure_lifecycle(struct security_config_dat
     return TFM_PLAT_ERR_SUCCESS;
 }
 
+static enum tfm_plat_err_t get_tcg_debug(struct security_config_data *security_config)
+{
+    enum lcm_error_t lcm_err;
+    int i;
+    uint32_t dcu_state[LCM_DCU_WIDTH_IN_BYTES / sizeof(uint32_t)];
+
+    bool tcg_debug = false;
+    const char tcg_debug_name[] = "TCG_DEBUG";
+
+    lcm_err = lcm_dcu_get_enabled(&LCM_DEV_S, (uint8_t *)&dcu_state);
+    if (lcm_err != LCM_ERROR_NONE) {
+        return (enum tfm_plat_err_t)lcm_err;
+    }
+
+    for (i = 0; i < sizeof(dcu_state)/sizeof(uint32_t); i++) {
+        if (dcu_state[i]) {
+            tcg_debug = true;
+            break;
+        }
+    }
+
+    if (sizeof(tcg_debug_name) > MAX_SECURITY_CONFIG_NAME_LEN) {
+        return TFM_PLAT_ERR_MAX_VALUE;
+    }
+
+    security_config->name_length = sizeof(tcg_debug_name);
+    memcpy(&security_config->name, tcg_debug_name, security_config->name_length);
+
+    security_config->data_length = sizeof(bool);
+    security_config->config_data[0] = (uint8_t)tcg_debug;
+
+    return TFM_PLAT_ERR_SUCCESS;
+}
+
 enum tfm_plat_err_t tfm_plat_get_security_config_data(struct security_config *security_config,
                                                       size_t *security_config_len,
                                                       size_t security_config_max_len)
 {
     enum tfm_plat_err_t err;
-    size_t arr_index = 0;
+    size_t arr_index;
 
-    event_log_metadata_t security_config_metadata = {INSECURE_LIFECYCLE_EVENT_ID,
-                                                     "TCG_INSECURE_LIFECYCLE", 1};
+    struct platform_security_config platform_sec_config[] = {
+        {get_tcg_insecure_lifecycle, {INSECURE_LIFECYCLE_EVENT_ID, "TCG_INSECURE_LIFECYCLE", 1}},
+        {get_tcg_debug, {DEBUG_EVENT_ID, "TCG_DEBUG", 1}},
+    };
 
-    if (arr_index + 1 > security_config_max_len) {
+    size_t platform_sec_config_len = sizeof(platform_sec_config)/sizeof(platform_sec_config[0]);
+
+    if (platform_sec_config_len < security_config_max_len) {
         return TFM_PLAT_ERR_MAX_VALUE;
     }
 
-    err = get_tcg_insecure_lifecycle(
-            &security_config[arr_index].security_config_data);
-    if (err != TFM_PLAT_ERR_SUCCESS) {
-        return err;
+    for (arr_index = 0; arr_index < platform_sec_config_len; arr_index++) {
+        err = platform_sec_config[arr_index].security_config_function(
+                                  &security_config[arr_index].security_config_data);
+        if (err != TFM_PLAT_ERR_SUCCESS) {
+            return err;
+        }
+
+        security_config[arr_index].hash_type = PSA_ALG_SHA_256;
+        security_config[arr_index].pcr_index = 1;
+
+        memcpy(&security_config[arr_index].event_log_metadata,
+               &platform_sec_config[arr_index].evlog_security_config_metadata,
+               sizeof(event_log_metadata_t));
+
     }
 
-    security_config[arr_index].hash_type = PSA_ALG_SHA_256;
-    security_config[arr_index].pcr_index = 1;
-    memcpy(&security_config[arr_index].event_log_metadata,
-           &security_config_metadata, sizeof(event_log_metadata_t));
-    arr_index += 1;
     *security_config_len = arr_index;
 
     return TFM_PLAT_ERR_SUCCESS;
