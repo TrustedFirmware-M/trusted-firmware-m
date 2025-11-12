f7d41f407e06424e8b75df2e55a0aaa1d5368e35
Mudit Sharma <mudit.sharma@arm.com>
2025-11-12 17:20:55 +0000

    RP2350: Add default implementation for dtpm_client_hal API
    `tfm_plat_get_security_config_data` default implementation is added with
    `security_config_len` set to 0.
    
    Signed-off-by: Mudit Sharma <mudit.sharma@arm.com>
    Change-Id: I2221937b7b516a5c6565e38fff73b0fbcfe27bf5

---
+++ platform/ext/target/rpi/rp2350/dtpm_client_hal.c
@@ -0,0 +1,19 @@
+/*
+ * SPDX-FileCopyrightText: Copyright The TrustedFirmware-M Contributors
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#include <stdint.h>
+
+#include "dtpm_client_partition_hal.h"
+
+enum tfm_plat_err_t tfm_plat_get_security_config_data(struct security_config *security_config,
+                                                      size_t *security_config_len,
+                                                      size_t security_config_max_len)
+{
+    *security_config_len = 0;
+    (void)security_config;
+    (void)security_config_max_len;
+}
