c64bd106350090450d4b402f827764c63615bff7
Mudit Sharma <mudit.sharma@arm.com>
2025-11-19 14:41:59 +0000

    RP2350: Add missing return from platform function
    `tfm_plat_get_security_config_data` for RP2350 was missing concluding
    return which this commit adds.
    
    Signed-off-by: Mudit Sharma <mudit.sharma@arm.com>
    Change-Id: Ice671231f851f6d2a76bd1043563648337ab8739

--- platform/ext/target/rpi/rp2350/dtpm_client_hal.c
+++ platform/ext/target/rpi/rp2350/dtpm_client_hal.c
@@ -16,4 +16,6 @@ enum tfm_plat_err_t tfm_plat_get_security_config_data(struct security_config *se
     *security_config_len = 0;
     (void)security_config;
     (void)security_config_max_len;
+
+    return TFM_PLAT_ERR_SUCCESS;
 }
