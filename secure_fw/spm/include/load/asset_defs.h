71edc9dfaa1175361c6223b6f738e30816492f9c
Nicola Mazzucato <nicola.mazzucato@arm.com>
2025-12-16 14:01:01 +0000

    SPM: defs: Prefer enumerator for asset flags
    Switch asset flags to a typed enum, reducing misuse of
    MMIO/PPB attribute bits.
    
    Signed-off-by: Nicola Mazzucato <nicola.mazzucato@arm.com>
    Change-Id: I69dfbe0e63badc46bd2130f2411083d75c0637a0

--- secure_fw/spm/include/load/asset_defs.h
+++ secure_fw/spm/include/load/asset_defs.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Arm Limited. All rights reserved.
+ * SPDX-FileCopyrightText: Copyright The TrustedFirmware-M Contributors
  *
  * SPDX-License-Identifier: BSD-3-Clause
  *
@@ -9,28 +9,34 @@
 
 #include <stdint.h>
 
-/* Memory, MMIO and PPB asset attributes */
-#define ASSET_ATTR_READ_ONLY        (1U << 0)  /* 1: Read-only MMIO         */
-#define ASSET_ATTR_READ_WRITE       (1U << 1)  /* 1: Read-write MMIO        */
-#define ASSET_ATTR_PPB              (1U << 2)  /* 1: PPB indicator          */
+/**
+ * \brief Memory, MMIO and PPB asset attributes.
+ */
+enum assets_attribute_t {
+    ASSET_ATTR_READ_ONLY =      (1U << 0), /*!< 1: Read-only MMIO */
+    ASSET_ATTR_READ_WRITE =     (1U << 1), /*!< 1: Read-write MMIO */
+    ASSET_ATTR_PPB =            (1U << 2), /*!< 1: PPB indicator */
+    ASSET_ATTR_NAMED_MMIO =     (1U << 3), /*!< 1: Named MMIO object */
+    ASSET_ATTR_NUMBERED_MMIO =  (1U << 4), /*!< 1: Numbered MMIO object */
+};
 
-#define ASSET_ATTR_NAMED_MMIO       (1U << 3)  /* 1: Named mmio object       */
-#define ASSET_ATTR_NUMBERED_MMIO    (1U << 4)  /* 1: Numbered mmio object    */
-#define ASSET_ATTR_MMIO             (ASSET_ATTR_NAMED_MMIO | \
-                                     ASSET_ATTR_NUMBERED_MMIO)
+#define ASSET_ATTR_MMIO (ASSET_ATTR_NAMED_MMIO | ASSET_ATTR_NUMBERED_MMIO)
 
+/**
+ * \brief Asset descriptor.
+ */
 struct asset_desc_t {
     union {
-        struct {                            /* Memory-based asset type  */
+        struct {                            /*!< Memory-based asset type */
             uintptr_t start;
             uintptr_t limit;
         } mem;
-        struct {                            /* Device asset type        */
+        struct {                            /*!< Device asset type */
             uintptr_t dev_ref;
             uintptr_t reserved;
         } dev;
     };
-    uint32_t attr;                          /* Asset attributes         */
+    enum assets_attribute_t attr;           /*!< Assets attributes */
 } __attribute__((aligned(4)));
 
 #endif /* __ASSET_DEFS_H__ */
