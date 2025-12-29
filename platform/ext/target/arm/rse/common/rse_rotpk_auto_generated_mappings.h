TrustedFirmware-M/trusted-firmware-m

commit fa017e0909231c83589bfa8cf3a8c40e33f4ed6b

RSE: Update ROTPK to use new OTP layout
Add default config and mapping with option for subplatforms to override
by config parameter

Change-Id: I5c592b82efb1b8116f21d588750daaf1fe27b34c
Signed-off-by: Raef Coles <raef.coles@arm.com>

platform/ext/target/arm/rse/common/rse_rotpk_auto_generated_mappings.h
@@ -1,0 +1,56 @@
+/*
+ * SPDX-FileCopyrightText: Copyright The TrustedFirmware-M Contributors
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#ifndef __RSE_ROTPK_AUTO_GENERATED_MAPPINGS_H__
+#define __RSE_ROTPK_AUTO_GENERATED_MAPPINGS_H__
+
+#include <stdint.h>
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum rse_rotpk_cm_mapping_t {
+    CM_ROTPK_BL1_0 = 0,
+    CM_ROTPK_BL1_MAX = CM_ROTPK_BL1_0 + RSE_ROTPK_CM_BL1_AMOUNT,
+
+    CM_ROTPK_BL2_0,
+    CM_ROTPK_BL2_MAX = CM_ROTPK_BL2_0 + RSE_ROTPK_CM_BL2_AMOUNT,
+
+    CM_ROTPK_HOST_0,
+    CM_ROTPK_HOST_MAX = CM_ROTPK_HOST_0 + RSE_ROTPK_CM_HOST_AMOUNT,
+
+    CM_ROTPK_SECURE_DEBUG_0,
+    CM_ROTPK_SECURE_DEBUG_0_MAX = CM_ROTPK_SECURE_DEBUG_0 + RSE_ROTPK_CM_SECURE_DEBUG_AMOUNT,
+
+    CM_ROTPK_SUBPLATFORM_0,
+    CM_ROTPK_SUBPLATFORM_MAX = CM_ROTPK_HOST_0 + RSE_ROTPK_CM_HOST_AMOUNT,
+};
+
+enum rse_rotpk_dm_mapping_t {
+    DM_ROTPK_BL1_0 = 0,
+    DM_ROTPK_BL1_MAX = DM_ROTPK_BL1_0 + RSE_ROTPK_DM_BL1_AMOUNT,
+
+    DM_ROTPK_BL2_0,
+    DM_ROTPK_BL2_MAX = DM_ROTPK_BL2_0 + RSE_ROTPK_DM_BL2_AMOUNT,
+
+    DM_ROTPK_HOST_0,
+    DM_ROTPK_HOST_MAX = DM_ROTPK_HOST_0 + RSE_ROTPK_DM_HOST_AMOUNT,
+
+    DM_ROTPK_SECURE_DEBUG_0,
+    DM_ROTPK_SECURE_DEBUG_0_MAX = DM_ROTPK_SECURE_DEBUG_0 + RSE_ROTPK_DM_SECURE_DEBUG_AMOUNT,
+
+    DM_ROTPK_SUBPLATFORM_0,
+    DM_ROTPK_SUBPLATFORM_MAX = DM_ROTPK_HOST_0 + RSE_ROTPK_DM_HOST_AMOUNT,
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __RSE_ROTPK_AUTO_GENERATED_MAPPINGS_H__ */

platform/ext/target/arm/rse/common/CMakeLists.txt
@@ -208,6 +208,7 @@ target_sources(platform_s
         ${PLATFORM_DIR}/ext/target/arm/drivers/lcm/lcm_drv.c
         otp_lcm.c
         nv_counters.c
+        $<$<BOOL:${RSE_SUBPLATFORM_PAL_DEFAULT_ROTPK}>:${CMAKE_CURRENT_SOURCE_DIR}/rse_rotpk_mapping.c>
         attest_hal.c
         tfm_hal_platform_reset.c
         $<$<BOOL:${PLATFORM_SVC_HANDLERS}>:${CMAKE_CURRENT_SOURCE_DIR}/platform_svc_handler.c>
@@ -318,6 +319,7 @@ target_sources(platform_bl2
         native_drivers/integrity_checker_drv.c
         otp_lcm.c
         nv_counters.c
+        $<$<BOOL:${RSE_SUBPLATFORM_PAL_DEFAULT_ROTPK}>:${CMAKE_CURRENT_SOURCE_DIR}/rse_rotpk_mapping.c>
         $<$<BOOL:${RSE_XIP}>:${CMAKE_CURRENT_SOURCE_DIR}/bl2/sic_boot.c>
         ${PLATFORM_DIR}/ext/target/arm/drivers/gpio/pl061/gpio_pl061_drv.c
         ./platform_fatal_error.c
@@ -409,6 +411,7 @@ target_sources(platform_bl1_1
         ./native_drivers/tram_drv.c
         ./native_drivers/integrity_checker_drv.c
         ./nv_counters.c
+        $<$<BOOL:${RSE_SUBPLATFORM_PAL_DEFAULT_ROTPK}>:${CMAKE_CURRENT_SOURCE_DIR}/rse_rotpk_mapping.c>
         ./otp_lcm.c
         ./tfm_hal_platform_reset.c
         $<$<BOOL:${RSE_DEBUG_UART}>:${CMAKE_CURRENT_SOURCE_DIR}/cmsis_drivers/Driver_USART_cmsdk.c>

platform/ext/target/arm/rse/common/config.cmake
@@ -210,7 +210,9 @@ set(MCUBOOT_SIGNATURE_TYPE            "EC-P256"        CACHE STRING    "Algorith
 set(MCUBOOT_HW_KEY                    ON               CACHE BOOL      "Whether to embed the entire public key in the image metadata instead of the hash only")
 set(MCUBOOT_BUILTIN_KEY               OFF              CACHE BOOL      "Use builtin key(s) for validation, no public key data is embedded into the image metadata")
 
+set(RSE_SUBPLATFORM_PAL_DEFAULT_ROTPK       ON             CACHE BOOL      "Use default subplatform PAL rotpk mapping")
 set(RSE_SUBPLATFORM_PAL_CONFIG_DIR "${CMAKE_CURRENT_LIST_DIR}/subplatform_pal_default_config" CACHE PATH "path where subplatform PAL config headers can be found")
+
 ################################################################################
 
 # Specifying the accepted values for certain configuration options to facilitate

platform/ext/target/arm/rse/common/rse_rotpk_mapping.c
@@ -1,0 +1,110 @@
+/*
+ * SPDX-FileCopyrightText: Copyright The TrustedFirmware-M Contributors
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+
+#include "rse_rotpk_mapping.h"
+#include "rse_otp_dev.h"
+
+#include "rse_rotpk_config.h"
+
+enum tfm_plat_err_t rse_rotpk_get_type(enum tfm_otp_element_id_t otp_id,
+                                       enum rse_rotpk_type *type)
+{
+
+    enum tfm_otp_element_id_t rotpk_base;
+    uint32_t policy_word;
+    uint32_t rotpk_index;
+
+    if (otp_id >= PLAT_OTP_ID_CM_ROTPK && otp_id < PLAT_OTP_ID_CM_ROTPK_MAX) {
+        rotpk_base = PLAT_OTP_ID_CM_ROTPK;
+        policy_word = P_RSE_OTP_CM->rotpk_policies;
+    } else if (otp_id >= PLAT_OTP_ID_DM_ROTPK && otp_id < PLAT_OTP_ID_DM_ROTPK_MAX) {
+        rotpk_base = PLAT_OTP_ID_DM_ROTPK;
+        policy_word = P_RSE_OTP_DM->rotpk_policies;
+    } else {
+        return TFM_PLAT_ERR_ROTPK_GET_TYPE_INVALID_ID;
+    }
+
+    rotpk_index = otp_id - rotpk_base;
+
+    *type = (policy_word >> (2 * rotpk_index)) & 0b11;
+    return TFM_PLAT_ERR_SUCCESS;
+}
+
+enum tfm_plat_err_t rse_rotpk_get_policy(enum tfm_otp_element_id_t otp_id,
+                                         enum rse_rotpk_policy *policy)
+{
+    enum tfm_otp_element_id_t rotpk_base;
+    uint32_t policy_word;
+    uint32_t rotpk_index;
+
+    if (otp_id >= PLAT_OTP_ID_CM_ROTPK && otp_id < PLAT_OTP_ID_CM_ROTPK_MAX) {
+        rotpk_base = PLAT_OTP_ID_CM_ROTPK;
+        policy_word = P_RSE_OTP_CM->rotpk_policies;
+    } else if (otp_id >= PLAT_OTP_ID_DM_ROTPK && otp_id < PLAT_OTP_ID_DM_ROTPK_MAX) {
+        rotpk_base = PLAT_OTP_ID_DM_ROTPK;
+        policy_word = P_RSE_OTP_DM->rotpk_policies;
+    } else {
+        return TFM_PLAT_ERR_ROTPK_GET_POLICY_INVALID_ID;
+    }
+
+    rotpk_index = otp_id - rotpk_base;
+
+    *policy = (policy_word >> (16 + rotpk_index)) & 0b1;
+    return TFM_PLAT_ERR_SUCCESS;
+}
+
+
+enum tfm_otp_element_id_t rse_cm_get_bl1_rotpk()
+{
+    return PLAT_OTP_ID_CM_ROTPK + RSE_CM_OTP_ID_FOR_BL1_IMAGE();
+}
+
+enum tfm_otp_element_id_t rse_cm_get_bl2_rotpk(uint32_t image_id)
+{
+    return PLAT_OTP_ID_CM_ROTPK + RSE_CM_OTP_ID_FOR_BL2_IMAGE(image_id);
+}
+
+enum tfm_otp_element_id_t rse_cm_get_host_rotpk(uint32_t host_rotpk_id)
+{
+    return PLAT_OTP_ID_CM_ROTPK + RSE_CM_OTP_ID_FOR_HOST(host_rotpk_id);
+}
+
+enum tfm_otp_element_id_t rse_cm_get_secure_debug_rotpk()
+{
+    return PLAT_OTP_ID_CM_ROTPK + RSE_CM_OTP_ID_FOR_SECURE_DEBUG();
+}
+
+enum tfm_otp_element_id_t rse_cm_get_subplatform_rotpk(uint32_t subplatform_rotpk_id)
+{
+    return PLAT_OTP_ID_CM_ROTPK + RSE_CM_OTP_ID_FOR_SUBPLATFORM(subplatform_rotpk_id);
+}
+
+enum tfm_otp_element_id_t rse_dm_get_bl1_rotpk()
+{
+    return PLAT_OTP_ID_DM_ROTPK + RSE_DM_OTP_ID_FOR_BL1_IMAGE();
+}
+
+enum tfm_otp_element_id_t rse_dm_get_bl2_rotpk(uint32_t image_id)
+{
+    return PLAT_OTP_ID_DM_ROTPK + RSE_DM_OTP_ID_FOR_BL2_IMAGE(image_id);
+}
+
+enum tfm_otp_element_id_t rse_dm_get_host_rotpk(uint32_t host_rotpk_id)
+{
+    return PLAT_OTP_ID_DM_ROTPK + RSE_DM_OTP_ID_FOR_HOST(host_rotpk_id);
+}
+
+enum tfm_otp_element_id_t rse_dm_get_secure_debug_rotpk()
+{
+    return PLAT_OTP_ID_DM_ROTPK + RSE_DM_OTP_ID_FOR_SECURE_DEBUG();
+}
+
+enum tfm_otp_element_id_t rse_dm_get_subplatform_rotpk(uint32_t subplatform_rotpk_id)
+{
+    return PLAT_OTP_ID_DM_ROTPK + RSE_DM_OTP_ID_FOR_SUBPLATFORM(subplatform_rotpk_id);
+}

platform/ext/target/arm/rse/common/rse_rotpk_mapping.h
@@ -1,0 +1,49 @@
+/*
+ * SPDX-FileCopyrightText: Copyright The TrustedFirmware-M Contributors
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#ifndef __RSE_ROTPK_MAPPING_H__
+#define __RSE_ROTPK_MAPPING_H__
+
+#include "tfm_plat_otp.h"
+
+enum rse_rotpk_type {
+    RSE_ROTPK_TYPE_ECDSA = 0b00,
+    RSE_ROTPK_TYPE_LMS   = 0b01,
+};
+
+enum rse_rotpk_policy {
+    RSE_ROTPK_POLICY_SIG_OPTIONAL = 0b00,
+    RSE_ROTPK_POLICY_SIG_REQUIRED = 0b01,
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum tfm_plat_err_t rse_rotpk_get_type(enum tfm_otp_element_id_t otp_id,
+                                       enum rse_rotpk_type *type);
+
+enum tfm_plat_err_t rse_rotpk_get_policy(enum tfm_otp_element_id_t otp_id,
+                                         enum rse_rotpk_policy *policy);
+
+enum tfm_otp_element_id_t rse_cm_get_bl1_rotpk();
+enum tfm_otp_element_id_t rse_cm_get_bl2_rotpk(uint32_t image_id);
+enum tfm_otp_element_id_t rse_cm_get_host_rotpk(uint32_t host_rotpk_id);
+enum tfm_otp_element_id_t rse_cm_get_secure_debug_rotpk();
+enum tfm_otp_element_id_t rse_cm_get_subplatform_rotpk(uint32_t subplatform_rotpk_id);
+
+enum tfm_otp_element_id_t rse_dm_get_bl1_rotpk();
+enum tfm_otp_element_id_t rse_dm_get_bl2_rotpk(uint32_t image_id);
+enum tfm_otp_element_id_t rse_dm_get_host_rotpk(uint32_t host_rotpk_id);
+enum tfm_otp_element_id_t rse_dm_get_secure_debug_rotpk();
+enum tfm_otp_element_id_t rse_dm_get_subplatform_rotpk(uint32_t subplatform_rotpk_id);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __RSE_ROTPK_MAPPING_H__ */

platform/ext/target/arm/rse/common/rse_rotpk_policy.h
@@ -1,0 +1,36 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ * SPDX-FileCopyrightText: Copyright The TrustedFirmware-M Contributors
+ */
+
+#ifndef __RSE_ROTPK_POLICY_H__
+#define __RSE_ROTPK_POLICY_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum rse_rotpk_type {
+    RSE_ROTPK_TYPE_ECDSA = 0b00,
+    RSE_ROTPK_TYPE_LMS   = 0b01,
+};
+
+enum rse_rotpk_policy {
+    RSE_ROTPK_POLICY_SIG_OPTIONAL = 0b00,
+    RSE_ROTPK_POLICY_SIG_REQUIRED = 0b01,
+};
+
+enum rse_rotpk_hash_alg {
+    RSE_ROTPK_HASH_ALG_SHA256 = 0b00,
+    RSE_ROTPK_HASH_ALG_SHA384 = 0b01,
+};
+
+#define RSE_ROTPK_SIZE_FROM_ALG(x) (alg == RSE_ROTPK_HASH_ALG_SHA256 ? 32 : \
+                                    alg == RSE_ROTPK_HASH_ALG_SHA384 ? 48 : \
+                                    0)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __RSE_ROTPK_POLICY_H__ */

platform/ext/target/arm/rse/common/subplatform_pal_default_config/rse_rotpk_config.h
@@ -1,0 +1,68 @@
+/*
+ * SPDX-FileCopyrightText: Copyright The TrustedFirmware-M Contributors
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#ifndef __RSE_ROTPK_CONFIG_H__
+#define __RSE_ROTPK_CONFIG_H__
+
+enum rse_host_rotpk_id {
+    RSE_HOST_ROTPK_S,
+    RSE_HOST_ROTPK_NS,
+    RSE_HOST_ROTPK_CCA,
+};
+
+#define RSE_ROTPK_CM_BL1_AMOUNT  1
+#define RSE_ROTPK_CM_BL2_AMOUNT  1
+#define RSE_ROTPK_CM_HOST_AMOUNT 1
+#define RSE_ROTPK_CM_SECURE_DEBUG_AMOUNT 1
+#define RSE_ROTPK_CM_SUBPLATFORM_AMOUNT 0
+
+#define RSE_ROTPK_DM_BL1_AMOUNT  0
+#define RSE_ROTPK_DM_BL2_AMOUNT  1
+#define RSE_ROTPK_DM_HOST_AMOUNT 2
+#define RSE_ROTPK_DM_SECURE_DEBUG_AMOUNT 0
+#define RSE_ROTPK_DM_SUBPLATFORM_AMOUNT 0
+
+#include "rse_rotpk_auto_generated_mappings.h"
+
+/* ========================== CM Mappings ====================================*/
+
+#define RSE_CM_OTP_ID_FOR_BL1_IMAGE() \
+    (RSE_ROTPK_CM_BL1_AMOUNT ? CM_ROTPK_BL1_0 : PLAT_OTP_ID_INVALID)
+
+#define RSE_CM_OTP_ID_FOR_BL2_IMAGE(id) \
+    (RSE_ROTPK_CM_BL2_AMOUNT && \
+    (id) != 1 ? CM_ROTPK_BL2_0 : PLAT_OTP_ID_INVALID)
+
+#define RSE_CM_OTP_ID_FOR_HOST(id) \
+    (RSE_ROTPK_CM_HOST_AMOUNT ? CM_ROTPK_HOST_0 + (id) : PLAT_OTP_ID_INVALID)
+
+#define RSE_CM_OTP_ID_FOR_SECURE_DEBUG() \
+    (RSE_ROTPK_CM_SECURE_DEBUG_AMOUNT ? CM_ROTPK_SECURE_DEBUG_0 : PLAT_OTP_ID_INVALID)
+
+#define RSE_CM_OTP_ID_FOR_SUBPLATFORM(id) \
+    (RSE_ROTPK_CM_SUBPLATFORM_AMOUNT ? CM_ROTPK_SUBPLATFORM_0 + (id) : PLAT_OTP_ID_INVALID)
+
+/* ========================== DM Mappings ====================================*/
+
+#define RSE_DM_OTP_ID_FOR_BL1_IMAGE() \
+    (RSE_ROTPK_DM_BL1_AMOUNT ? DM_ROTPK_BL1_0 : PLAT_OTP_ID_INVALID)
+
+#define RSE_DM_OTP_ID_FOR_BL2_IMAGE(id) \
+    (RSE_ROTPK_DM_BL2_AMOUNT && \
+    (id) == 1 ? DM_ROTPK_BL2_0 : PLAT_OTP_ID_INVALID)
+
+#define RSE_DM_OTP_ID_FOR_HOST(id) \
+    (RSE_ROTPK_DM_HOST_AMOUNT ? DM_ROTPK_HOST_0 + (id) : PLAT_OTP_ID_INVALID)
+
+#define RSE_DM_OTP_ID_FOR_SECURE_DEBUG() \
+    (RSE_ROTPK_DM_SECURE_DEBUG_AMOUNT ? DM_ROTPK_SECURE_DEBUG_0 : PLAT_OTP_ID_INVALID)
+
+#define RSE_DM_OTP_ID_FOR_SUBPLATFORM(id) \
+    (RSE_ROTPK_DM_SUBPLATFORM_AMOUNT ? DM_ROTPK_SUBPLATFORM_0 + (id) : PLAT_OTP_ID_INVALID)
+
+
+#endif /* __RSE_ROTPK_CONFIG_H__ */

commit 220eb3f2a0e19ed27f63aa27b5e50a3d56080918

RSE: common: fix subplatform ROTPK ranges
The subplatform ROTPK ranges are currently mis-specified to end where the
host ROTPK ranges end; fix that.

While at it, fix the typo in "SECURE_DEBUG_0_MAX" -- it should be
"SECURE_DEBUG_MAX".

Change-Id: I484bb44adfe0bdf8839232ccb101f66719a30cba
Fixes: fa017e0909231
Signed-off-by: Laszlo Ersek <laszlo.ersek@arm.com>

platform/ext/target/arm/rse/common/rse_rotpk_auto_generated_mappings.h
@@ -26,10 +26,10 @@ enum rse_rotpk_cm_mapping_t {
     CM_ROTPK_HOST_MAX = CM_ROTPK_HOST_0 + RSE_ROTPK_CM_HOST_AMOUNT,
 
     CM_ROTPK_SECURE_DEBUG_0 = CM_ROTPK_HOST_MAX,
-    CM_ROTPK_SECURE_DEBUG_0_MAX = CM_ROTPK_SECURE_DEBUG_0 + RSE_ROTPK_CM_SECURE_DEBUG_AMOUNT,
+    CM_ROTPK_SECURE_DEBUG_MAX = CM_ROTPK_SECURE_DEBUG_0 + RSE_ROTPK_CM_SECURE_DEBUG_AMOUNT,
 
-    CM_ROTPK_SUBPLATFORM_0 = CM_ROTPK_SECURE_DEBUG_0_MAX,
-    CM_ROTPK_SUBPLATFORM_MAX = CM_ROTPK_HOST_0 + RSE_ROTPK_CM_HOST_AMOUNT,
+    CM_ROTPK_SUBPLATFORM_0 = CM_ROTPK_SECURE_DEBUG_MAX,
+    CM_ROTPK_SUBPLATFORM_MAX = CM_ROTPK_SUBPLATFORM_0 + RSE_ROTPK_CM_SUBPLATFORM_AMOUNT,
 
     CM_ROTPK_CNT = CM_ROTPK_SUBPLATFORM_MAX
 };
@@ -45,13 +45,12 @@ enum rse_rotpk_dm_mapping_t {
     DM_ROTPK_HOST_MAX = DM_ROTPK_HOST_0 + RSE_ROTPK_DM_HOST_AMOUNT,
 
     DM_ROTPK_SECURE_DEBUG_0 = DM_ROTPK_HOST_MAX,
-    DM_ROTPK_SECURE_DEBUG_0_MAX = DM_ROTPK_SECURE_DEBUG_0 + RSE_ROTPK_DM_SECURE_DEBUG_AMOUNT,
+    DM_ROTPK_SECURE_DEBUG_MAX = DM_ROTPK_SECURE_DEBUG_0 + RSE_ROTPK_DM_SECURE_DEBUG_AMOUNT,
 
-    DM_ROTPK_SUBPLATFORM_0 = DM_ROTPK_SECURE_DEBUG_0_MAX,
-    DM_ROTPK_SUBPLATFORM_MAX = DM_ROTPK_HOST_0 + RSE_ROTPK_DM_HOST_AMOUNT,
+    DM_ROTPK_SUBPLATFORM_0 = DM_ROTPK_SECURE_DEBUG_MAX,
+    DM_ROTPK_SUBPLATFORM_MAX = DM_ROTPK_SUBPLATFORM_0 + RSE_ROTPK_DM_SUBPLATFORM_AMOUNT,
 
     DM_ROTPK_CNT = DM_ROTPK_SUBPLATFORM_MAX
-
 };
 
 #ifdef __cplusplus