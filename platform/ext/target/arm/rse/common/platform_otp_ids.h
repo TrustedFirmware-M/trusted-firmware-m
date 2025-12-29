TrustedFirmware-M/trusted-firmware-m

commit de988a7f0f4dfe2e8bf91ee60e96ba3d511be1cb

RSE: Update OTP layout
To make it more easily usable by different subplatforms. Add
"single source of truth" OTP config header. Add device header to support
efficient OTP access without using OTP HAL.

Change-Id: I1858c3ae906afa58c15ed07d4d53b5e4483ae741
Signed-off-by: Raef Coles <raef.coles@arm.com>

platform/ext/target/arm/rse/common/platform_otp_ids.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022-2024, Arm Limited. All rights reserved.
+ * SPDX-FileCopyrightText: Copyright The TrustedFirmware-M Contributors
  *
  * SPDX-License-Identifier: BSD-3-Clause
  *
@@ -9,6 +9,7 @@
 #define __PLATFORM_OTP_IDS_H__
 
 #include <stdint.h>
+#include "rse_otp_config.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -20,85 +21,75 @@ enum tfm_otp_element_id_t {
 
     PLAT_OTP_ID_LCS,
 
-    PLAT_OTP_ID_IAK_LEN,
-    PLAT_OTP_ID_IAK_TYPE,
-    PLAT_OTP_ID_IAK_ID,
-
+    PLAT_OTP_ID_BOOT_SEED,
     PLAT_OTP_ID_IMPLEMENTATION_ID,
+    PLAT_OTP_ID_CERT_REF,
     PLAT_OTP_ID_VERIFICATION_SERVICE_URL,
     PLAT_OTP_ID_PROFILE_DEFINITION,
 
-    /* BL2 ROTPK must be contiguous */
-    PLAT_OTP_ID_BL2_ROTPK_0,
-    PLAT_OTP_ID_BL2_ROTPK_1,
-    PLAT_OTP_ID_BL2_ROTPK_2,
-    PLAT_OTP_ID_BL2_ROTPK_3,
-    PLAT_OTP_ID_BL2_ROTPK_4,
-    PLAT_OTP_ID_BL2_ROTPK_5,
-    PLAT_OTP_ID_BL2_ROTPK_6,
-    PLAT_OTP_ID_BL2_ROTPK_7,
-    PLAT_OTP_ID_BL2_ROTPK_8,
+    PLAT_OTP_ID_CM_ROTPK,
+    PLAT_OTP_ID_CM_ROTPK_MAX = PLAT_OTP_ID_CM_ROTPK + RSE_OTP_CM_ROTPK_AMOUNT,
+    PLAT_OTP_ID_CM_ROTPK_REPROVISIONING,
 
-    /* BL2 NV counters must be contiguous */
-    PLAT_OTP_ID_NV_COUNTER_BL2_0,
-    PLAT_OTP_ID_NV_COUNTER_BL2_1,
-    PLAT_OTP_ID_NV_COUNTER_BL2_2,
-    PLAT_OTP_ID_NV_COUNTER_BL2_3,
-    PLAT_OTP_ID_NV_COUNTER_BL2_4,
-    PLAT_OTP_ID_NV_COUNTER_BL2_5,
-    PLAT_OTP_ID_NV_COUNTER_BL2_6,
-    PLAT_OTP_ID_NV_COUNTER_BL2_7,
-    PLAT_OTP_ID_NV_COUNTER_BL2_8,
+    PLAT_OTP_ID_DM_ROTPK,
+    PLAT_OTP_ID_DM_ROTPK_MAX = PLAT_OTP_ID_DM_ROTPK + RSE_OTP_DM_ROTPK_AMOUNT,
+    PLAT_OTP_ID_DM_ROTPK_REPROVISIONING,
 
-#ifdef PLATFORM_HAS_PS_NV_OTP_COUNTERS
-    PLAT_OTP_ID_NV_COUNTER_PS_0,
-    PLAT_OTP_ID_NV_COUNTER_PS_1,
-    PLAT_OTP_ID_NV_COUNTER_PS_2,
-#endif /* PLATFORM_HAS_PS_NV_OTP_COUNTERS */
+    PLAT_OTP_ID_KRTL_USAGE_COUNTER,
+    PLAT_OTP_ID_LFT_COUNTER,
 
-    PLAT_OTP_ID_NV_COUNTER_NS_0,
-    PLAT_OTP_ID_NV_COUNTER_NS_1,
-    PLAT_OTP_ID_NV_COUNTER_NS_2,
+    PLAT_OTP_ID_CM_CONFIG_FLAGS,
+    PLAT_OTP_ID_DM_CONFIG_FLAGS,
 
-    PLAT_OTP_ID_KEY_BL2_ENCRYPTION,
-    PLAT_OTP_ID_KEY_SECURE_ENCRYPTION,
-    PLAT_OTP_ID_KEY_NON_SECURE_ENCRYPTION,
+    PLAT_OTP_ID_CM_ROTPK_POLICIES,
+    PLAT_OTP_ID_DM_ROTPK_POLICIES,
+
+    PLAT_OTP_ID_NV_COUNTER_BANK_0_COUNTER,
+    PLAT_OTP_ID_NV_COUNTER_BANK_0_COUNTER_MAX =
+        PLAT_OTP_ID_NV_COUNTER_BANK_0_COUNTER + RSE_OTP_NV_COUNTERS_BANK_0_AMOUNT,
+    PLAT_OTP_ID_NV_COUNTER_BANK_1_COUNTER,
+    PLAT_OTP_ID_NV_COUNTER_BANK_1_COUNTER_MAX =
+        PLAT_OTP_ID_NV_COUNTER_BANK_1_COUNTER + RSE_OTP_NV_COUNTERS_BANK_1_AMOUNT,
+    PLAT_OTP_ID_NV_COUNTER_BANK_2_COUNTER,
+    PLAT_OTP_ID_NV_COUNTER_BANK_2_COUNTER_MAX =
+        PLAT_OTP_ID_NV_COUNTER_BANK_2_COUNTER + RSE_OTP_NV_COUNTERS_BANK_2_AMOUNT,
+    PLAT_OTP_ID_NV_COUNTER_BANK_3_COUNTER,
+    PLAT_OTP_ID_NV_COUNTER_BANK_3_COUNTER_MAX =
+        PLAT_OTP_ID_NV_COUNTER_BANK_3_COUNTER + RSE_OTP_NV_COUNTERS_BANK_3_AMOUNT,
 
     PLAT_OTP_ID_BL1_2_IMAGE,
     PLAT_OTP_ID_BL1_2_IMAGE_LEN,
     PLAT_OTP_ID_BL1_2_IMAGE_HASH,
-    PLAT_OTP_ID_BL1_ROTPK_0,
-
-    PLAT_OTP_ID_NV_COUNTER_BL1_0,
-
-    PLAT_OTP_ID_SECURE_DEBUG_PK,
-
-    PLAT_OTP_ID_HOST_ROTPK_S,
-    PLAT_OTP_ID_HOST_ROTPK_NS,
-    PLAT_OTP_ID_HOST_ROTPK_CCA,
-
-    PLAT_OTP_ID_CCA_SYSTEM_PROPERTIES,
 
     PLAT_OTP_ID_REPROVISIONING_BITS,
-    PLAT_OTP_ID_RSE_ID,
 
-    PLAT_OTP_ID_CM_CONFIG_FLAGS,
-    PLAT_OTP_ID_DM_CONFIG_FLAGS,
+#ifdef RSE_OTP_HAS_RSE_ID
+    PLAT_OTP_ID_RSE_ID,
+#endif
+
+#ifdef RSE_OTP_HAS_CCA_ITEMS
+    PLAT_OTP_ID_CCA_SYSTEM_CONFIG,
+#endif
 
     PLAT_OTP_ID_DMA_ICS,
 
     PLAT_OTP_ID_MANUFACTURING_DATA_LEN,
     PLAT_OTP_ID_MANUFACTURING_DATA,
 
-    PLAT_OTP_ID_ROM_OTP_ENCRYPTION_KEY,
-    PLAT_OTP_ID_RUNTIME_OTP_ENCRYPTION_KEY,
+    PLAT_OTP_ID_CM_CODE_ENCRYPTION_KEY,
+    PLAT_OTP_ID_CM_PROVISIONING_KEY,
 
+    PLAT_OTP_ID_DM_CODE_ENCRYPTION_KEY,
+    PLAT_OTP_ID_DM_PROVISIONING_KEY,
+
+#ifdef RSE_OTP_HAS_ROUTING_TABLES
     PLAT_OTP_ID_RSE_TO_RSE_SENDER_ROUTING_TABLE,
     PLAT_OTP_ID_RSE_TO_RSE_RECEIVER_ROUTING_TABLE,
-
-    PLAT_OTP_ID_ATTACK_TRACKING_BITS,
+#endif
 
     PLAT_OTP_ID_MAX,
+
+    PLAT_OTP_ID_INVALID,
 
     _PLAT_OTP_ID_PAD = UINT32_MAX,
 };

platform/ext/target/arm/rse/common/CMakeLists.txt
@@ -1,5 +1,5 @@
 #-------------------------------------------------------------------------------
-# Copyright (c) 2020-2024, Arm Limited. All rights reserved.
+# SPDX-FileCopyrightText: Copyright The TrustedFirmware-M Contributors
 # Copyright (c) 2022 Cypress Semiconductor Corporation (an Infineon company)
 # or an affiliate of Cypress Semiconductor Corporation. All rights reserved.
 #
@@ -171,6 +171,7 @@ target_include_directories(platform_s
         ${PLATFORM_DIR}/ext/target/arm/drivers/cc3xx/low_level_driver/include
         ${PLATFORM_DIR}/ext/target/arm/drivers/cc3xx/common
         soft_crc
+        ${RSE_SUBPLATFORM_PAL_CONFIG_DIR}
 )
 
 target_sources(platform_s
@@ -282,6 +283,7 @@ if (TFM_PARTITION_CRYPTO)
             ./native_drivers
             ./device/include
             ./device/config
+            ${RSE_SUBPLATFORM_PAL_CONFIG_DIR}
     )
 endif()
 
@@ -357,6 +359,7 @@ target_include_directories(platform_bl2
         ./bringup_helpers
         ./cc3xx
         bl2/cc3xx
+        ${RSE_SUBPLATFORM_PAL_CONFIG_DIR}
     PRIVATE
         ${PLATFORM_DIR}/..
 )
@@ -461,6 +464,7 @@ target_include_directories(platform_bl1_1_interface
         ./cc3xx
         ./bl1/cc3xx
         soft_crc
+        ${RSE_SUBPLATFORM_PAL_CONFIG_DIR}
 )
 
 target_compile_definitions(platform_bl1_1_interface

platform/ext/target/arm/rse/common/config.cmake
@@ -1,5 +1,5 @@
 #-------------------------------------------------------------------------------
-# Copyright (c) 2022-2024, Arm Limited. All rights reserved.
+# SPDX-FileCopyrightText: Copyright The TrustedFirmware-M Contributors
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
@@ -210,6 +210,7 @@ set(MCUBOOT_SIGNATURE_TYPE            "EC-P256"        CACHE STRING    "Algorith
 set(MCUBOOT_HW_KEY                    ON               CACHE BOOL      "Whether to embed the entire public key in the image metadata instead of the hash only")
 set(MCUBOOT_BUILTIN_KEY               OFF              CACHE BOOL      "Use builtin key(s) for validation, no public key data is embedded into the image metadata")
 
+set(RSE_SUBPLATFORM_PAL_CONFIG_DIR "${CMAKE_CURRENT_LIST_DIR}/subplatform_pal_default_config" CACHE PATH "path where subplatform PAL config headers can be found")
 ################################################################################
 
 # Specifying the accepted values for certain configuration options to facilitate

platform/ext/target/arm/rse/common/rse_otp_check_config.h
@@ -1,0 +1,35 @@
+/*
+ * SPDX-FileCopyrightText: Copyright The TrustedFirmware-M Contributors
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#ifndef __RSE_OTP_CHECK_CONFIG_H__
+#define __RSE_OTP_CHECK_CONFIG_H__
+
+#if RSE_OTP_CM_ROTPK_AMOUNT > 6
+#error ">6 ROTPKs overflows allocated space for key types"
+#endif
+
+#if RSE_OTP_DM_ROTPK_AMOUNT > 6
+#error ">6 ROTPKs overflows allocated space for key types"
+#endif
+
+#if defined(MCUBOOT_HW_KEY) != defined(RSE_OTP_CM_ROTPK_IS_HASH_NOT_KEY)
+#error "MCUBOOT_HW_KEY requires RSE_OTP_CM_ROTPK_IS_HASH_NOT_KEY"
+#endif
+
+#if defined(MCUBOOT_HW_KEY) != defined(RSE_OTP_DM_ROTPK_IS_HASH_NOT_KEY)
+#error "MCUBOOT_HW_KEY requires RSE_OTP_DM_ROTPK_IS_HASH_NOT_KEY"
+#endif
+
+#if defined(RSE_OTP_CM_ROTPK_IS_HASH_NOT_KEY) != defined(TFM_BL1_2_EMBED_ROTPK_IN_IMAGE)
+#error "RSE_OTP_CM_ROTPK_IS_HASH_NOT_KEY requires TFM_BL1_2_EMBED_ROTPK_IN_IMAGE"
+#endif
+
+#if defined(RSE_OTP_DM_ROTPK_IS_HASH_NOT_KEY) != defined(TFM_BL1_2_EMBED_ROTPK_IN_IMAGE)
+#error "RSE_OTP_DM_ROTPK_IS_HASH_NOT_KEY requires TFM_BL1_2_EMBED_ROTPK_IN_IMAGE"
+#endif
+
+#endif /* __RSE_OTP_CHECK_CONFIG_H__ */

platform/ext/target/arm/rse/common/rse_otp_dev.h
@@ -1,0 +1,59 @@
+/*
+ * SPDX-FileCopyrightText: Copyright The TrustedFirmware-M Contributors
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#ifndef __RSE_OTP_DEV_H__
+#define __RSE_OTP_DEV_H__
+
+#include "device_definition.h"
+#include "rse_otp_layout.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef RSE_OTP_HAS_CM_AREA
+extern struct rse_otp_area_info_t cm_area_info;
+extern struct rse_otp_area_info_t cm_rotpk_area_info;
+#endif
+#ifdef RSE_OTP_HAS_BL1_2
+extern struct rse_otp_area_info_t bl1_2_area_info;
+#endif
+#ifdef RSE_OTP_HAS_DM_AREA
+extern struct rse_otp_area_info_t dm_area_info;
+extern struct rse_otp_area_info_t dm_rotpk_area_info;
+#endif
+#ifdef RSE_OTP_HAS_DYNAMIC_AREA
+extern struct rse_otp_area_info_t dynamic_area_info;
+#endif
+#ifdef RSE_OTP_HAS_SOC_AREA
+extern const struct rse_otp_area_info_t soc_area_info;
+#endif
+
+#define OTP_BASE_S (LCM_BASE_S + 0x1000)
+
+#define OTP_OFFSET(x) (((uintptr_t)&x) - OTP_BASE_S)
+
+#define AREA_PTR(name) \
+    ((name ## _area_info.offset != 0 && name ## _area_info.size != 0) ? \
+        ((volatile const struct rse_otp_ ## name ## _area_t *)(OTP_BASE_S + name ## _area_info.offset)) \
+        : NULL)
+
+#define P_RSE_OTP_HEADER   ((volatile struct rse_otp_header_area_t *)(OTP_BASE_S))
+#define P_RSE_OTP_CM       AREA_PTR(cm)
+#define P_RSE_OTP_BL1_2    AREA_PTR(bl1_2)
+#define P_RSE_OTP_DM       AREA_PTR(dm)
+#define P_RSE_OTP_DYNAMIC  AREA_PTR(dynamic)
+#define P_RSE_OTP_SOC      AREA_PTR(soc)
+
+#define P_RSE_OTP_CM_ROTPK AREA_PTR(cm_rotpk)
+#define P_RSE_OTP_DM_ROTPK AREA_PTR(dm_rotpk)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __RSE_OTP_DEV_H__ */

platform/ext/target/arm/rse/common/rse_otp_layout.h
@@ -1,0 +1,180 @@
+/*
+ * SPDX-FileCopyrightText: Copyright The TrustedFirmware-M Contributors
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#ifndef __RSE_OTP_LAYOUT_H__
+#define __RSE_OTP_LAYOUT_H__
+
+#include "rse_otp_config.h"
+#include "lcm_otp_layout.h"
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef __PACKED_STRUCT
+#define __PACKED_STRUCT struct __attribute__((packed))
+#endif
+
+#ifndef __PACKED_UNION
+#define __PACKED_UNION union __attribute__((packed))
+#endif
+
+#define ROUND_UP(x, bound) ((((x) + bound - 1) / bound) * bound)
+#define ALIGN(x, bound) ROUND_UP(x, bound)
+
+#define COUNTER_BYTES(x) ALIGN((ROUND_UP((x), 8) / 8), 8)
+
+__PACKED_STRUCT rse_otp_area_info_t {
+    __PACKED_UNION {
+        __PACKED_STRUCT {
+            uint16_t offset;
+            uint16_t size;
+        };
+        uint32_t raw_data;
+    };
+};
+
+__PACKED_STRUCT rse_otp_header_area_t {
+    struct lcm_otp_hardware_area_t lcm_hardware_area;
+
+    struct rse_otp_area_info_t cm_area_info;
+    uint32_t cm_area_info_zero_count;
+    struct rse_otp_area_info_t bl1_2_area_info;
+    uint32_t bl1_2_area_info_zero_count;
+    struct rse_otp_area_info_t dm_area_info;
+    uint32_t dm_area_info_zero_count;
+    struct rse_otp_area_info_t dynamic_area_info;
+    uint32_t dynamic_area_info_zero_count;
+    struct rse_otp_area_info_t soc_area_info;
+    uint32_t soc_area_info_zero_count;
+
+    uint32_t device_status;
+
+#ifdef RSE_OTP_HAS_KRTL_USAGE_COUNTER
+    uint8_t krtl_usage_counter[COUNTER_BYTES(RSE_OTP_KRTL_COUNTER_MAX_VALUE)];
+#endif
+#ifdef RSE_OTP_HAS_LFT_COUNTER
+    uint8_t lft_counter[COUNTER_BYTES(RSE_OTP_LFT_COUNTER_MAX_VALUE)];
+#endif
+
+#ifdef RSE_OTP_HEADER_SUBPLATFORM_ITEMS
+    struct rse_otp_subplatform_header_area_t subplatform;
+#endif
+
+    uint8_t dma_ics[RSE_OTP_DMA_ICS_SIZE];
+};
+
+struct rse_otp_cm_rotpk_area_t {
+    uint32_t zero_count;
+    uint32_t cm_rotpk_policies;
+    uint32_t _pad;
+    uint8_t rotpk[RSE_OTP_CM_ROTPK_AMOUNT][RSE_OTP_CM_ROTPK_SIZE];
+};
+
+__PACKED_STRUCT rse_otp_cm_area_t {
+    uint32_t zero_count;
+    uint32_t provisioning_blob_version;
+    uint32_t config_flags;
+
+#ifdef RSE_OTP_HAS_CCA_ITEMS
+    uint32_t cca_system_properties;
+#endif
+#ifdef RSE_OTP_HAS_RSE_ID
+    uint32_t rse_id;
+#endif
+#ifdef RSE_OTP_HAS_ROUTING_TABLES
+    uint32_t rse_to_rse_sender_routing_table[RSE_ROUTING_TABLES_SIZE];
+    uint32_t rse_to_rse_receiver_routing_table[RSE_ROUTING_TABLES_SIZE];
+#endif
+
+#ifdef RSE_OTP_CM_SUBPLATFORM_ITEMS
+    struct rse_otp_subplatform_cm_area_t subplatform;
+#endif
+
+    uint8_t reserved[RSE_OTP_CM_RESERVED_SIZE];
+    struct rse_otp_cm_rotpk_area_t rotpk_areas[RSE_OTP_CM_ROTPK_MAX_REVOCATIONS + 1];
+};
+
+__PACKED_STRUCT rse_otp_bl1_2_area_t {
+    uint32_t zero_count;
+    uint32_t bl1_2_size;
+    uint32_t version;
+    uint8_t bl1_2_hash[RSE_OTP_BL1_2_HASH_SIZE];
+
+#ifdef RSE_OTP_BL1_2_SUBPLATFORM_ITEMS
+    struct rse_otp_subplatform_bl1_2_area_t subplatform;
+#endif
+
+    uint8_t bl1_2[RSE_OTP_BL1_2_MAX_SIZE];
+};
+
+struct rse_otp_dm_rotpk_area_t {
+    uint32_t zero_count;
+    uint32_t _pad;
+    uint32_t dm_rotpk_policies;
+    uint8_t rotpk[RSE_OTP_DM_ROTPK_AMOUNT][RSE_OTP_DM_ROTPK_SIZE];
+};
+
+__PACKED_STRUCT rse_otp_dm_area_t {
+    uint32_t zero_count;
+    uint32_t provisioning_blob_version;
+    uint32_t config_flags;
+
+#ifdef RSE_OTP_DM_SUBPLATFORM_ITEMS
+    struct rse_otp_subplatform_dm_area_t subplatform;
+#endif
+
+    uint8_t reserved[RSE_OTP_DM_RESERVED_SIZE];
+
+    struct rse_otp_dm_rotpk_area_t rotpk_areas[RSE_OTP_DM_ROTPK_MAX_REVOCATIONS + 1];
+};
+
+__PACKED_STRUCT rse_otp_dynamic_area_t {
+    uint8_t cm_reprovisioning[COUNTER_BYTES(RSE_OTP_MAX_REPROVISIONINGS)];
+    uint8_t cm_rotpk_revocation[COUNTER_BYTES(RSE_OTP_CM_ROTPK_MAX_REVOCATIONS)];
+    uint8_t dm_rotpk_revocation[COUNTER_BYTES(RSE_OTP_DM_ROTPK_MAX_REVOCATIONS)];
+
+    /* 4 banks of NV counters are provided. Each bank must be a fixed size, so
+     * there can be up to 4 different sizes of NV counters in the unlocked area,
+     * but with unlimited amounts of each size of counter.
+     */
+    uint8_t security_version_counters_bank_0[RSE_OTP_NV_COUNTERS_BANK_0_AMOUNT][COUNTER_BYTES(RSE_OTP_NV_COUNTERS_BANK_0_MAX_VALUE)];
+    uint8_t security_version_counters_bank_1[RSE_OTP_NV_COUNTERS_BANK_1_AMOUNT][COUNTER_BYTES(RSE_OTP_NV_COUNTERS_BANK_1_MAX_VALUE)];
+    uint8_t security_version_counters_bank_2[RSE_OTP_NV_COUNTERS_BANK_2_AMOUNT][COUNTER_BYTES(RSE_OTP_NV_COUNTERS_BANK_2_MAX_VALUE)];
+    uint8_t security_version_counters_bank_3[RSE_OTP_NV_COUNTERS_BANK_3_AMOUNT][COUNTER_BYTES(RSE_OTP_NV_COUNTERS_BANK_3_MAX_VALUE)];
+
+
+#ifdef RSE_OTP_HAS_ENDORSEMENT_CERTIFICATE
+    uint8_t iak_endorsement_certificate[RSE_OTP_ENDORSEMENT_CERTIFICATE_SIZE];
+    uint8_t iak_endorsement_parameters[RSE_OTP_ENDORSEMENT_CERTIFICATE_METADATA_SIZE];
+#endif
+
+#ifdef RSE_OTP_DYNAMIC_SUBPLATFORM_ITEMS
+    struct rse_otp_subplatform_dynamic_area_t subplatform;
+#endif
+};
+
+__PACKED_STRUCT rse_otp_soc_area_t {
+    uint32_t zero_count;
+    uint8_t unique_id[12];
+    uint8_t family_id[4];
+    uint8_t serial_number[8];
+
+#ifdef RSE_OTP_SOC_SUBPLATFORM_ITEMS
+    struct rse_otp_subplatform_soc_area_t subplatform;
+#endif
+
+    uint8_t reserved[RSE_OTP_SOC_RESERVED_SIZE];
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __RSE_OTP_LAYOUT_H__ */

platform/ext/target/arm/rse/common/subplatform_pal_default_config/rse_otp_config.h
@@ -1,0 +1,122 @@
+/*
+ * SPDX-FileCopyrightText: Copyright The TrustedFirmware-M Contributors
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#ifndef __RSE_OTP_CONFIG_H__
+#define __RSE_OTP_CONFIG_H__
+
+#include "rse_nv_counter_config.h"
+
+#include <stdint.h>
+
+#define RSE_OTP_SIZE 16384
+
+/* The following options control the ROM view of the OTP. They must remain
+ * constant for a given ROM
+ */
+#define RSE_OTP_HAS_CM_AREA
+#define RSE_OTP_HAS_BL1_2
+#define RSE_OTP_HAS_DM_AREA
+#define RSE_OTP_HAS_DYNAMIC_AREA
+#define RSE_OTP_HAS_SOC_AREA
+#define RSE_OTP_HAS_KRTL_USAGE_COUNTER
+#define RSE_OTP_HAS_LFT_COUNTER
+
+/* #define RSE_OTP_DM_SETS_DM_AND_DYNAMIC_AREA_SIZE */
+
+#define RSE_OTP_KRTL_COUNTER_MAX_VALUE 16
+#define RSE_OTP_LFT_COUNTER_MAX_VALUE 512
+
+#define RSE_OTP_DMA_ICS_SIZE 0x108
+
+/* The following options control the CM area, and can be changed without
+ * altering the ROM.
+ */
+#define RSE_OTP_HAS_CCA_ITEMS
+/* #define RSE_OTP_HAS_RSE_ID */
+/* #define RSE_OTP_HAS_ROUTING_TABLES */
+
+#define RSE_OTP_CM_ROTPK_SIZE 32
+#define RSE_OTP_CM_ROTPK_AMOUNT 4
+#define RSE_OTP_CM_ROTPK_IS_HASH_NOT_KEY
+#define RSE_OTP_CM_ROTPK_MAX_REVOCATIONS 0
+#define RSE_ROUTING_TABLES_SIZE 0
+#define RSE_OTP_CM_RESERVED_SIZE 4
+
+/* The following options control the BL1_2 area, and can be changed without
+ * altering the ROM.
+ */
+
+#define RSE_OTP_BL1_2_MAX_SIZE 8192
+#define RSE_OTP_BL1_2_HASH_SIZE 48
+
+/* The following options control the DM area, and can be changed without
+ * altering the ROM.
+ */
+#define RSE_OTP_DM_ROTPK_SIZE 32
+#define RSE_OTP_DM_ROTPK_AMOUNT 3
+#define RSE_OTP_DM_ROTPK_IS_HASH_NOT_KEY
+#define RSE_OTP_DM_ROTPK_MAX_REVOCATIONS 0
+#define RSE_OTP_DM_RESERVED_SIZE 0
+
+/* The following options control the Dynamic area, and can be changed without
+ * altering the ROM.
+ */
+
+#define RSE_OTP_MAX_REPROVISIONINGS 8
+/* #define RSE_OTP_HAS_IAK_ENDORSEMENT_CERTIFICATE */
+/* #define RSE_OTP_ENDORSEMENT_CERTIFICATE_SIZE 0x60 */
+/* #define RSE_OTP_ENDORSEMENT_CERTIFICATE_METADATA_SIZE 0x30 */
+
+/* 4 banks of NV counters are provided. Each bank must be a fixed size, so there
+ * can be up to 4 different sizes of NV counters in the unlocked area, but with
+ * unlimited amounts of each size of counter.
+ */
+#define RSE_OTP_NV_COUNTERS_BANK_0_MAX_VALUE 256
+#define RSE_OTP_NV_COUNTERS_BANK_0_AMOUNT    RSE_NV_COUNTER_AMOUNT
+
+#define RSE_OTP_NV_COUNTERS_BANK_1_MAX_VALUE 0
+#define RSE_OTP_NV_COUNTERS_BANK_1_AMOUNT    0
+
+#define RSE_OTP_NV_COUNTERS_BANK_2_MAX_VALUE 0
+#define RSE_OTP_NV_COUNTERS_BANK_2_AMOUNT    0
+
+#define RSE_OTP_NV_COUNTERS_BANK_3_MAX_VALUE 0
+#define RSE_OTP_NV_COUNTERS_BANK_3_AMOUNT    0
+
+/* The following options control the SoC area, and cannot be changed without
+ * altering the ROM.
+ */
+
+#define RSE_OTP_SOC_RESERVED_SIZE 0x12D4
+
+/* #define RSE_OTP_HEADER_SUBPLATFORM_ITEMS */
+/* struct __attribute__((packed)) rse_otp_subplatform_header_area_t { */
+/* }; */
+
+/* #define RSE_OTP_CM_SUBPLATFORM_ITEMS */
+/* struct __attribute__((packed)) rse_otp_subplatform_cm_area_t { */
+/* }; */
+
+/* #define RSE_OTP_BL1_2_SUBPLATFORM_ITEMS */
+/* struct __attribute__((packed)) rse_otp_subplatform_bl1_2_area_t { */
+/* }; */
+
+/* #define RSE_OTP_DM_SUBPLATFORM_ITEMS */
+/* struct __attribute__((packed)) rse_otp_subplatform_dm_area_t { */
+/* }; */
+
+/* #define RSE_OTP_DYNAMIC_SUBPLATFORM_ITEMS */
+/* struct __attribute__((packed)) rse_otp_subplatform_dynamic_area_t { */
+/* }; */
+
+/* #define RSE_OTP_SOC_SUBPLATFORM_ITEMS */
+/* struct __attribute__((packed)) rse_otp_subplatform_soc_area_t { */
+/* }; */
+
+#include "rse_otp_check_config.h"
+
+#endif /* __RSE_OTP_CONFIG_H__ */

commit 120a27f299017706ba7cc64475fdeea9e0090df0

RSE: common: remove OTP ID gaps
The various PLAT_OTP_ID_*_MAX enum constants denote exclusive range end
values; see:

- platform/ext/target/arm/rse/common/bl2/rse_boot_measurements.c
- platform/ext/target/arm/rse/common/provisioning/rse_provisioning_rotpk.c
- platform/ext/target/arm/rse/common/rse_rotpk_mapping.c

Thus, a PLAT_OTP_ID_*_MAX is never associated with an OTP slot per se; the
first meaningful OTP slot following the range in question must alias the
last PLAT_OTP_ID_*_MAX value.

This bug -- i.e., gaps in the OTP ID space -- causes the C code to get out
of sync with the combined provisioning message, and (for example) the
crypto partition of the TF-M runtime can crash in tfm_plat_otp_get_size()
due to passing an invalid OTP ID (an ID that does not match the
provisioned entries).

Change-Id: I178b8c8c459ca8d09e3651f0f220af450cd18d57
Fixes: de988a7f0f4df
Signed-off-by: Laszlo Ersek <laszlo.ersek@arm.com>

platform/ext/target/arm/rse/common/platform_otp_ids.h
@@ -29,11 +29,11 @@ enum tfm_otp_element_id_t {
 
     PLAT_OTP_ID_CM_ROTPK,
     PLAT_OTP_ID_CM_ROTPK_MAX = PLAT_OTP_ID_CM_ROTPK + RSE_OTP_CM_ROTPK_AMOUNT,
-    PLAT_OTP_ID_CM_ROTPK_REPROVISIONING,
+    PLAT_OTP_ID_CM_ROTPK_REPROVISIONING = PLAT_OTP_ID_CM_ROTPK_MAX,
 
     PLAT_OTP_ID_DM_ROTPK,
     PLAT_OTP_ID_DM_ROTPK_MAX = PLAT_OTP_ID_DM_ROTPK + RSE_OTP_DM_ROTPK_AMOUNT,
-    PLAT_OTP_ID_DM_ROTPK_REPROVISIONING,
+    PLAT_OTP_ID_DM_ROTPK_REPROVISIONING = PLAT_OTP_ID_DM_ROTPK_MAX,
 
     PLAT_OTP_ID_KRTL_USAGE_COUNTER,
     PLAT_OTP_ID_LFT_COUNTER,
@@ -49,17 +49,17 @@ enum tfm_otp_element_id_t {
     PLAT_OTP_ID_NV_COUNTER_BANK_0_COUNTER,
     PLAT_OTP_ID_NV_COUNTER_BANK_0_COUNTER_MAX =
         PLAT_OTP_ID_NV_COUNTER_BANK_0_COUNTER + RSE_OTP_NV_COUNTERS_BANK_0_AMOUNT,
-    PLAT_OTP_ID_NV_COUNTER_BANK_1_COUNTER,
+    PLAT_OTP_ID_NV_COUNTER_BANK_1_COUNTER = PLAT_OTP_ID_NV_COUNTER_BANK_0_COUNTER_MAX,
     PLAT_OTP_ID_NV_COUNTER_BANK_1_COUNTER_MAX =
         PLAT_OTP_ID_NV_COUNTER_BANK_1_COUNTER + RSE_OTP_NV_COUNTERS_BANK_1_AMOUNT,
-    PLAT_OTP_ID_NV_COUNTER_BANK_2_COUNTER,
+    PLAT_OTP_ID_NV_COUNTER_BANK_2_COUNTER = PLAT_OTP_ID_NV_COUNTER_BANK_1_COUNTER_MAX,
     PLAT_OTP_ID_NV_COUNTER_BANK_2_COUNTER_MAX =
         PLAT_OTP_ID_NV_COUNTER_BANK_2_COUNTER + RSE_OTP_NV_COUNTERS_BANK_2_AMOUNT,
-    PLAT_OTP_ID_NV_COUNTER_BANK_3_COUNTER,
+    PLAT_OTP_ID_NV_COUNTER_BANK_3_COUNTER = PLAT_OTP_ID_NV_COUNTER_BANK_2_COUNTER_MAX,
     PLAT_OTP_ID_NV_COUNTER_BANK_3_COUNTER_MAX =
         PLAT_OTP_ID_NV_COUNTER_BANK_3_COUNTER + RSE_OTP_NV_COUNTERS_BANK_3_AMOUNT,
 
-    PLAT_OTP_ID_BL1_2_IMAGE,
+    PLAT_OTP_ID_BL1_2_IMAGE = PLAT_OTP_ID_NV_COUNTER_BANK_3_COUNTER_MAX,
     PLAT_OTP_ID_BL1_2_IMAGE_LEN,
     PLAT_OTP_ID_BL1_2_IMAGE_HASH,
 