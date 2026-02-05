97ae43b0d0a0d73e9cbb649e22c5c3d84a511df7
David Vincze <david.vincze@arm.com>
2026-02-05 09:38:22 +0000

    Platform: Nuvoton: Use PSA and EC crypto in BL2
    As of Mbed TLS 4.0 the cryptography part has been moved into a separate
    product called TF-PSA-Crypto. TF-M has also transitioned from Mbed TLS
    to TF-PSA-Crypto and its CMake build system has been updated to reflect
    this change.
    
    Nuvoton platforms were using the MCUBOOT_USE_MBED_TLS option for BL2
    (aka MCUboot). This commit updates the BL2 configurations to use
    PSA Crypto APIs in the cryptographic abstraction layer instead of
    Mbed TLS (MCUBOOT_USE_PSA_CRYPTO=ON). This is required as TF-M now
    integrates TF-PSA-Crypto in the BL2 CMake module too which provides PSA
    API (almost exclusively).
    
    Also the signature type is updated to EC-P256 from RSA-3072 for a
    smaller BL2 memory footprint which was an issue on M2354 platform.
    
    Change-Id: I6bb1b5c9fcc962322f15569bf3bf68ec60e0b3ac
    Signed-off-by: David Vincze <david.vincze@arm.com>

--- platform/ext/target/nuvoton/m2351/config.cmake
+++ platform/ext/target/nuvoton/m2351/config.cmake
@@ -1,13 +1,16 @@
 #-------------------------------------------------------------------------------
-# Copyright (c) 2020-2023, Arm Limited. All rights reserved.
+# SPDX-FileCopyrightText: Copyright The TrustedFirmware-M Contributors
 # Copyright (c) 2023, Nuvoton Technology Corp. All rights reserved.
-# 
+#
 # SPDX-License-Identifier: BSD-3-Clause
 #
 #-------------------------------------------------------------------------------
 
-set(MCUBOOT_IMAGE_NUMBER    2      CACHE STRING    "Whether to combine S and NS into either 1 image, or sign each separately")
-set(BL2_TRAILER_SIZE        0x800  CACHE STRING    "Trailer size")
+set(MCUBOOT_IMAGE_NUMBER    2           CACHE STRING    "Whether to combine S and NS into either 1 image, or sign each separately")
+set(BL2_TRAILER_SIZE        0x800       CACHE STRING    "Trailer size")
+set(MCUBOOT_USE_PSA_CRYPTO  ON          CACHE BOOL      "Enable the cryptographic abstraction layer to use PSA Crypto APIs")
+set(MCUBOOT_SIGNATURE_TYPE  "EC-P256"   CACHE STRING    "Algorithm to use for signature validation [RSA-2048, RSA-3072, EC-P256, EC-P384]")
+
 
 # Platform-specific configurations
 set(CONFIG_TFM_USE_TRUSTZONE          ON )
