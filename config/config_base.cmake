af6b17ba36af925bcdc19fd808368043bcb17169
Antonio de Angelis <antonio.deangelis@arm.com>
2026-02-05 07:36:32 +0000

    Build: Bump tf-m-extras
    To pick up the latest changes on dTPM
    
    Signed-off-by: Antonio de Angelis <antonio.deangelis@arm.com>
    Change-Id: Ie59d39df296a2df977251d857ff06676f54939a0

--- config/config_base.cmake
+++ config/config_base.cmake
@@ -46,7 +46,7 @@ set(MCUBOOT_FORCE_PATCH                 OFF         CACHE BOOL      "Always appl
 set(MCUBOOT_VERSION                     "tfm-2.3.0" CACHE STRING    "The version of MCUboot to use")
 
 set(TFM_EXTRAS_REPO_PATH                "DOWNLOAD"  CACHE PATH      "Path to tf-m-extras repo (or DOWNLOAD to fetch automatically")
-set(TFM_EXTRAS_REPO_VERSION             "4e4bc2b"   CACHE STRING    "The version of tf-m-extras to use")
+set(TFM_EXTRAS_REPO_VERSION             "52aa709b"  CACHE STRING    "The version of tf-m-extras to use")
 
 set(LIB_EVENTLOG_PATH                   "DOWNLOAD"  CACHE PATH      "Path to LibEventLog repo (or DOWNLOAD to fetch automatically")
 set(LIB_EVENTLOG_VERSION                "f459fa60"  CACHE STRING    "The version of LibEventLog to use")
