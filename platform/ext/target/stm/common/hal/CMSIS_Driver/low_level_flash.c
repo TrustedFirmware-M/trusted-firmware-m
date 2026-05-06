TrustedFirmware-M/trusted-firmware-m

commit 630d633eae654ce060020a3ef649a172e06a96e7

STM32U5: update for TF-PSA-Crypto 1.0.0 deactivate hardware crypto acceleration
Change-Id: Id0a00bc35286f20303650b76139786bf2350ecef
Signed-off-by: Marc JACOB <marc.jacob@st.com>

platform/ext/target/stm/common/hal/CMSIS_Driver/low_level_flash.c
@@ -579,13 +579,17 @@ static int32_t Flash_ProgramData(uint32_t addr,
   do
   {
 #if FLASH0_PROG_UNIT == 0x8
-    /* dword api*/
-    uint64_t dword;
-    memcpy(&dword, (void *)((uint32_t)data + loop), sizeof(dword));
-    if (dword != -1)
-        err = HAL_FLASH_Program(write_type, (flash_base + addr), dword);
+    /* doubleword api*/
+    uint32_t dword[2];
+    memcpy(dword, (void *)((uint32_t)data + loop), sizeof(dword));
+    if ((dword[0] != -1) || (dword[1] != -1))
+    {
+      err = HAL_FLASH_Program(write_type, (flash_base + addr), (uint32_t)&dword[0]);
+    }
     else
+	{
         err = HAL_OK;
+	}
 #elif FLASH0_PROG_UNIT == 0x10
     /* quadword api*/
     uint64_t dword[2];

platform/ext/target/stm/b_u585i_iot02a/accelerator/CMakeLists.txt
@@ -11,49 +11,23 @@
 if (TFM_PARTITION_CRYPTO)
     target_sources(crypto_service_crypto_hw
         PRIVATE
-            ${PLATFORM_DIR}/ext/target/stm/common/hal/accelerator/rsa_alt.c
-            ${PLATFORM_DIR}/ext/target/stm/common/hal/accelerator/ecdsa_alt.c
-            ${PLATFORM_DIR}/ext/target/stm/common/hal/accelerator/gcm_alt.c
-            ${PLATFORM_DIR}/ext/target/stm/common/hal/accelerator/aes_alt.c
-            ${PLATFORM_DIR}/ext/target/stm/common/hal/accelerator/ccm_alt.c
-            ${PLATFORM_DIR}/ext/target/stm/common/hal/accelerator/ecp_alt.c
-            ${PLATFORM_DIR}/ext/target/stm/common/hal/accelerator/ecp_curves_alt.c
-            ${PLATFORM_DIR}/ext/target/stm/common/hal/accelerator/sha1_alt.c
-            ${PLATFORM_DIR}/ext/target/stm/common/hal/accelerator/sha256_alt.c
             ${PLATFORM_DIR}/ext/target/stm/common/hal/accelerator/stm.c
+            ${PLATFORM_DIR}/ext/target/stm/common/hal/accelerator/rng.c
     )
 
     target_include_directories(crypto_service_crypto_hw
-        PRIVATE
-            ${PLATFORM_DIR}/ext/target/${TFM_PLATFORM}/accelerator/
+		PUBLIC
+            ${PLATFORM_DIR}/ext/target/${TFM_PLATFORM}/accelerator/include
             ${PLATFORM_DIR}/ext/target/stm/common/hal/accelerator/
+            ${PLATFORM_DIR}/ext/target/stm/common/hal/Native_Driver/
             ${PLATFORM_DIR}/ext/target/${TFM_PLATFORM}/include/
             ${PLATFORM_DIR}/ext/target/stm/common/stm32u5xx/hal/Inc/
             ${PLATFORM_DIR}/ext/target/stm/common/stm32u5xx/Device/Include/
-            ${PLATFORM_DIR}/include
-            ${CMAKE_BINARY_DIR}/generated
-            ${CMAKE_SOURCE_DIR}/interface/include
     )
-    target_include_directories(crypto_service_tfpsacrypto
+
+    target_include_directories(platform_s
         PUBLIC
-            ${PLATFORM_DIR}/ext/target/${TFM_PLATFORM}/accelerator/
-            ${PLATFORM_DIR}/ext/target/stm/common/hal/accelerator/
-            ${PLATFORM_DIR}/ext/target/${TFM_PLATFORM}/include/
-            ${PLATFORM_DIR}/ext/target/stm/common/stm32u5xx/hal/Inc/
-            ${PLATFORM_DIR}/ext/target/stm/common/stm32u5xx/Device/Include/
-    )
-
-    target_include_directories(psa_crypto_config
-        INTERFACE
-            $<BUILD_INTERFACE:${PLATFORM_DIR}/ext/target/${TFM_PLATFORM}/accelerator/>
-    )
-
-    target_compile_definitions(crypto_service_crypto_hw
-        PRIVATE
-            ST_HW_CONTEXT_SAVING
-            $<$<AND:$<BOOL:${TFM_PARTITION_PROTECTED_STORAGE}>,$<STREQUAL:${PS_CRYPTO_AEAD_ALG},PSA_ALG_GCM>>:BUILD_CRYPTO_TFM>
-        INTERFACE
-            $<$<AND:$<BOOL:${TFM_PARTITION_PROTECTED_STORAGE}>,$<STREQUAL:${PS_CRYPTO_AEAD_ALG},PSA_ALG_GCM>>:PSA_WANT_ALG_GCM>
+            ${PLATFORM_DIR}/ext/target/${TFM_PLATFORM}/accelerator/include
     )
 
     target_link_libraries(crypto_service_crypto_hw
@@ -63,17 +37,10 @@ if (TFM_PARTITION_CRYPTO)
             cmsis
     )
 
-    target_link_libraries(crypto_service_tfpsacrypto
-        PUBLIC
-            cmsis
+    # The config files need to access crypto accelerator configurations.
+    target_include_directories(psa_crypto_library_config
+        INTERFACE
+            ${PLATFORM_DIR}/ext/target/${TFM_PLATFORM}/accelerator/include
     )
 
-    target_link_libraries(platform_s
-        PRIVATE
-            crypto_service_crypto_hw
-    )
-    target_link_libraries(crypto_service_crypto_hw
-         INTERFACE
-            tfm_config
-    )
 endif()

platform/ext/target/stm/b_u585i_iot02a/accelerator/crypto_accelerator_config.h
@@ -1,77 +1,0 @@
-/*
- * Copyright (c) 2019-2022, Arm Limited. All rights reserved.
- * Copyright (c) 2021 STMicroelectronics. All rights reserved.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- *
- */
-
-#ifndef CRYPTO_ACCELERATOR_CONF_H
-#define CRYPTO_ACCELERATOR_CONF_H
-
-#ifdef __cplusplus
-extern "C" {
-#endif /* __cplusplus */
-
-/****************************************************************/
-/* Require built-in implementations based on PSA requirements */
-/****************************************************************/
-#ifdef PSA_USE_SE_ST
-/* secure element define */
-#define PSA_WANT_KEY_TYPE_AES                   1
-#ifdef MBEDTLS_PSA_CRYPTO_C
-#define MBEDTLS_PSA_CRYPTO_SE_C
-#define MBEDTLS_CMAC_C
-#define MBEDTLS_CIPHER_MODE_CBC
-#endif
-
-#ifdef PSA_WANT_ALG_SHA_1
-#define MBEDTLS_SHA1_ALT
-#endif /* PSA_WANT_ALG_SHA_1 */
-
-#ifdef PSA_WANT_ALG_SHA_256
-#define MBEDTLS_SHA256_ALT
-#endif /* PSA_WANT_ALG_SHA_256 */
-
-#if defined(PSA_WANT_ALG_RSA_OAEP)                 ||     \
-    defined(PSA_WANT_ALG_RSA_PKCS1V15_CRYPT)       ||     \
-    defined(PSA_WANT_ALG_RSA_PKCS1V15_SIGN)        ||     \
-    defined(PSA_WANT_ALG_RSA_PSS)                  ||     \
-    defined(PSA_WANT_KEY_TYPE_RSA_KEY_PAIR_BASIC)  ||     \
-    defined(PSA_WANT_KEY_TYPE_RSA_PUBLIC_KEY)
-#define MBEDTLS_RSA_ALT
-#endif
-
-#if defined(PSA_WANT_ALG_ECDH)                    ||  \
-    defined(PSA_WANT_ALG_ECDSA)                   ||  \
-    defined(PSA_WANT_KEY_TYPE_ECC_KEY_PAIR_BASIC) ||  \
-    defined(PSA_WANT_KEY_TYPE_ECC_PUBLIC_KEY)
-#define MBEDTLS_ECP_ALT
-#undef MBEDTLS_ECP_NIST_OPTIM
-#endif
-
-#ifdef PSA_WANT_ALG_CCM
-#define MBEDTLS_CCM_ALT
-#endif /* PSA_WANT_ALG_CCM */
-
-#ifdef PSA_WANT_KEY_TYPE_AES
-#define MBEDTLS_AES_ALT
-#endif /* PSA_WANT_KEY_TYPE_AES */
-
-#ifdef PSA_WANT_ALG_GCM
-#define MBEDTLS_GCM_ALT
-#endif /* PSA_WANT_ALG_GCM */
-
-#if defined(PSA_WANT_ALG_ECDSA)  ||  \
-    defined(PSA_WANT_ALG_DETERMINISTIC_ECDSA)
-#define MBEDTLS_ECDSA_VERIFY_ALT
-#define MBEDTLS_ECDSA_SIGN_ALT
-#endif
-
-#endif
-
-#ifdef __cplusplus
-}
-#endif /* __cplusplus */
-
-#endif /* CRYPTO_ACCELERATOR_CONF_H */

platform/ext/target/stm/b_u585i_iot02a/accelerator/include/tf_psa_crypto_accelerator_config.h
@@ -1,0 +1,29 @@
+
+/*
+ * SPDX-FileCopyrightText: Copyright The TrustedFirmware-M Contributors
+ * Copyright (c) 2026 STMicroelectronics. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+#ifndef __TF_PSA_CRYPTO_ACCELERATOR_CONFIG_H__
+#define __TF_PSA_CRYPTO_ACCELERATOR_CONFIG_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+/* RNG Config */
+#undef MBEDTLS_ENTROPY_NV_SEED
+#undef MBEDTLS_ENTROPY_NO_SOURCES_OK
+#define MBEDTLS_PSA_CRYPTO_EXTERNAL_RNG
+
+/****************************************************************/
+/* Require built-in implementations based on PSA requirements */
+/****************************************************************/
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* __TF_PSA_CRYPTO_ACCELERATOR_CONFIG_H__ */

platform/ext/target/stm/b_u585i_iot02a/accelerator/mbedtls_accelerator_config.h
@@ -1,86 +1,0 @@
-/*
- * Copyright (c) 2019-2022, Arm Limited. All rights reserved.
- * Copyright (c) 2021 STMicroelectronics. All rights reserved.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- *
- */
-
-#ifndef MBEDTLS_ACCELERATOR_CONF_H
-#define MBEDTLS_ACCELERATOR_CONF_H
-
-#ifdef __cplusplus
-extern "C" {
-#endif /* __cplusplus */
-
-/* RNG Config */
-#undef MBEDTLS_ENTROPY_NV_SEED
-#undef MBEDTLS_NO_DEFAULT_ENTROPY_SOURCES
-#define MBEDTLS_ENTROPY_C
-#define MBEDTLS_ENTROPY_HARDWARE_ALT
-
-#undef MBEDTLS_AES_SETKEY_DEC_ALT
-#undef MBEDTLS_AES_DECRYPT_ALT
-
-/* specific Define for platform hardware accelerator */
-#define GENERATOR_HW_PKA_EXTENDED_API
-#define GENERATOR_HW_CRYPTO_DPA_SUPPORTED
-#define HW_CRYPTO_DPA_AES
-#define HW_CRYPTO_DPA_GCM
-
-/****************************************************************/
-/* Infer PSA requirements from Mbed TLS capabilities */
-/****************************************************************/
-#ifndef MBEDTLS_PSA_CRYPTO_CONFIG
-
-#ifdef MBEDTLS_SHA1_C
-#define MBEDTLS_SHA1_ALT
-#endif /* MBEDTLS_SHA1_C */
-
-#ifdef MBEDTLS_SHA256_C
-#define MBEDTLS_SHA256_ALT
-#endif /* MBEDTLS_SHA256_C */
-
-#ifdef MBEDTLS_RSA_C
-#define MBEDTLS_RSA_ALT
-#endif /* MBEDTLS_RSA_C */
-
-#if defined(MBEDTLS_ECP_C)
-#define MBEDTLS_ECP_ALT
-#undef MBEDTLS_ECP_NIST_OPTIM
-/*#define MBEDTLS_MD5_ALT*/
-#endif /* MBEDTLS_ECP_C && MBEDTLS_MD_C */
-
-#ifdef MBEDTLS_CCM_C
-#define MBEDTLS_CCM_ALT
-#endif /* MBEDTLS_CCM_C */
-
-#ifdef MBEDTLS_AES_C
-#define MBEDTLS_AES_ALT
-#endif /* MBEDTLS_AES_C */
-
-#ifdef MBEDTLS_GCM_C
-#define MBEDTLS_GCM_ALT
-#endif /* MBEDTLS_GCM_C */
-
-#ifdef MBEDTLS_ECDSA_C
-#define MBEDTLS_ECDSA_VERIFY_ALT
-#define MBEDTLS_ECDSA_SIGN_ALT
-#endif /* MBEDTLS_ECDSA_C */
-
-/* secure element define */
-#ifdef MBEDTLS_PSA_CRYPTO_C
-#ifdef PSA_USE_SE_ST
-#define MBEDTLS_PSA_CRYPTO_SE_C
-#define MBEDTLS_CMAC_C
-#define MBEDTLS_CIPHER_MODE_CBC
-#endif
-#endif
-
-#endif /* MBEDTLS_PSA_CRYPTO_CONFIG */
-
-#ifdef __cplusplus
-}
-#endif /* __cplusplus */
-
-#endif /* MBEDTLS_ACCELERATOR_CONF_H */

platform/ext/target/stm/b_u585i_iot02a/config.cmake
@@ -13,10 +13,11 @@ set(MCUBOOT_IMAGE_NUMBER                   2           CACHE STRING    "Whether 
 set(BL2_TRAILER_SIZE                       0x2000      CACHE STRING    "Trailer size")
 set(MCUBOOT_ALIGN_VAL                      16          CACHE STRING    "Align option to build image with imgtool")
 set(MCUBOOT_UPGRADE_STRATEGY      "SWAP_USING_SCRATCH" CACHE STRING    "Upgrade strategy for images")
+set(MCUBOOT_USE_PSA_CRYPTO                 ON          CACHE BOOL      "Enable the cryptographic abstraction layer to use PSA Crypto APIs")
 set(TFM_PARTITION_PLATFORM                 ON          CACHE BOOL      "Enable platform partition")
 set(MCUBOOT_BOOTSTRAP                      ON          CACHE BOOL      "Allow initial state with images in secondary slots(empty primary slots)")
-set(MCUBOOT_ENC_IMAGES                     ON          CACHE BOOL      "Enable encrypted image upgrade support")
-set(MCUBOOT_ENCRYPT_RSA                    ON          CACHE BOOL      "Use RSA for encrypted image upgrade support")
+set(MCUBOOT_ENC_IMAGES                     OFF         CACHE BOOL      "Enable encrypted image upgrade support")
+set(MCUBOOT_ENCRYPT_RSA                    OFF         CACHE BOOL      "Use RSA for encrypted image upgrade support")
 set(MCUBOOT_DATA_SHARING                   ON          CACHE BOOL      "Enable Data Sharing")
 ################################## Dependencies ################################################################################################
 set(TFM_PARTITION_INTERNAL_TRUSTED_STORAGE ON          CACHE BOOL      "Enable Internal Trusted Storage partition")
@@ -26,7 +27,6 @@ set(TF_PSA_CRYPTO_BUILD_TYPE               minsizerel  CACHE STRING    "Build ty
 set(TFM_DUMMY_PROVISIONING                 OFF         CACHE BOOL      "Provision with dummy values. NOT to be used in production")
 set(PLATFORM_DEFAULT_OTP_WRITEABLE         OFF         CACHE BOOL      "Use on chip flash with write support")
 set(PLATFORM_DEFAULT_NV_COUNTERS           OFF         CACHE BOOL      "Use default nv counter implementation.")
-set(PS_CRYPTO_AEAD_ALG                     PSA_ALG_GCM CACHE STRING    "The AEAD algorithm to use for authenticated encryption in Protected Storage")
 set(MCUBOOT_FIH_PROFILE                    LOW         CACHE STRING    "Fault injection hardening profile [OFF, LOW, MEDIUM, HIGH]")
 
 ################################## Platform-specific configurations ############################################################################

platform/ext/target/stm/common/hal/Native_Driver/low_level_rng.c
@@ -82,7 +82,7 @@ int RNG_Init(void)
   return 0;
 }
 
-static void RNG_GetBytes(uint8_t *output, size_t length, size_t *output_length)
+void RNG_GetBytes(uint8_t *output, size_t length, size_t *output_length)
 {
   int32_t ret = 0;
   uint8_t try = 0;
@@ -131,14 +131,3 @@ void RNG_DeInit(void)
   users = 0;
 }
 
-
-/*  interface for mbed-crypto */
-int mbedtls_hardware_poll(void *data, unsigned char *output, size_t len, size_t *olen)
-{
-  RNG_GetBytes(output, len, olen);
-  if (*olen != len)
-  {
-    return -1;
-  }
-  return 0;
-}

platform/ext/target/stm/common/hal/Native_Driver/low_level_rng.h
@@ -17,8 +17,9 @@
 #ifndef __LOW_LEVEL_RNG_H__
 #define __LOW_LEVEL_RNG_H__
 #include <stddef.h>
-int mbedtls_hardware_poll(void *data, unsigned char *output, size_t len, size_t *olen);
+#include <stdint.h>
 int RNG_Init(void);
 void RNG_DeInit(void);
+void RNG_GetBytes(uint8_t *output, size_t length, size_t *output_length);
 
 #endif /* __LOW_LEVEL_RNG_H__ */

commit fc8bcd762e4d69dbae14cae631ff337d8e67c051

platform: ext: stm: common: fix CMSIS Flash_ProgramData() for L5xx
Fix CMSIS Flash_ProgramData() function for STM32L5xx targets for which
this HAL function expects the 64bit data to be programmed as last
arguments whereas other STM32 SoCs currently supported in TF-M
expect a data address argument.

Change-Id: I85ca8b3eb565f6eda9db34fe01b345a286d22fa9
Fixes: 630d633eae65 ("STM32U5: update for TF-PSA-Crypto 1.0.0")
Signed-off-by: Etienne Carriere <etienne.carriere@st.com>

platform/ext/target/stm/common/hal/CMSIS_Driver/low_level_flash.c
@@ -580,6 +580,19 @@ static int32_t Flash_ProgramData(uint32_t addr,
   {
 #if FLASH0_PROG_UNIT == 0x8
     /* doubleword api*/
+ #ifdef STM32L5xx_HAL_H
+    /* HAL_FLASH_Program() for L5xx expects a 64bit data, not a data address */
+    uint64_t dword;
+    memcpy(&dword, (void *)((uint32_t)data + loop), sizeof(dword));
+    if (dword != -1)
+    {
+        err = HAL_FLASH_Program(write_type, flash_base + addr, dword);
+    }
+    else
+    {
+        err = HAL_OK;
+    }
+ #else /* STM32L5xx_HAL_H */
     uint32_t dword[2];
     memcpy(dword, (void *)((uint32_t)data + loop), sizeof(dword));
     if ((dword[0] != -1) || (dword[1] != -1))
@@ -590,6 +603,7 @@ static int32_t Flash_ProgramData(uint32_t addr,
 	{
         err = HAL_OK;
 	}
+ #endif /* STM32L5xx_HAL_H */
 #elif FLASH0_PROG_UNIT == 0x10
     /* quadword api*/
     uint64_t dword[2];