78a0ad7f43e81c2eb10cef16f1887fd61d9823ec
Hussein Serageldin <hussein.serageldin@arm.com>
2026-07-13 14:12:59 +0200

    Musca-B1: directly disable unsupported crypto tests
    The platform started using the cc3xx driver instead of
    crypto-312-runtime driver. tf-m-tests repository enables a few
    algorithm tests by default. Overriding those defaults are crucial
    because the cc3xx driver does not support the corresponding algorithms.
    
    Change-Id: I803b7c33a0eec34d2b6e41654bc823434a75d9bb
    Signed-off-by: Hussein Serageldin <hussein.serageldin@arm.com>

--- platform/ext/target/arm/musca_b1/tests/tfm_tests_config.cmake
+++ platform/ext/target/arm/musca_b1/tests/tfm_tests_config.cmake
@@ -5,7 +5,12 @@
 #
 #-------------------------------------------------------------------------------
 
-set(TFM_CRYPTO_TEST_ALG_CFB             OFF         CACHE BOOL      "Test CFB cryptography mode")
-set(CC3XX_RUNTIME_ENABLED               ON          CACHE BOOL       "Whether the CC3XX driver is used at runtime")
+# The tf-m-tests repository enables these algorithm tests by default. Override
+# those defaults here because the CC3XX driver used on Musca-B1 does not
+# support the corresponding algorithms.
+set(TFM_CRYPTO_TEST_ALG_CFB                     OFF        CACHE BOOL     "Test CFB cryptography mode")
+set(TFM_CRYPTO_TEST_ALG_RSASSA_PSS_VERIFICATION OFF        CACHE BOOL     "Test RSASSA-PSS signature verification algorithm")
+set(TFM_CRYPTO_TEST_ALG_SHA_224                 OFF        CACHE BOOL     "Test SHA-224 hash algorithm")
+set(TFM_CRYPTO_TEST_ALG_OFB                     OFF        CACHE BOOL     "Test OFB cryptography mode")
 
 set(PLATFORM_SLIH_IRQ_TEST_SUPPORT    ON)
