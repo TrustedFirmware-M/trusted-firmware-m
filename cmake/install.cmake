1b702ac4d14e44a6bf5778b7a8310a025087dccc
Bohdan Hunko <Bohdan.Hunko@infineon.com>
2026-08-26 14:59:17 +0300

    Build: Make CMSIS repository layout configurable
    Allow platforms to override the CMSIS patch directory and source
    subdirectory while preserving the upstream defaults.
    
    It helps for platforms which ship own cmsis as pure CMSIS repository
    without the extra subdirectory.
    
    Change-Id: I0757f3a119a4ae1d1a1630fadfc4213b4fce0a74
    Signed-off-by: Bohdan Hunko <Bohdan.Hunko@infineon.com>

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -293,8 +293,8 @@ include(${CMAKE_SOURCE_DIR}/lib/ext/tf-m-tests/read_version.cmake)
 
 install(CODE "MESSAGE(\"----- Installing platform NS -----\")")
 
-install(DIRECTORY   $<BUILD_INTERFACE:${CMSIS_PATH}/CMSIS/Core/Include>
-                    $<BUILD_INTERFACE:${CMSIS_PATH}/CMSIS/Driver/Include>
+install(DIRECTORY   $<BUILD_INTERFACE:${CMSIS_SOURCE_ROOT}/Core/Include>
+                    $<BUILD_INTERFACE:${CMSIS_SOURCE_ROOT}/Driver/Include>
         DESTINATION ${INSTALL_PLATFORM_NS_DIR}/ext/cmsis)
 
 if(PLATFORM_DEFAULT_UART_STDOUT)
