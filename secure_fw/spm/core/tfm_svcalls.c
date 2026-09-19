TrustedFirmware-M/trusted-firmware-m

commit e5d803c328cbc5551fb381e3badf8b50e4f8d054

Build: Remove unused includes from SPM
Use IWYU tool to find unused includes in SPM and remove them.

Change-Id: I61f586d1f40dbc5482e9a03dedc3eb0e9dbe3486
Signed-off-by: Anton Komlev <anton.komlev@arm.com>

secure_fw/spm/core/tfm_svcalls.c
@@ -8,10 +8,7 @@
 #include <string.h>
 #include <stdint.h>
 #include "aapcs_local.h"
-#include "config_spm.h"
 #include "current.h"
-#include "interrupt.h"
-#include "internal_status_code.h"
 #include "memory_symbols.h"
 #include "spm.h"
 #include "coverity_check.h"
@@ -19,16 +16,11 @@
 #include "tfm_arch.h"
 #include "tfm_svcalls.h"
 #include "tfm_boot_data.h"
-#include "tfm_hal_platform.h"
 #include "tfm_hal_isolation.h"
 #include "tfm_hal_spm_logdev.h"
-#include "tfm_core_trustzone.h"
 #include "utilities.h"
 #include "ffm/backend.h"
 #include "ffm/psa_api.h"
-#include "load/spm_load_api.h"
-#include "load/partition_defs.h"
-#include "psa/client.h"
 
 #define INVALID_PSP_VALUE 0xFFFFFFFFU
 

platform/ext/target/cypress/psoc64/tfm_hal_isolation.c
@@ -20,6 +20,7 @@
 #include "load/asset_defs.h"
 #include "load/spm_load_api.h"
 #include "tfm_hal_isolation.h"
+#include "utilities.h"
 
 #define PROT_BOUNDARY_VAL \
     ((1U << HANDLE_ATTR_PRIV_POS) & HANDLE_ATTR_PRIV_MASK)

secure_fw/spm/core/arch/tfm_arch.c
@@ -1,13 +1,11 @@
 /*
- * Copyright (c) 2018-2023, Arm Limited. All rights reserved.
+ * SPDX-FileCopyrightText: Copyright The TrustedFirmware-M Contributors
  *
  * SPDX-License-Identifier: BSD-3-Clause
  *
  */
 
-#include "security_defs.h"
 #include "tfm_arch.h"
-#include "tfm_core_trustzone.h"
 #include "utilities.h"
 #include "config_impl.h"
 #include "coverity_check.h"

secure_fw/spm/core/arch/tfm_arch_v8m_main.c
@@ -1,8 +1,5 @@
 /*
  * SPDX-FileCopyrightText: Copyright The TrustedFirmware-M Contributors
- * Copyright (c) 2022-2024 Cypress Semiconductor Corporation (an Infineon
- * company) or an affiliate of Cypress Semiconductor Corporation. All rights
- * reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  *
@@ -12,12 +9,7 @@
 
 #include "config_spm.h"
 #include "security_defs.h"
-#include "region_defs.h"
-#include "spm.h"
-#include "svc_num.h"
 #include "tfm_arch.h"
-#include "tfm_hal_device_header.h"
-#include "tfm_svcalls.h"
 #include "utilities.h"
 #include "ffm/backend.h"
 

secure_fw/spm/core/backend_ipc.c
@@ -11,7 +11,6 @@
 #include "async.h"
 #include "config_spm.h"
 #include "critical_section.h"
-#include "config_spm.h"
 #include "current.h"
 #include "ffm/psa_api.h"
 #include "fih.h"
@@ -21,14 +20,9 @@
 #include "tfm_hal_isolation.h"
 #include "tfm_hal_platform.h"
 #include "tfm_nspm.h"
-#include "tfm_rpc.h"
 #include "ffm/backend.h"
 #include "utilities.h"
-#include "memory_symbols.h"
-#include "load/partition_defs.h"
-#include "load/service_defs.h"
 #include "load/spm_load_api.h"
-#include "psa/error.h"
 #include "internal_status_code.h"
 #include "sprt_partition_metadata_indicator.h"
 #include "coverity_check.h"
@@ -36,8 +30,6 @@
 #if TFM_PARTITION_NS_AGENT_MAILBOX == 1
 #include "psa_manifest/ns_agent_mailbox.h"
 #endif
-
-#include "compiler_ext_defs.h" /* Keep last. */
 
 /* Declare the global component list */
 struct partition_head_t partition_listhead;

secure_fw/spm/core/backend_sfn.c
@@ -1,8 +1,5 @@
 /*
  * SPDX-FileCopyrightText: Copyright The TrustedFirmware-M Contributors
- * Copyright (c) 2022-2023 Cypress Semiconductor Corporation (an Infineon
- * company) or an affiliate of Cypress Semiconductor Corporation. All rights
- * reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  *
@@ -16,10 +13,7 @@
 #include "tfm_nspm.h"
 #include "ffm/backend.h"
 #include "stack_watermark.h"
-#include "load/partition_defs.h"
-#include "load/service_defs.h"
 #include "load/spm_load_api.h"
-#include "psa/error.h"
 #include "psa/service.h"
 #include "spm.h"
 #include "memory_symbols.h"

secure_fw/spm/core/interrupt.h
@@ -12,11 +12,9 @@
 extern "C" {
 #endif /* __cplusplus */
 
-#include "spm.h"
-
 #include "load/interrupt_defs.h"
 #include "load/partition_defs.h"
-#include "psa/service.h"
+#include "tfm_arch.h"
 
 /**
  * \brief Return the IRQ load info context pointer associated with a signal

secure_fw/spm/core/main.c
@@ -6,12 +6,13 @@
  */
 
 #include <assert.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include "build_config_check.h"
 #include "internal_status_code.h"
 #include "fih.h"
 #include "tfm_boot_data.h"
 #include "memory_symbols.h"
-#include "spm.h"
 #include "coverity_check.h"
 #include "tfm_hal_isolation.h"
 #include "tfm_hal_platform.h"

commit c1e34d56d7d1a983a914e74071df0e356a4dae51

SPM: Restore interrupt include for tfm_svcalls
"interrupt.h" is required for:
 - tfm_flih_prepare_depriv_flih
 - tfm_flih_return_to_isr

Fixes: e5d803c328 (Build: Remove unused includes from SPM)
Signed-off-by: Nicola Mazzucato <nicola.mazzucato@arm.com>
Change-Id: Iddf5564942a2ad8f5266eaa1b53ed95859bd6fc4

secure_fw/spm/core/tfm_svcalls.c
@@ -9,6 +9,7 @@
 #include <stdint.h>
 #include "aapcs_local.h"
 #include "current.h"
+#include "interrupt.h"
 #include "memory_symbols.h"
 #include "spm.h"
 #include "coverity_check.h"